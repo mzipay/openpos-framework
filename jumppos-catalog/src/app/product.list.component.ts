f7beb6b986a94b690aaf168b9b1af7309f428124
mmichalek <mmichalek@jumpmind.com>
2017-09-14 19:30:11 -0400

    Additional styling, fix prices for demo items.

--- jumppos-catalog/src/app/product.list.component.ts
+++ jumppos-catalog/src/app/product.list.component.ts
@@ -1,5 +1,7 @@
 import { Component,Input } from '@angular/core';
 import {Product} from './product';
+import {MediaChange, ObservableMedia} from "@angular/flex-layout";
+
 import {
   trigger,
   state,
@@ -14,7 +16,28 @@ import {
 })
 
 export class ProductListComponent {
-  title = 'home';
-
+  gutterSize: number = 20;
+  gridColumns: number = 3;  
   products = Product.getProducts();
+
+  constructor(public media: ObservableMedia) {
+    
+ }
+
+ ngOnInit() {
+   this.updateGrid();
+   this.media.subscribe(() => {
+       this.updateGrid();
+   });
+ }
+
+ private updateGrid(): void {  
+   let isLarge = (this.media.isActive('xl') || this.media.isActive('lg') || this.media.isActive('md'));
+   let isSmall = (this.media.isActive('sm'));
+   this.gridColumns = isLarge ? 3 : (isSmall ? 2 : 1);
+   this.gutterSize = isLarge ? 40 : 10;
+ }   
+
 }
+
+
