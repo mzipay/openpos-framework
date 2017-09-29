b75d1fee70ac605ea53862f48a51538e52562d38
mmichalek <mmichalek@jumpmind.com>
2017-09-28 23:55:59 -0400

    Responsive UI changes

--- jumppos-client/src/app/screens/home.component.ts
+++ jumppos-client/src/app/screens/home.component.ts
@@ -3,6 +3,7 @@ import { IMenuItem } from '../common/imenuitem';
 // import {MdButtonModule, MdCheckboxModule} from '@angular/material';
 import { Component, ViewChild, AfterViewInit, DoCheck } from '@angular/core';
 import { SessionService } from '../session.service';
+import {MediaChange, ObservableMedia} from "@angular/flex-layout";
 
 
 @Component({
@@ -12,10 +13,26 @@ import { SessionService } from '../session.service';
 export class HomeComponent implements AfterViewInit, DoCheck, IScreen {
 
   public menuItems: IMenuItem[];
+  gutterSize: number = 40;
+  gridColumns: number = 3;
 
-  constructor(public session: SessionService) {
+  constructor(public session: SessionService, public media: ObservableMedia) {
     this.menuItems = session.screen.menuItems;
   }
+
+  ngOnInit() {
+    this.updateGrid();
+    this.media.subscribe(() => {
+        this.updateGrid();
+    });
+  }
+
+  private updateGrid(): void {  
+    let isLarge = (this.media.isActive('xl') || this.media.isActive('lg') || this.media.isActive('md'));
+    let isSmall = (this.media.isActive('sm'));
+    this.gridColumns = isLarge ? 3 : (isSmall ? 2 : 1);
+    this.gutterSize = isLarge ? 20 : 10;
+  }  
 
   show(session: SessionService) {
   }
@@ -29,5 +46,10 @@ export class HomeComponent implements AfterViewInit, DoCheck, IScreen {
 
   onEnter(value: string) {
     this.session.onAction('Save');
+  }
+
+  getClass(): String {
+    //return 'main-menu-grid-list';
+    return 'foo';
   }
 }
