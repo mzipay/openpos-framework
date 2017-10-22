24108d73dbbb289bbe7abe24177cd4c4c1d2b083
Chris Henson <chenson42@gmail.com>
2017-10-22 13:58:34 -0400

    make item inquiry responsive

--- jumppos-client/src/app/app-routing.module.ts
+++ jumppos-client/src/app/app-routing.module.ts
@@ -1,4 +1,3 @@
-import { InquiryComponent } from './screens/inquiry.component'; // TODO: remove when done experimenting with item search
 import { PosComponent } from './pos/pos.component';
 import { KioskComponent } from './kiosk/kiosk.component';
 import { NgModule } from '@angular/core';
@@ -7,8 +6,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: '/pos', pathMatch: 'full' },
   { path: 'pos',  component: PosComponent },
-  { path: 'kiosk',  component: KioskComponent },
-  { path: 'inquiry', component: InquiryComponent }  // TODO: remove when done experimenting with item search
+  { path: 'kiosk',  component: KioskComponent }
 ];
 
 @NgModule({
