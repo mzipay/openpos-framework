7f1f939e827730f13da8242bc59507b6266f4c42
Jason Mihalick <jason@jcraftsmen.com>
2017-10-26 16:41:19 -0400

    Refinements to signature capture

--- jumppos-client/src/app/screens/signature-capture.component.ts
+++ jumppos-client/src/app/screens/signature-capture.component.ts
@@ -74,8 +74,23 @@ export class SignatureCaptureComponent implements AfterViewInit, DoCheck, IScree
       const matches: RegExpMatchArray|null = dataUrl.match(/^data:.+\/(.+);base64,(.*)$/);
       encodedImage = matches && matches.length > 2 ? matches[2] : null;
     }
-    this.session.screen.signatureData = encodedImage;
-    this.session.response = JSON.stringify( {points: dataPoints, mediaType: mediaType, base64EncodedImage: encodedImage });
+    const signatureData: ISignature = {pointGroups: this.signaturePad.toData(), mediaType: mediaType, 
+      base64EncodedImage: encodedImage};
+
+    this.session.response = signatureData;
     this.session.onAction('SaveSignature');
   }
+}
+
+export interface ISignature {
+  pointGroups: IPoint[][];
+  mediaType: string;
+  base64EncodedImage: string|null;
+}
+
+export interface IPoint {
+  x: number;
+  y: number;
+  time: number;
+  color?: string;
 }
---
+++ jumppos-server/src/main/java/org/jumpmind/jumppos/core/model/Signature.java
@@ -0,0 +1,81 @@
+package org.jumpmind.jumppos.core.model;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class Signature implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    
+    private List<List<Point>> pointGroups;
+    private String mediaType;
+    private String base64EncodedImage;
+
+    public Signature() {}
+    
+    public List<List<Point>> getPointGroups() {
+        return pointGroups;
+    }
+
+
+    public void setPointGroups(List<List<Point>> pointGroups) {
+        this.pointGroups = pointGroups;
+    }
+
+
+    public String getMediaType() {
+        return mediaType;
+    }
+
+
+    public void setMediaType(String mediaType) {
+        this.mediaType = mediaType;
+    }
+
+
+    public String getBase64EncodedImage() {
+        return base64EncodedImage;
+    }
+
+
+    public void setBase64EncodedImage(String base64EncodedImage) {
+        this.base64EncodedImage = base64EncodedImage;
+    }
+
+
+    public static class Point implements Serializable {
+        private static final long serialVersionUID = 1L;
+            
+        private int x;
+        private int y;
+        private long time;
+        private String color;
+        
+        public Point() {}
+        
+        public int getX() {
+            return x;
+        }
+        public void setX(int x) {
+            this.x = x;
+        }
+        public int getY() {
+            return y;
+        }
+        public void setY(int y) {
+            this.y = y;
+        }
+        public long getTime() {
+            return time;
+        }
+        public void setTime(long time) {
+            this.time = time;
+        }
+        public String getColor() {
+            return color;
+        }
+        public void setColor(String color) {
+            this.color = color;
+        }
+    }
+}
