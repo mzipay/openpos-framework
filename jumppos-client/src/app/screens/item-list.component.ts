d552c481c5b2728eb16c2021c2269868031c91f4
Jason Mihalick <jason@jcraftsmen.com>
2017-11-01 14:54:34 -0400

    Fix repeated screen show bug

--- jumppos-client/src/app/screens/item-list.component.ts
+++ jumppos-client/src/app/screens/item-list.component.ts
@@ -1,6 +1,6 @@
 import { ItemClickAction, MenuClickAction } from './../common/controls/product-list.component';
 import { IMenuItem } from './../common/imenuitem';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, DoCheck } from '@angular/core';
 import { SessionService } from '../session.service';
 import { IScreen } from './../common/iscreen';
 import { IItem } from './../common/iitem';
@@ -9,12 +9,13 @@ import { IItem } from './../common/iitem';
     selector: 'app-item-list',
     templateUrl: './item-list.component.html'
 })
-export class ItemListComponent implements IScreen, OnInit {
+export class ItemListComponent implements IScreen, OnInit, DoCheck {
 
     items: IItem[];
     itemActionName: string;
     text: string;
     itemActions: IMenuItem[] = [];
+    private lastSequenceNum: number;
 
     constructor(public session: SessionService) {
     }
@@ -27,6 +28,14 @@ export class ItemListComponent implements IScreen, OnInit {
         this.itemActionName = this.session.screen.itemActionName;
         this.text = this.session.screen.text;
         this.itemActions = this.session.screen.itemActions;
+    }
+
+    ngDoCheck(): void {
+        if (this.session.screen.type === 'ItemList'
+            && this.session.screen.sequenceNumber !== this.lastSequenceNum) {
+          this.ngOnInit();
+          this.lastSequenceNum = this.session.screen.sequenceNumber;
+        }
     }
 
     onItemClick(itemInfo: ItemClickAction): void {
