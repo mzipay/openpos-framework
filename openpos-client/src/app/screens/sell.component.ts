3a1e8b656c908dcb6791b67fd10747285323cdd7
Jason Mihalick <jason@jCraftsmen.com>
2017-11-22 09:25:51 -0500

    Don't allow non-numeric characters in barcode field

--- openpos-client/src/app/screens/sell.component.ts
+++ openpos-client/src/app/screens/sell.component.ts
@@ -32,7 +32,40 @@ export class SellComponent implements AfterViewInit, DoCheck, IScreen {
 
   ngAfterViewInit(): void {
     this.initialized = true;
-    // setTimeout(() => this.vc.nativeElement.focus(), 0);
+  }
+
+  private filterBarcodeValue(val: string): string {
+    if (!val) {
+      return val;
+    }
+    // Filter out extra characters permitted by HTML5 input type=number (for exponentials)
+    const pattern = /[e|E|\+|\-]/g;
+
+    return val.toString().replace(pattern, '');
+  }
+
+  onBarcodeKeydown(event: KeyboardEvent) {
+    /*
+    console.log(`[onBarcodeKeydown] key: ${event.key}`);
+    console.log(`[onBarcodeKeydown] fromCharCode: ${String.fromCharCode(event.keyCode)}`);
+    console.log(`[onBarcodeKeydown] keyCode: ${event.keyCode}`);
+    */
+    if (event.altKey || event.ctrlKey || event.metaKey ) {
+      return true;
+    }
+    const filteredKey = this.filterBarcodeValue(event.key);
+    console.log(`[onBarcodeKeydown] filtered key: ${filteredKey}`);
+    return filteredKey !== null && filteredKey.length !== 0;
+  }
+
+  onBarcodePaste(event: ClipboardEvent) {
+    const content = event.clipboardData.getData('text/plain');
+    // console.log(`[onBarcodePaste]: ${content}`);
+    const filteredContent = this.filterBarcodeValue(content);
+    if (filteredContent !== content) {
+      console.log(`Clipboard data contains invalid characters for barcode, suppressing pasted content '${content}'`);
+    }
+    return filteredContent === content;
   }
 
   onEnter(value: string) {
