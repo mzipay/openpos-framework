3de88c4c3087c52cee7a5f9571d290164bb0e96a
Austin Brougher <austin.brougher@gmail.com>
2017-12-12 12:53:46 -0500

    TS-2414: Display status updates from the pin pad

--- openpos-client/src/app/screens/payment-status.component.ts
+++ openpos-client/src/app/screens/payment-status.component.ts
@@ -2,12 +2,15 @@ import { IScreen } from '../common/iscreen';
 import {Component, ViewChild, AfterViewInit, DoCheck} from '@angular/core';
 import {SessionService} from '../services/session.service';
 import { AbstractApp } from './abstract-app';
+import { IMenuItem } from '../common/imenuitem';
 
 @Component({
   selector: 'app-payment-status',
   templateUrl: './payment-status.component.html'
 })
 export class PaymentStatusComponent implements AfterViewInit, DoCheck, IScreen {
+
+  pinPadStatus: string = "0";
 
   constructor(public session: SessionService) {
   }
@@ -16,9 +19,20 @@ export class PaymentStatusComponent implements AfterViewInit, DoCheck, IScreen {
   }
 
   ngDoCheck(): void {
+    this.ngOnInit();
   }
 
   ngAfterViewInit(): void {
+    // Poll for status change if servers upports payment status updates.
+    this.session.onAction('GetPaymentStatus');
+  }
+
+  ngOnInit(): void {
+    this.pinPadStatus = this.session.screen.pinPadStatus;
+  }
+
+  public doMenuItemAction(menuItem: IMenuItem) {
+    this.session.onAction(menuItem.action);
   }
 
 }
--- openpos-core/src/main/java/org/jumpmind/pos/core/screen/PaymentStatusScreen.java
+++ openpos-core/src/main/java/org/jumpmind/pos/core/screen/PaymentStatusScreen.java
@@ -4,6 +4,16 @@ public class PaymentStatusScreen extends DefaultScreen {
 
     private static final long serialVersionUID = 1L;
     
+    private String pinPadStatus;
+    
+    public String getPinPadStatus() {
+        return pinPadStatus;
+    }
+
+    public void setPinPadStatus(String pinPadStatus) {
+        this.pinPadStatus = pinPadStatus;
+    }
+
     public PaymentStatusScreen() {
         setType(ScreenType.PaymentStatus);
     }
