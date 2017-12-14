95213837f0e4527e78ac596a19dc13c3eece860f
Dan Kaste <dan.kaste@leadingedje.com>
2017-12-14 11:44:32 -0500

    try a different approach for the media sizing with drawer state

--- openpos-client/src/app/templates/sell/sell.component.ts
+++ openpos-client/src/app/templates/sell/sell.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { OverlayContainer } from '@angular/cdk/overlay';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/takeWhile';
+import 'rxjs/add/operator/startWith';
 import { AbstractTemplate } from '../../common/abstract-template';
 import { SessionService } from '../../services/session.service';
 
 import { IMenuItem } from '../../common/imenuitem';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { ScanSomethingComponent } from '../../common/controls/scan-something/scan-something.component';
+import { ObservableMedia } from '@angular/flex-layout';
 
 @Component({
   selector: 'app-sell',
@@ -17,18 +22,55 @@ export class SellComponent extends AbstractTemplate implements OnInit {
   scanInputCallback: Function;
 
   @ViewChild('drawer') drawer;
-  public isMobile: boolean;
+  public drawerOpen: Observable<boolean>;
 
-  constructor( public overlayContainer: OverlayContainer, public session: SessionService, breakpointObserver: BreakpointObserver) {
+  public drawerMode: Observable<string>;
+
+  constructor( public overlayContainer: OverlayContainer, public session: SessionService, private observableMedia: ObservableMedia) {
     super(overlayContainer);
-    breakpointObserver.observe([
-      Breakpoints.Handset
-    ]).subscribe(result => {
-        this.isMobile = result.matches;
-    });
+
    }
 
   public ngOnInit(): void {
+    const openMap = new Map([
+      ['xs', false],
+      ['sm', true],
+      ['md', true],
+      ['lg', true],
+      ['xl', true]
+    ]);
+
+    let startOpen: boolean;
+    openMap.forEach((open, mqAlias) => {
+      if ( this.observableMedia.isActive(mqAlias)) {
+        startOpen = open;
+      }
+    });
+    this.drawerOpen = this.observableMedia.asObservable().map(
+      change => {
+        return openMap.get(change.mqAlias);
+      }
+    ).startWith(startOpen);
+
+    const modeMap = new Map([
+      ['xs', 'over'],
+      ['sm', 'side'],
+      ['md', 'side'],
+      ['lg', 'side'],
+      ['xl', 'side']
+    ]);
+
+    let startMode: string;
+    modeMap.forEach((mode, mqAlias) => {
+      if ( this.observableMedia.isActive(mqAlias)) {
+        startMode = mode;
+      }
+    });
+    this.drawerMode = this.observableMedia.asObservable().map(
+      change => {
+        return modeMap.get(change.mqAlias);
+      }
+    ).startWith(startMode);
     this.scanInputCallback = this.onScanInputEnter.bind(this);
   }
 
