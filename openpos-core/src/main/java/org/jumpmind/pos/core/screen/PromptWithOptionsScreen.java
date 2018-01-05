9b1b6fed9e0d6016cfe3a4dfd44db6729d1ea9a3
Dan Kaste <dan.kaste@leadingedje.com>
2018-01-05 15:45:06 -0500

    Update the prompt with options screen to extend the prompt screen instead of the choose option screen and then implement its own options.

--- openpos-core/src/main/java/org/jumpmind/pos/core/screen/PromptWithOptionsScreen.java
+++ openpos-core/src/main/java/org/jumpmind/pos/core/screen/PromptWithOptionsScreen.java
@@ -4,116 +4,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class PromptWithOptionsScreen extends ChooseOptionsScreen implements IPromptScreen {
+public class PromptWithOptionsScreen extends PromptScreen {
     private static final long serialVersionUID = 1L;
-
-    private String promptIcon;
-    private String placeholderText;
-    private boolean editable = true;
-    private String responseType;
-    private String responseText;
-    private int minLength;
-    private int maxLength;
-    private String action = "Next";
+    
+    private List<OptionItem> options;
 
     public PromptWithOptionsScreen() {
         this(new ArrayList<>());
     }
 
-    public PromptWithOptionsScreen(List<OptionItem> options) {
-        this(options, SelectionMode.Single);
-    }
-
-    public PromptWithOptionsScreen(List<OptionItem> options, SelectionMode selectionMode) {
-        super(options, selectionMode);
+    public PromptWithOptionsScreen(List<OptionItem> options ) {
+        super();
+        this.options = options;
         setType(ScreenType.PromptWithOptions);
     }
-
-    @Override
-    public int getMinLength() {
-        return minLength;
-    }
-
-    @Override
-    public void setMinLength(int minLength) {
-        this.minLength = minLength;
-    }
-
-    @Override
-    public int getMaxLength() {
-        return maxLength;
-    }
-
-    @Override
-    public void setMaxLength(int maxLength) {
-        this.maxLength = maxLength;
-    }
-
-    @Override
-    public String getPromptIcon() {
-        return promptIcon;
-    }
-
-    @Override
-    public void setPromptIcon(String promptIcon) {
-        this.promptIcon = promptIcon;
-    }
-
-    @Override
-    public String getPlaceholderText() {
-        return placeholderText;
-    }
-
-    @Override
-    public void setPlaceholderText(String placeholderText) {
-        this.placeholderText = placeholderText;
-    }
-
-    public String getAction() {
-        return action;
-    }
-
-    public void setAction(String action) {
-        this.action = action;
-    }
-
-    @Override
-    public String getResponseText() {
-        return responseText;
-    }
-
-    @Override
-    public void setResponseText(String responseText) {
-        this.responseText = responseText;
-    }
-
-    @Override
-    public String getResponseType() {
-        return responseType;
-    }
-
-    @Override
-    public void setResponseType(String responseType) {
-        this.responseType = responseType;
+    
+    public void addOption( OptionItem option ) {
+        this.getOptions().add( option );
     }
     
-    @Override
-    public boolean isEditable() {
-        return editable;
+    public List<OptionItem> getOptions() {
+        return options;
+    }
+    public void setOptions(List<OptionItem> options) {
+        this.options = options;
     }
 
-    @Override
-    public void setEditable(boolean editable) {
-        this.editable = editable;
-    }
-
-    @Override
-    public String getText() {
-        return this.getPromptText();
-    }
-
-    @Override
-    public void setText(String promptText) {
-        this.setPromptText(promptText);
-    }
 }
