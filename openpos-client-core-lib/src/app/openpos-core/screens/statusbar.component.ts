a2a23a57cd45ecbda8ae309b932b0e77c339e864
Jason Mihalick <jason@jCraftsmen.com>
2018-01-10 13:55:36 -0500

    Add dev menu to status bar

--- openpos-client-core-lib/src/app/openpos-core/screens/statusbar.component.ts
+++ openpos-client-core-lib/src/app/openpos-core/screens/statusbar.component.ts
@@ -1,14 +1,20 @@
+import { IPlugin } from './../common/iplugin';
 import { IMenuItem } from '../common/imenuitem';
 import { Component } from '@angular/core';
 import { SessionService } from '../services/session.service';
+import { PluginService } from './../services/plugin.service';
 
 @Component({
   selector: 'app-statusbar',
   templateUrl: './statusbar.component.html'
 })
 export class StatusBarComponent {
+  logFilenames: string[];
+  logPlugin: IPlugin;
+  protected devModeEnabled = false;
+  protected clickTimes: number[] = [];
 
-  constructor(public session: SessionService) {
+  constructor(public session: SessionService, public pluginService: PluginService) {
   }
 
   public doMenuItemAction(menuItem: IMenuItem) {
@@ -22,5 +28,59 @@ export class StatusBarComponent {
     }
     return enabled;
   }
+
+  protected onToolbarClick(): void  {
+    if (this.clickTimes.length >= 5) {
+      this.clickTimes.shift();
+    }
+    this.clickTimes.push((new Date()).getTime());
+
+    let numClicksUnderThreshold = 0;
+    for (let i = this.clickTimes.length - 1; i > 0; i--) {
+      const t = this.clickTimes[i];
+      const tPrev = this.clickTimes[i - 1];
+      if (t - tPrev <= 1500) {
+        numClicksUnderThreshold++;
+      }
+    }
+
+    console.log(`numClicksUnderThreshold: ${numClicksUnderThreshold}`);
+    if (numClicksUnderThreshold >= 4) {
+      this.clickTimes = [];
+      this.devModeEnabled = (! this.devModeEnabled);
+    }
+  }
+
+  protected onDevMenuClick(): void {
+    this.pluginService.getPlugin('openPOSCordovaLogPlugin').then(
+      (plugin: IPlugin) => {
+        this.logPlugin = plugin;
+        if (this.logPlugin && this.logPlugin.impl) {
+          this.logPlugin.impl.listLogFiles(
+            (fileNames) => {
+              this.logFilenames = fileNames;
+            },
+            (error) => {
+              this.logFilenames = [];
+            }
+          );
+        }
+      }
+    );
+  }
+
+  protected onLogfileSelected(logFilename: string): void {
+    if (this.logPlugin && this.logPlugin.impl) {
+      this.logPlugin.impl.shareLogFile(
+        logFilename,
+        () => {
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    }
+  }
+
 
 }
