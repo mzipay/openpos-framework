0aefe123f813d403a44a5d1c870ad54f0734a423
Jason Mihalick <jason@jCraftsmen.com>
2018-01-18 15:25:48 -0500

    Add retailer-independent SellItemDetail screen

--- openpos-translate/src/main/java/org/jumpmind/pos/translate/ILegacyCargo.java
+++ openpos-translate/src/main/java/org/jumpmind/pos/translate/ILegacyCargo.java
@@ -5,5 +5,10 @@ public interface ILegacyCargo {
     public <T> T getCargo();
     
     public String getOperatorFirstLastName();
+    /**
+     * Returns the RetailTransaction if the cargo has it, otherwise returns null.
+     * @return
+     */
+    public ILegacyRetailTransaction getRetailTransaction();
 
 }
