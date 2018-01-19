ebb6fd59e45179e93c8096e2296c43bc3a7b4a72
Dan Kaste <dan.kaste@leadingedje.com>
2018-01-19 14:08:22 -0500

    clean up linter errors

--- openpos-client-core-lib/src/app/openpos-core/common/actionIntercepter.ts
+++ openpos-client-core-lib/src/app/openpos-core/common/actionIntercepter.ts
@@ -1,34 +1,32 @@
 
 export class ActionIntercepter {
 
-  constructor( intercepter: FunctionActionIntercepter, behavior: ActionIntercepterBehavior ){
+  private intercepter: FunctionActionIntercepter;
+  private behavior: ActionIntercepterBehavior;
+
+  constructor( intercepter: FunctionActionIntercepter, behavior: ActionIntercepterBehavior ) {
     this.intercepter = intercepter;
   }
 
   public intercept( payload: any,  continueWith: Function) {
-      switch ( this.behavior){
+      switch ( this.behavior) {
         case ActionIntercepterBehavior.before:
-          console.log( "Intercepting Action" );
+          console.log( 'Intercepting Action' );
           this.intercepter( payload );
-          console.log( "Continuing with default action handler");
+          console.log( 'Continuing with default action handler');
           continueWith();
           break;
         case ActionIntercepterBehavior.block:
-          console.log( "Intercepting Action");
+          console.log( 'Intercepting Action');
           this.intercepter( payload );
-          console.log( "Skipping default action handler");
+          console.log( 'Skipping default action handler');
           break;
       }
   }
 
-  private intercepter: FunctionActionIntercepter;
-  private behavior: ActionIntercepterBehavior;
-
 }
 
-export interface FunctionActionIntercepter {
-  (data?: any): void;
-}
+export type FunctionActionIntercepter = (data?: any) => void;
 
 export enum ActionIntercepterBehavior {
   before,
