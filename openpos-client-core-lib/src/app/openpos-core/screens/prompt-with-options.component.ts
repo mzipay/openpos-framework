0867f3d8033335956a30931201ac0dd4f06db3cf
Dan Kaste <dan.kaste@leadingedje.com>
2018-02-16 10:54:33 -0500

    update prompt with options to match the new prompt intput control

--- openpos-client-core-lib/src/app/openpos-core/screens/prompt-with-options.component.ts
+++ openpos-client-core-lib/src/app/openpos-core/screens/prompt-with-options.component.ts
@@ -1,28 +1,39 @@
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, ValidatorFn, Validators } from '@angular/forms';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { SessionService } from '../services/session.service';
 import { ChooseOptionsComponent } from './choose-options.component';
 import { PromptInputComponent } from '../common/controls/prompt-input.component';
+
+import { PhoneUSValidatorDirective } from '../common/validators/phone.directive';
+import { OpenPosValidators } from '../common/validators/openpos-validators';
+
 @Component({
   selector: 'app-prompt-with-options',
   templateUrl: './prompt-with-options.component.html'
 })
 export class PromptWithOptionsComponent extends ChooseOptionsComponent implements OnInit {
-  @ViewChild(PromptInputComponent)  promptInput: PromptInputComponent;
-  promptFormGroup = new FormGroup({
-    promptInputControl: new FormControl('promptInputControl')
-  });
+
+  promptFormGroup: FormGroup;
 
   constructor(public session: SessionService) {
     super(session);
   }
 
   public ngOnInit(): void {
+
+    let group: any = {};
+    let validators: ValidatorFn[] = [];
+    validators.push(Validators.required);
+    if(this.session.screen.responseType == "phoneUS"){
+      validators.push(OpenPosValidators.PhoneUS);
+    }
+    group["promptInputControl"] = new FormControl(this.session.screen.responseText, validators);
+    this.promptFormGroup = new FormGroup(group);
   }
 
   onAction( action: string ): void {
-    if ( this.promptInput.responseText ) {
-      this.session.onAction( action, this.promptInput.responseText);
+    if ( this.promptFormGroup.valid ) {
+      this.session.onAction( action, this.promptFormGroup.value["promptInputControl"]);
     }
   }
 }
