aa9624cdccd02a3bd1dc0c9be5af35c42dfca7b8
Jason Mihalick <jason@jCraftsmen.com>
2018-02-21 15:39:26 -0500

    Piggy back of Dan, add support for localized phone formatting.
    Dan's commits, plus this fix address Ascena JIRA SS-7324, SS-7322

--- openpos-client-core-lib/src/app/openpos-core/common/formatters/phone-formatter.ts
+++ openpos-client-core-lib/src/app/openpos-core/common/formatters/phone-formatter.ts
@@ -1,12 +1,38 @@
 import { IFormatter } from './iformatter';
 
 export class PhoneFormatter implements IFormatter {
+    static readonly DEFAULT_FILTER_REGEX = /^[2-9]$|^[2-9][0-8]$|^[2-9][0-8][0-9]$|^[2-9][0-8][0-9][2-9]$|^[2-9][0-8][0-9][2-9][0-9]$|^[2-9][0-8][0-9][2-9][0-9]{2}$|^[2-9][0-8][0-9][2-9][0-9]{3}$|^[2-9][0-8][0-9][2-9][0-9]{4}$|^[2-9][0-8][0-9][2-9][0-9]{5}$|^[2-9][0-8][0-9][2-9][0-9]{6}$/;
+    static readonly LOCALE_FILTER_MAP = new Map<string, RegExp>([
+        ['us', PhoneFormatter.DEFAULT_FILTER_REGEX],
+        ['ca', /^[0-9]{1,10}$/]
+    ]);
 
-    locale?: string;
+    private _locale: string;
 
     private keyFilter = /[0-9]/;
-    private newValueFilter = /^[2-9]$|^[2-9][0-8]$|^[2-9][0-8][0-9]$|^[2-9][0-8][0-9][2-9]$|^[2-9][0-8][0-9][2-9][0-9]$|^[2-9][0-8][0-9][2-9][0-9]{2}$|^[2-9][0-8][0-9][2-9][0-9]{3}$|^[2-9][0-8][0-9][2-9][0-9]{4}$|^[2-9][0-8][0-9][2-9][0-9]{5}$|^[2-9][0-8][0-9][2-9][0-9]{6}$/
+    private newValueFilter = PhoneFormatter.DEFAULT_FILTER_REGEX;
     constructor() {
+    }
+
+    set locale(locl: string) {
+        this._locale = locl;
+        // cache the formater we'll use
+        if (locl) {
+            PhoneFormatter.LOCALE_FILTER_MAP.forEach((value, mapLocaleKey) => {
+                // If given at least a portion of the mapkey is contained in the set locale,
+                // that is the filter regex we will choose.
+                if (mapLocaleKey.indexOf(this._locale.toLowerCase()) >= 0) {
+                    this.newValueFilter = value;
+                }
+            });
+        } else {
+            this.newValueFilter = PhoneFormatter.DEFAULT_FILTER_REGEX;
+        }
+
+    }
+
+    get locale(): string {
+        return this._locale;
     }
 
     formatValue(value: string): string {
