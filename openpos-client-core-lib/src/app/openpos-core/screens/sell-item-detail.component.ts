feaf167bab9e19a9e0b40cd24af2b13c73e850ec
Austin Brougher <austin.brougher@gmail.com>
2018-02-21 10:18:22 -0500

    Add Promo Item List to the Sell Item Detail screen

--- openpos-client-core-lib/src/app/openpos-core/screens/sell-item-detail.component.ts
+++ openpos-client-core-lib/src/app/openpos-core/screens/sell-item-detail.component.ts
@@ -3,6 +3,8 @@ import {Component} from '@angular/core';
 import { ISellItem } from '../common/isellitem';
 import {SessionService} from '../services/session.service';
 import { AbstractApp } from '../common/abstract-app';
+import { IPromoItem } from '../common/ipromoitem';
+import {MatTableDataSource} from '@angular/material';
 
 @Component({
   selector: 'app-sell-item-detail',
@@ -13,11 +15,20 @@ import { AbstractApp } from '../common/abstract-app';
 export class SellItemDetailComponent implements IScreen {
   public item: ISellItem;
 
+  promosDataSource : MatTableDataSource<IPromoItem>;
+
   constructor(public session: SessionService ) {
     this.item = session.screen.item;
   }
 
   show(session: SessionService, app: AbstractApp) {
+  }
+
+  ngDoCheck(): void {
+      if (typeof this.session.screen !== 'undefined') {
+        // this.items = this.session.screen.items;
+        this.promosDataSource = new MatTableDataSource(this.session.screen.promos);
+      }
   }
 
 }
--- openpos-core/src/main/java/org/jumpmind/pos/core/screen/SellItemDetailScreen.java
+++ openpos-core/src/main/java/org/jumpmind/pos/core/screen/SellItemDetailScreen.java
@@ -10,6 +10,7 @@ public class SellItemDetailScreen extends DefaultScreen {
     private SellItem item = new SellItem();
     private List<MenuItem> itemActions = new ArrayList<>();
     private Transaction transaction = new Transaction();
+    private List<PromoItem> promos = new ArrayList<>();
 
     public SellItemDetailScreen() {
         setType(ScreenType.SellItemDetail);
@@ -37,6 +38,14 @@ public class SellItemDetailScreen extends DefaultScreen {
 
     public void setTransaction(Transaction transaction) {
         this.transaction = transaction;
-    }       
+    }
+
+	public List<PromoItem> getPromos() {
+		return promos;
+	}
+
+	public void setPromos(List<PromoItem> promos) {
+		this.promos = promos;
+	}
 
 }
