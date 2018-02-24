e98bc32af5aea17325663052a680c8a7bd02ae5e
Jason Mihalick <jason@jCraftsmen.com>
2018-02-24 11:22:47 -0500

    Fix IDE warning about allowKey method not matching interface

--- openpos-client-core-lib/src/app/openpos-core/common/formatters/donothing-formatter.ts
+++ openpos-client-core-lib/src/app/openpos-core/common/formatters/donothing-formatter.ts
@@ -1,19 +1,19 @@
 import { IFormatter } from "./iformatter";
 
 export class DoNothingFormatter implements IFormatter {
-   
     locale?: string;
 
     formatValue(value: string): string {
         if( !value ) return "";
         return value;
     }
-   
+
+
     unFormatValue(value: string): string {
         return value;
     }
 
-    allowKey(key: string, newValue: string) {
+    allowKey(key: string, newValue: string): boolean {
         return true;
     }
 }
