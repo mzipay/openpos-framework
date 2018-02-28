17734cd1995c0b1f7215714e673a9b98ff507ae4
Jason Mihalick <jason@jCraftsmen.com>
2018-02-28 13:49:45 -0500

    Add provision to exclude items from ChooseOptions screen...
    ... upon construction.  Add other convenience methods.

--- openpos-translate/src/main/java/org/jumpmind/pos/translate/ChooseOptionsScreenTranslator.java
+++ openpos-translate/src/main/java/org/jumpmind/pos/translate/ChooseOptionsScreenTranslator.java
@@ -1,6 +1,8 @@
 package org.jumpmind.pos.translate;
 
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import org.jumpmind.pos.core.screen.ChooseOptionsScreen;
 import org.jumpmind.pos.core.screen.OptionItem;
@@ -8,11 +10,26 @@ import org.jumpmind.pos.core.screen.ScreenType;
 
 public class ChooseOptionsScreenTranslator<T extends ChooseOptionsScreen> extends AbstractLegacyScreenTranslator<T> { 
     
+    protected Function<OptionItem, Boolean> optionItemEvalFunc = null;
+    
     public ChooseOptionsScreenTranslator(ILegacyScreen headlessScreen, Class<T> screenClass) {
-        super(headlessScreen, screenClass);
-        getScreen().setType(ScreenType.ChooseOptions);
+        this(headlessScreen, screenClass, null);
     }
 
+    /**
+     * Constructor to be used for modifying OptionItems or excluding them.
+     *  
+     * @param headlessScreen The legacy screen,
+     * @param screenClass The screenClass
+     * @param optionFilter A function that can be used for modifying options or excluding them.  The given function will be invoked
+     * for each OptionItem.  If the function returns false, the item will be excluded.
+     */
+    public ChooseOptionsScreenTranslator(ILegacyScreen headlessScreen, Class<T> screenClass, Function<OptionItem, Boolean> optionFilter) {
+        super(headlessScreen, screenClass);
+        this.optionItemEvalFunc = optionFilter;
+        getScreen().setType(ScreenType.ChooseOptions);
+    }
+    
     @Override
     protected void buildMainContent() {
         super.buildMainContent();
@@ -25,6 +42,9 @@ public class ChooseOptionsScreenTranslator<T extends ChooseOptionsScreen> extend
     
     protected void buildOptions() {
         List<OptionItem> options = this.generateUIActionsForLocalNavButtons(OptionItem.class, true, new String[]{});
+        if (this.optionItemEvalFunc != null) {
+            options = options.stream().filter(o -> { return this.optionItemEvalFunc.apply(o); }).collect(Collectors.toList());
+        }
         getScreen().setOptions(options);
     }
 }
