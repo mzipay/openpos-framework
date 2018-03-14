7b22b16ca393a21124e113eaed835b79c7fdaae5
Dan Kaste <dan.kaste@gmail.com>
2018-03-14 16:12:37 -0400

    pull a newer version of material into the core

--- openpos-client-core-lib/src/app/openpos-core/index.ts
+++ openpos-client-core-lib/src/app/openpos-core/index.ts
@@ -5,4 +5,6 @@ export { OpenposCoreModule } from './openpos-core.module';
 export { MaterialModule } from './material.module';
 export * from './services';
 export * from './common';
+export * from '@angular/material';
+export * from '@angular/cdk/layout';
 
