0834155d7aa23a262b16f1bcf7fd363ae40e5709
Dan Kaste <dan.kaste@gmail.com>
2018-03-19 15:47:54 -0400

    pass a screens template into the show function and add "actionDisablers"
    This allows us to disable buttons on the template from logic that exists
    on the screen.

--- openpos-client-core-lib/src/app/openpos-core/common/iscreen.ts
+++ openpos-client-core-lib/src/app/openpos-core/common/iscreen.ts
@@ -1,6 +1,7 @@
 import { SessionService } from '../services/session.service';
 import { AbstractApp } from '../common/abstract-app';
+import { AbstractTemplate } from '.';
 
 export interface IScreen {
-    show(screen: any, app: AbstractApp): void;
+    show(screen: any, app: AbstractApp, template?: AbstractTemplate): void;
 }
