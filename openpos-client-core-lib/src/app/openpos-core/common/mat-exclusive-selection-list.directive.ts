edbb3f71ef1aafc0ce82458357c0029c0f426e17
Unknown <dan.kaste@gmail.com>
2018-04-18 14:33:22 -0400

    use HostListener instead

--- openpos-client-core-lib/src/app/openpos-core/common/mat-exclusive-selection-list.directive.ts
+++ openpos-client-core-lib/src/app/openpos-core/common/mat-exclusive-selection-list.directive.ts
@@ -1,13 +1,12 @@
-import { Directive, Input, ElementRef, forwardRef, Renderer2, OnInit } from '@angular/core';
+import { Directive, Input, ElementRef, forwardRef, Renderer2, OnInit, HostListener } from '@angular/core';
 import { MatSelectionList, MatListOption } from '@angular/material';
 
 @Directive({
-    selector: 'mat-selection-list[exclusiveSelect]',
-    host: { 
-        '(selectionChange)': 'onSelectionChange($event.option, $event.source)'
-     }
+    selector: 'mat-selection-list[exclusiveSelect]'
 })
 export class MatExclusiveSelectionListDirective {
+
+    @HostListener('selectionChange', ['$event.option', '$event.source'])
     onSelectionChange(option: MatListOption, source: MatSelectionList) {
         if(option.selected) {
             source.options.forEach(o=>
