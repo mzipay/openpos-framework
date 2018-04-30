163e71169380938de489d24a62a87349019dc344
Jason Mihalick <jason@jCraftsmen.com>
2018-04-30 15:59:29 -0400

    Added support for passing in window.open options/features

--- openpos-client-core-lib/src/app/openpos-core/common/iurlmenuitem.ts
+++ openpos-client-core-lib/src/app/openpos-core/common/iurlmenuitem.ts
@@ -3,4 +3,5 @@ import { IMenuItem } from './imenuitem';
 export interface IUrlMenuItem extends IMenuItem {
     url: string;
     targetMode: string;
+    options: string;  // Corresponds to options/features that can be passed to window.open()
 }
