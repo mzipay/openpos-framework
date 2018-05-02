1b9f0242afb6c596a777543742455f538edf251b
Jason Mihalick <jason@jCraftsmen.com>
2018-05-02 16:55:32 -0400

    Add methods for accessing integer params

--- openpos-translate/src/main/java/org/jumpmind/pos/translate/ILegacyParameterManager.java
+++ openpos-translate/src/main/java/org/jumpmind/pos/translate/ILegacyParameterManager.java
@@ -5,4 +5,6 @@ public interface ILegacyParameterManager {
     Boolean getBooleanValue(String paramName);
     Boolean getBooleanValue(String paramName, Boolean defaultValue);
     String getStringValue(String paramName);
+    Integer getIntegerValue(String paramName);
+    Integer getIntegerValue(String paramName, Integer defaultValue);
 }
