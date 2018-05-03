4c808c7d13dfce1fb146f376f02b71872937e767
Austin Brougher <austin.brougher@gmail.com>
2018-05-03 19:16:14 -0400

    Add customerFacing flag to dialogs.

--- openpos-client-core-lib/src/app/openpos-core/common/idialog.ts
+++ openpos-client-core-lib/src/app/openpos-core/common/idialog.ts
@@ -6,6 +6,7 @@ export interface IDialog {
     buttons: IMenuItem[];
     subType: string;
     message: string[];
+    customerFacing: boolean;
 }
 
 export interface OpenPOSDialogConfig extends MatDialogConfig {
