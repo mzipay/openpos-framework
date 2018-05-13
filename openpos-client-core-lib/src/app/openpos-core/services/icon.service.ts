c9ed6d3e4dfe4237b71ba1a0adb7b49374e45e9b
Jason Mihalick <jason@jCraftsmen.com>
2018-05-12 22:24:06 -0400

    Serve custom icons from spring-boot instead of locally.
    Fixes issue with icons not rendering/loading from when app is run from
    local file system (as it is in Cordova or when just run by launching in
    browser from local file system).

--- openpos-client-core-lib/src/app/openpos-core/services/icon.service.ts
+++ openpos-client-core-lib/src/app/openpos-core/services/icon.service.ts
@@ -1,21 +1,28 @@
-import { DomSanitizer } from '@angular/platform-browser';
+import { SessionService } from './session.service';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { Injectable } from '@angular/core';
 import { MatIconRegistry } from '@angular/material';
 
 @Injectable()
 export class IconService {
     constructor(private iconRegistry: MatIconRegistry,
-        private sanitizer: DomSanitizer) {
-        this.iconRegistry.addSvgIcon('local_calculator', this.sanitizer.bypassSecurityTrustResourceUrl('./assets/icons/calculator.svg'));
-        this.iconRegistry.addSvgIcon('local_cash', this.sanitizer.bypassSecurityTrustResourceUrl('./assets/icons/cash.svg'));
-        this.iconRegistry.addSvgIcon('local_cash-multiple',
-            this.sanitizer.bypassSecurityTrustResourceUrl('./assets/icons/cash-multiple.svg'));
-        this.iconRegistry.addSvgIcon('local_postvoid', this.sanitizer.bypassSecurityTrustResourceUrl('./assets/icons/postvoid.svg'));
-        this.iconRegistry.addSvgIcon('barcode', this.sanitizer.bypassSecurityTrustResourceUrl('./assets/icons/barcode.svg'));
-        this.iconRegistry.addSvgIcon('percent', this.sanitizer.bypassSecurityTrustResourceUrl('./assets/icons/percent.svg'));
+        private sanitizer: DomSanitizer, private sessionService: SessionService) {
+        this.iconRegistry.addSvgIcon('local_calculator',    this.makeIconSafeUrl('calculator.svg'));
+        this.iconRegistry.addSvgIcon('local_cash',          this.makeIconSafeUrl('cash.svg'));
+        this.iconRegistry.addSvgIcon('local_cash-multiple', this.makeIconSafeUrl('cash-multiple.svg'));
+        this.iconRegistry.addSvgIcon('local_postvoid',      this.makeIconSafeUrl('postvoid.svg'));
+        this.iconRegistry.addSvgIcon('barcode',             this.makeIconSafeUrl('barcode.svg'));
+        this.iconRegistry.addSvgIcon('local_barcode',       this.makeIconSafeUrl('barcode.svg'));
+        this.iconRegistry.addSvgIcon('percent',             this.makeIconSafeUrl('percent.svg'));
     }
 
     public addIcon(key: string, asset: string) {
+        // TODO: needs fixed. adding icons as local file resources will not work when running the application
+        // as standalone javascript app in Cordova or locally in file system.  (i.e., when not served)
         this.iconRegistry.addSvgIcon(key, this.sanitizer.bypassSecurityTrustResourceUrl(`./assets/icons/${asset}`));
+    }
+
+    private makeIconSafeUrl(iconFilename: string): SafeResourceUrl {
+        return this.sanitizer.bypassSecurityTrustResourceUrl(`${this.sessionService.getServerBaseURL()}/img/${iconFilename}`);
     }
 }
