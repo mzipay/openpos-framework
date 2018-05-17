4df5df8f96628a58196dcd775d25523ef63d5f36
Chris Henson <chenson42@gmail.com>
2018-05-17 09:31:51 -0400

    specify the correct tx manager to use

--- openpos-user/src/main/java/org/jumpmind/pos/user/service/AuthenticateEndpoint.java
+++ openpos-user/src/main/java/org/jumpmind/pos/user/service/AuthenticateEndpoint.java
@@ -14,7 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Component
-@Transactional
+@Transactional(transactionManager="userTxManager")
 public class AuthenticateEndpoint {
 
     @Autowired
