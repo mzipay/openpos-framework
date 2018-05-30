5948eaef363b4d9c4e098c64b77942e159971a7c
JJStarrett <jstarrett@jumpmind.com>
2018-05-30 10:13:09 -0400

    updated tax repository to reflect TODO

--- openpos-tax/src/main/java/org/jumpmind/pos/tax/model/TaxRepository.java
+++ openpos-tax/src/main/java/org/jumpmind/pos/tax/model/TaxRepository.java
@@ -37,15 +37,14 @@ public class TaxRepository {
             String authorityId = jurisdiction.getAuthorityId();
             Authority authority = dbSession.findByNaturalId(Authority.class, authorityId);
             jurisdiction.setAuthority(authority);
-
-            updateRateRuleMap(rateRuleMap, authorityId);
-
+            
             List<GroupRule> groupRules = dbSession.query(taxGroupRuleLookup, authorityId);
             if (groupRules != null) {
                 for (GroupRule groupRule : groupRules) {
                     authority.addGroupRule(groupRule);
                     groupRule.setAuthority(authority);
                     String groupID = groupRule.getGroupId();
+                    updateRateRuleMap(rateRuleMap, authorityId, groupID);
                     if (groupMap.containsKey(groupID)) {
                         Group group = groupMap.get(groupID);
                         groupRule.setGroup(group);
@@ -64,16 +63,9 @@ public class TaxRepository {
         return jurisdictions;
     }
 
-    private void updateRateRuleMap(HashMap<String, List<RateRule>> rateRuleMap, String authorityID) {
-        Set<String> keySet = rateRuleMap.keySet();
-        boolean contains = false;
-        for (String string : keySet) {
-            if (string.contains(authorityID)) {
-                contains = true;
-            }
-        }
-        // TODO this should just be !rateRuleMap.contains(authorityID + "-" + groupdId") and pass in groupId
-        if (!contains) {
+    private void updateRateRuleMap(HashMap<String, List<RateRule>> rateRuleMap, String authorityID, String groupId) {
+
+        if (!rateRuleMap.containsKey(authorityID + "-" + groupId)) {
             List<RateRule> rateRules = dbSession.query(taxRateRuleLookup, authorityID);
             if (rateRules != null) {
                 for (RateRule rateRule : rateRules) {
