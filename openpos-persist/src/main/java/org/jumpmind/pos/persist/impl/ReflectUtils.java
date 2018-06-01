69655ba6472da9dbd808c75d52367abb51eec532
maxwellpettit <maxwell.pettit@gmail.com>
2018-06-01 14:29:20 -0400

    Fix casting of primative types from null database values

--- openpos-persist/src/main/java/org/jumpmind/pos/persist/impl/ReflectUtils.java
+++ openpos-persist/src/main/java/org/jumpmind/pos/persist/impl/ReflectUtils.java
@@ -33,8 +33,8 @@ public class ReflectUtils {
             return value;
         }
         Field field = getAccessibleField(target, propertyName);
-        if (field != null && field.getType().isAssignableFrom(boolean.class)) {
-            return false;
+        if (field != null && field.getType().isPrimitive()) {
+            return 0;
         }
         
         return value;
