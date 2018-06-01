6923b6efd4595c6a5ece989c491ed4fa0db49139
mmichalek <mmichalek@jumpmind.com>
2018-06-01 14:10:06 -0400

    Fix for properly setting super class fields

--- openpos-persist/src/main/java/org/jumpmind/pos/persist/impl/ReflectUtils.java
+++ openpos-persist/src/main/java/org/jumpmind/pos/persist/impl/ReflectUtils.java
@@ -10,62 +10,51 @@ public class ReflectUtils {
 
     public static void setProperty(Object target, String propertyName, Object value) {
         value = messageNulls(target, propertyName, value);
-        try {            
-            Class<? extends Object> clazz = target.getClass();
-            while (clazz != Object.class) {
-                Field field = null;
-                try {
-                    field = clazz.getDeclaredField(propertyName);                    
-                } catch (NoSuchFieldException ex2) {
-                }
-                if (field != null) {
-                    field.setAccessible(true);
-                    try {                        
-                        field.set(target, value);
-                    } catch (Exception ex) {
-                        try {                            
-                            BeanUtils.copyProperty(target, propertyName, value);
-                        } catch (Exception ex2) {
-                            throw ex;
-                        }
-                    }
+        try {
+            Field field = getAccessibleField(target, propertyName);
+            try {                        
+                field.set(target, value);
+                return;
+            } catch (Exception ex) {
+                try {                            
+                    BeanUtils.copyProperty(target, propertyName, value);
                     return;
+                } catch (Exception ex2) {
+                    throw ex;
                 }
-                clazz = clazz.getSuperclass();
             }
         } catch (Exception ex) {
             throw new PersistException(String.format("Failed to set field '%s' on target '%s' to value '%s'", propertyName, target, value), ex);
         }
-        
-        throw new PersistException(String.format("Could not locate field '%s' on target '%s' to set to value '%s'", propertyName, target, value));
     }
 
     private static Object messageNulls(Object target, String propertyName, Object value) {
         if (value != null) {
             return value;
         }
-        Field field = FieldUtils.getDeclaredField(target.getClass(), propertyName, true);
-        if (field.getType().isAssignableFrom(boolean.class)) {
+        Field field = getAccessibleField(target, propertyName);
+        if (field != null && field.getType().isAssignableFrom(boolean.class)) {
             return false;
         }
         
         return value;
     }
     
-//    private static Object messageValue(Object target, String propertyName, Field field, Object value) {
-//        
-//        if (Integer.class.isAssignableFrom(field.getType())
-//                || int.class.isAssignableFrom(field.getType())) {
-//            if (value instnaceof Long) {
-//                Long longValue = (Long) value;
-//                if (longValue <= Integer.MAX_VALUE) {
-//                    Integer intValue = (int) longValue.longValue();
-//                    return intValue;
-//                }
-//            }
-//        }
-//        
-//        return value;
-//    }    
+    private static Field getAccessibleField(Object target, String propertyName) {
+        Class<? extends Object> clazz = target.getClass();
+        while (clazz != Object.class) {
+            Field field = null;
+            try {
+                field = clazz.getDeclaredField(propertyName);                    
+            } catch (NoSuchFieldException ex2) {
+            }
+            if (field != null) {
+                field.setAccessible(true);
+                return field;
+            }
+            clazz = clazz.getSuperclass();
+        }
+        return null;
+    }
     
 }
