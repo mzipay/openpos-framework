a96f34a41acd250ea44c5cf945f21880f46b38d0
Chris Henson <chenson42@gmail.com>
2018-06-04 15:28:53 -0400

    add module and service

---
+++ openpos-ops/src/main/java/org/jumpmind/pos/ops/service/OpsService.java
@@ -0,0 +1,10 @@
+package org.jumpmind.pos.ops.service;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ops")
+public class OpsService {
+
+}
