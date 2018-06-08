93effb4fba18c51422b78b317f450060b3eebd42
Jason Mihalick <jason@jCraftsmen.com>
2018-06-07 22:14:50 -0400

    Add controller to block Swagger UI unless swagger-ui profile is present

---
+++ openpos-core/src/main/java/org/jumpmind/pos/core/config/DisableSwaggerUiController.java
@@ -0,0 +1,25 @@
+package org.jumpmind.pos.core.config;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@Profile("!swagger-ui")
+@RestController
+/**
+ * Disables the Swagger main UI page unless the Spring profile named 'swagger-ui' is active.
+ *
+ */
+public class DisableSwaggerUiController {
+
+    @RequestMapping(value = "swagger-ui.html", method = RequestMethod.GET)
+    public void getSwagger(HttpServletResponse httpResponse) throws IOException {
+        httpResponse.setStatus(HttpStatus.NOT_FOUND.value());
+    }
+}
