4f5e26768148bef1f5f87ba6fed12fbf632794f2
JJStarrett <jstarrett@jumpmind.com>
2018-06-14 13:38:57 -0400

    added Flow Scope and State Connections to dev menu backend

--- openpos-core/src/main/java/org/jumpmind/pos/core/screen/DevToolsMessage.java
+++ openpos-core/src/main/java/org/jumpmind/pos/core/screen/DevToolsMessage.java
@@ -11,6 +11,7 @@ import org.jumpmind.pos.core.flow.IStateManager;
 import org.jumpmind.pos.core.flow.Scope;
 import org.jumpmind.pos.core.flow.ScopeValue;
 import org.jumpmind.pos.core.flow.StateManager;
+import org.jumpmind.pos.core.flow.config.StateConfig;
 import org.jumpmind.pos.core.service.ScreenService;
 
 public class DevToolsMessage extends Screen {
@@ -19,15 +20,23 @@ public class DevToolsMessage extends Screen {
 	
 	private Map<String, List<ScopeField>> scopes = new HashMap<>();
 	
+	private List<String> actions = new ArrayList<>();
+	
 	public DevToolsMessage(IStateManager sm, ScreenService ss) {
 		setType(ScreenType.DevTools);
 		setName("DevTools");
 		setRefreshAlways(true);
 		setScopes(sm.getApplicationState().getScope());
-		setCurrentState(sm);
+		setCurrentStateAndActions(sm);
+		setConfigAndFlow(sm);
+		
 		this.put("scopes", scopes);
 		
-		//sm.getApplicationState().getCurrentContext().getFlowConfig().getConfigScope();
+		Map<String, Object> map = sm.getApplicationState().getCurrentContext().getFlowConfig().getConfigScope();
+		Set<String> keys = map.keySet();
+		for (String key : keys) {
+			Object obj = map.get(key);
+		}
 		
 
 	}
@@ -37,7 +46,8 @@ public class DevToolsMessage extends Screen {
 		setName("DevTools");
 		setRefreshAlways(true);
 		setScopes(sm.getApplicationState().getScope());
-		
+		setCurrentStateAndActions(sm);
+		setConfigAndFlow(sm);
 		
 		String scopeId = element.get("ID");
 		if (cmd.equals("remove")) {
@@ -52,6 +62,9 @@ public class DevToolsMessage extends Screen {
     			scopes.get("ConversationScope").removeIf(p -> p.getName().equals(scopeId));
     		} else if (scopeType.equals("Config")) {
     			//TODO add support
+    		} else if (scopeType.equals("Flow")) {
+    			sm.getApplicationState().getCurrentContext().removeFlowScope(scopeId);
+    			scopes.get("FlowScope").removeIf(p -> p.getName().equals(scopeId));
     		}
 		} else {
 			// TODO 'add a scope' support?
@@ -59,23 +72,34 @@ public class DevToolsMessage extends Screen {
 		this.put("scopes", scopes);
 	}
 
-	private void setCurrentState (IStateManager sm) {
+	private void setCurrentStateAndActions (IStateManager sm) {
 		IState currentState = sm.getCurrentState();
+		StateConfig sc = sm.getApplicationState().getCurrentContext().getFlowConfig().getStateConfig(currentState);
+		Set<String> keySet = sc.getActionToStateMapping().keySet();
+		for (String key : keySet) {
+			actions.add(key);
+			actions.add(sc.getActionToStateMapping().get(key).toString().replace("class ", ""));
+		}
+		this.put("actions", actions);
+		this.put("actionsSize", actions.size() / 2);
 		this.put("currentState", sm.getApplicationState().getCurrentContext().getFlowConfig().getStateConfig(currentState));
 	}
 
+	private void setConfigAndFlow(IStateManager sm) {
+		scopes.put("FlowScope", buildScope(sm.getApplicationState().getCurrentContext().getFlowScope()));
+	}
 	private void setScopes (Scope scope) {
 		scopes.put("ConversationScope", buildScope(scope.getConversationScope()));
 		scopes.put("NodeScope", buildScope(scope.getNodeScope()));
 		scopes.put("SessionScope", buildScope(scope.getSessionScope()));
-		
 	}
+	
 	
 	private List<ScopeField> buildScope(Map<String, ScopeValue> map) {
 		Set<String> keys = map.keySet();
 		List<ScopeField> res = new ArrayList<>();
 		for (String key : keys) {
-			res.add(new ScopeField(key, map.get(key).getValue().toString(), map.get(key).getCreatedTime().toString(), map.get(key).getCreatedStackTrace().replaceAll("at", "\n  at")));
+			res.add(new ScopeField(key, map.get(key).getValue().toString(), map.get(key).getCreatedTime().toString(), map.get(key).getCreatedStackTrace().replaceAll("at ", "\nat_")));
 		}
 		return res;
 	}
