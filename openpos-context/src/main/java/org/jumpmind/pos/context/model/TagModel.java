b1f01d5eded94899ccfa13f281eee0ca18f66473
Chris Henson <chenson42@gmail.com>
2018-06-19 20:11:57 -0400

    fix compile error

--- openpos-context/src/main/java/org/jumpmind/pos/context/model/TagModel.java
+++ openpos-context/src/main/java/org/jumpmind/pos/context/model/TagModel.java
@@ -5,6 +5,8 @@ public class TagModel {
     public static final String TAG_PREFIX = "TAG_";
     public static final String TAG_ALL = "*";
     public static final String TAG_CODE_TYPE = "CODE";
+    public static final String BRAND_ID_TAG = "BRAND";
+    
     
     private String name; // e.g. STORE
     private String group;  // e.g. LOCATION
