64dca8180da7772e88a8261920e43fab90757fc1
mmichalek <mmichalek@jumpmind.com>
2018-06-25 12:45:12 -0400

    Allow states to specify a particular timeout action per screen

--- openpos-server/src/main/java/org/jumpmind/pos/app/DefaultSessionTimeoutListener.java
+++ openpos-server/src/main/java/org/jumpmind/pos/app/DefaultSessionTimeoutListener.java
@@ -1,5 +1,6 @@
 package org.jumpmind.pos.app;
 
+import org.jumpmind.pos.core.flow.Action;
 import org.jumpmind.pos.core.flow.ISessionTimeoutListener;
 import org.jumpmind.pos.core.flow.StateManager;
 import org.springframework.stereotype.Component;
@@ -8,7 +9,7 @@ import org.springframework.stereotype.Component;
 public class DefaultSessionTimeoutListener implements ISessionTimeoutListener {
 
     @Override
-    public void onSessionTimeout(StateManager stateManager) {
+    public void onSessionTimeout(StateManager stateManager, Action action) {
         stateManager.endConversation();
         stateManager.endSession();
     }
