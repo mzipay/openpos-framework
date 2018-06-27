ba6fd0edeb06a4a973fbe33a06c92bd1ef09f406
Dan Kaste <dan.kaste@leadingedje.com>
2018-06-27 10:40:18 -0400

    experiment with adding a grunt file to do our core build process

---
+++ gruntfile.js
@@ -0,0 +1,54 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        watch: {
+            files: {
+              files: ['**/*', '!**/node_modules/**'],
+              tasks: ['sync-files'],
+              options: {
+                spawn: false,
+              },
+            },
+          },
+        sync: {
+            main: {
+              files: [
+                {
+                    cwd: './',
+                    src: [
+                        '**/*',
+                        '!**/node_modules/**',
+                        '!gruntfile.js'
+                    ], 
+                    dest: 
+                    '.dist/'
+                }, 
+              ],
+              verbose: true, // Default: false
+              pretend: false, // Don't do any disk operations - just write log. Default: false
+              failOnError: true, // Fail the task when copying is not possible. Default: false
+              updateAndDelete: true, // Remove all files from dest that are not found in src. Default: false
+              compareUsing: "md5" // compares via md5 hash of file contents, instead of file modification time. Default: "mtime"
+           
+            }
+          },
+          exec: {
+            link: {
+                cwd: '.dist',
+                cmd: 'npm link'
+            },
+            prune: {
+                cwd: '.dist',
+                cmd: 'rm -rf node_modules'
+            }
+        }
+      });
+       
+      grunt.loadNpmTasks('grunt-sync');
+      grunt.loadNpmTasks('grunt-contrib-watch');
+      grunt.loadNpmTasks('grunt-exec');
+
+      grunt.registerTask('default', ['sync', 'exec:link', 'exec:prune', 'watch']);
+      grunt.registerTask('sync-files', ['sync']);
+  
+  };
