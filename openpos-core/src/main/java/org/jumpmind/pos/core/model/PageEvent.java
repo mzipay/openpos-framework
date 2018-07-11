2d0dcb541414aaff43b3a2a2192b9b9eb9e271ba
Jason Mihalick <jason@jCraftsmen.com>
2018-07-11 09:50:36 -0400

    Add PageEvent class for receiving CatalogBrowser pagination info

---
+++ openpos-core/src/main/java/org/jumpmind/pos/core/model/PageEvent.java
@@ -0,0 +1,38 @@
+package org.jumpmind.pos.core.model;
+
+/**
+ * Corresponds to angular/material/PageEvent class and used
+ * for CatalogBrowser screen.
+ */
+public class PageEvent {
+    private Integer pageIndex;
+    private Integer previousPageIndex;
+    private Integer pageSize;
+    private Integer length;
+    
+    public Integer getPreviousPageIndex() {
+        return previousPageIndex;
+    }
+    public void setPreviousPageIndex(Integer previousPageIndex) {
+        this.previousPageIndex = previousPageIndex;
+    }
+    public Integer getPageIndex() {
+        return pageIndex;
+    }
+    public void setPageIndex(Integer pageIndex) {
+        this.pageIndex = pageIndex;
+    }
+    public Integer getPageSize() {
+        return pageSize;
+    }
+    public void setPageSize(Integer pageSize) {
+        this.pageSize = pageSize;
+    }
+    public Integer getLength() {
+        return length;
+    }
+    public void setLength(Integer length) {
+        this.length = length;
+    }
+    
+}
