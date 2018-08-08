d8828643c203758bdfaa7e529ff7de493c8546dd
Dan Kaste <dan.kaste@leadingedje.com>
2018-08-08 08:40:24 -0400

    export pos-screen.component

--- screens/index.ts
+++ screens/index.ts
@@ -37,5 +37,7 @@ export * from './wait/wait.component';
 export * from './warranty-coverage/warranty-coverage.component';
 export * from './catalog-browser/catalog-browser.component';
 
+export * from './pos-screen/pos-screen.component';
+
 export * from './templates';
 export * from './screens.module';
