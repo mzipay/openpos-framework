11122a2f3c8aee1d5f14dcbea4d262988d4a45c5
JJStarrett <jstarrett@jumpmind.com>
2018-08-14 14:42:33 -0400

    removed get Model

--- openpos-translate/src/main/java/org/jumpmind/pos/translate/ILegacyNavigationButtonBeanModel.java
+++ openpos-translate/src/main/java/org/jumpmind/pos/translate/ILegacyNavigationButtonBeanModel.java
@@ -3,5 +3,4 @@ package org.jumpmind.pos.translate;
 public interface ILegacyNavigationButtonBeanModel {
     ILegacyButtonSpec[] getModifyButtons();
     ILegacyButtonSpec[] getNewButtons();
-    Object getModel();
 }
