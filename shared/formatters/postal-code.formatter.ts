ac2c8c02c5ed007992273c9039488818125cecb7
Jason Mihalick <jason@jCraftsmen.com>
2018-08-27 12:27:52 -0400

    Don't allow bad chars to be pasted in postal code field
    Also addresses issue with allowing unlimited data to be pasted into
    field.
    
    
    SS-7510

--- shared/formatters/postal-code.formatter.ts
+++ shared/formatters/postal-code.formatter.ts
@@ -1,6 +1,7 @@
 import { IFormatter } from './formatter.interface';
 
 export class PostalCodeFormatter implements IFormatter {
+    static readonly CHAR_REGEX = /[^0-9a-zA-Z\-]/g;
     static readonly FILTER_REGEX = /^[0-9a-zA-Z\-]{1,20}$/;
 
     allowKey(key: string, newValue: string): boolean {
@@ -8,7 +9,13 @@ export class PostalCodeFormatter implements IFormatter {
     }
 
     formatValue(value: string): string {
-        return value;
+        // remove any invalid chars
+        let returnValue = value.replace(PostalCodeFormatter.CHAR_REGEX, '');
+        // trim to max len of 20
+        if (returnValue && returnValue.length > 20) {
+            returnValue = returnValue.substring(0, 20);
+        }
+        return returnValue;
     }
 
     unFormatValue(value: string): string {
