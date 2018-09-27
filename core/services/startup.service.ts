356d4286871227f9238e255e82a5dcdcdafd6596
Dan Kaste <dan.kaste@leadingedje.com>
2018-09-27 13:23:10 -0400

    Add hook to run a task on failure

--- core/services/startup.service.ts
+++ core/services/startup.service.ts
@@ -1,15 +1,16 @@
 import { Logger } from './logger.service';
 import { concat, throwError, Observable, Subject, ReplaySubject } from 'rxjs';
-import { Injectable, InjectionToken, Inject } from '@angular/core';
+import { Injectable, InjectionToken, Inject, Optional } from '@angular/core';
 import {
     IStartupTask
 } from '../interfaces';
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, ActivatedRoute } from '@angular/router';
 import { map, catchError } from 'rxjs/operators';
-import { MatDialog } from '@angular/material';
+import { MatDialog, MatDialogRef } from '@angular/material';
 import { ComponentType } from '@angular/cdk/overlay/index';
 
 export const STARTUP_TASKS = new InjectionToken<IStartupTask[]>('Startup Tasks');
+export const STARTUP_FAILED_TASK = new InjectionToken<IStartupTask>('Startup Failed Task');
 export const STARTUP_COMPONENT = new InjectionToken<ComponentType<any>>('Startup Component');
 export const STARTUP_FAILED_COMPONENT = new InjectionToken<ComponentType<any>>('Startup Failed Component');
 
@@ -22,8 +23,12 @@ export class StartupService implements CanActivate {
 
     public startupTaskMessages$ = new ReplaySubject<string>();
 
+    private allMessages = [];
+    private startupDialogRef: MatDialogRef<any>;
+
     constructor(
         @Inject(STARTUP_TASKS) tasks: Array<IStartupTask>,
+        @Optional() @Inject(STARTUP_FAILED_TASK) private failedTask: IStartupTask,
         @Inject(STARTUP_COMPONENT) private startupComponent: ComponentType<any>,
         @Inject(STARTUP_FAILED_COMPONENT) private startupFailedComponent: ComponentType<any>,
         private matDialog: MatDialog,
@@ -39,7 +44,7 @@ export class StartupService implements CanActivate {
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
 
-        const startupDialogRef = this.matDialog.open(
+        this.startupDialogRef = this.matDialog.open(
             this.startupComponent,
             {
                 disableClose: true,
@@ -54,38 +59,56 @@ export class StartupService implements CanActivate {
             map( message => `${task.name}: ${message}`),
             catchError( error => throwError(`${task.name}: ${error}`))) );
 
-        const allMessages = [];
 
         // Run all tasks in order
         return Observable.create( (result: Subject<boolean>) => {
             concat(...tasks).subscribe(
                 {
-                    next: (message) => {
-                        this.startupTaskMessages$.next(message);
-                        this.log.info(message);
-                        allMessages.push(message);
-                    },
+                    next: (message) => { this.handleMessage(message); },
                     error: (error) => {
-                        this.log.info(error);
                         result.next(false);
-                        startupDialogRef.close();
-                        this.matDialog.open(
-                            this.startupFailedComponent, {
-                                disableClose: true,
-                                hasBackdrop: false,
-                                data: {
-                                    error: error,
-                                    messages: allMessages
-                                }
-                            }).afterClosed().subscribe( () => location.reload() );
+                        this.handleError( error );
                     },
                     complete: () => {
                         this.log.info('Task completed');
                         result.next(true);
-                        startupDialogRef.close();
+                        this.startupDialogRef.close();
                     }
                 }
             );
         });
+    }
+
+    private handleMessage( message: string ) {
+        this.startupTaskMessages$.next(message);
+        this.log.info(message);
+        this.allMessages.push(message);
+    }
+
+    private handleError( error: string ) {
+        this.log.info(error);
+        if ( this.failedTask ) {
+            this.failedTask.execute().subscribe(
+                {
+                    next: (message) => this.handleMessage(message),
+                    complete: () => this.showFailure(error)
+                }
+            );
+        } else {
+            this.showFailure( error );
+        }
+    }
+
+    private showFailure( error: string ){
+        this.startupDialogRef.close();
+        this.matDialog.open(
+            this.startupFailedComponent, {
+                disableClose: true,
+                hasBackdrop: false,
+                data: {
+                    error: error,
+                    messages: this.allMessages
+                }
+            }).afterClosed().subscribe( () => location.reload() );
     }
 }
