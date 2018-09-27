73c3b473364413652b0c670586e13eb20648a068
Dan Kaste <dan.kaste@leadingedje.com>
2018-09-27 09:19:59 -0400

    update our 2 tests

--- core/services/session.service.spec.ts
+++ core/services/session.service.spec.ts
@@ -1,29 +1,39 @@
 import { TestBed } from '@angular/core/testing';
 import { SessionService } from './session.service';
 import { PersonalizationService } from './personalization.service';
-import { NgZone } from '@angular/core';
 import { MatDialog } from '@angular/material';
 import { StompRService } from '@stomp/ng2-stompjs';
+import { DeviceService } from './device.service';
+import { AppInjector } from '../app-injector';
+import { Injector } from '@angular/core';
 
 
 describe('SessionService', () => {
 
     let stompServiceSpy: jasmine.SpyObj<StompRService>;
     let sessionService: SessionService;
+    let deviceServiceSpy: jasmine.SpyObj<DeviceService>;
 
     beforeEach(() => {
         const stompSpy = jasmine.createSpyObj('StompRService', ['publish']);
         const personalizationSpy = jasmine.createSpyObj('PersonalizationService', ['getNodeId']);
         const matDialogSpy = jasmine.createSpyObj('MatDialog', ['open']);
+        const deviceSpy = jasmine.createSpyObj('DeviceService', ['isRunningInCordova']);
 
         TestBed.configureTestingModule({
             providers: [
                 { provide: PersonalizationService, useValue: personalizationSpy },
                 SessionService,
                 { provide: MatDialog, useValue: matDialogSpy },
-                {provide: StompRService, useValue: stompSpy }
+                { provide: StompRService, useValue: stompSpy },
+                { provide: DeviceService, useValue: deviceSpy}
             ]
         });
+        AppInjector.Instance = TestBed.get(Injector);
+
+
+        deviceServiceSpy = TestBed.get(DeviceService);
+        deviceServiceSpy.isRunningInCordova.and.returnValue(false);
 
         stompServiceSpy = TestBed.get(StompRService);
         sessionService = TestBed.get(SessionService);
