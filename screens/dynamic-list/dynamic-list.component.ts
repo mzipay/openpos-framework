574d77e8828650c53534d2ff7fa7115c86eec20e
Joe Brownlee <brownlee@wowway.com>
2018-10-17 10:30:27 -0400

    Add the DynamicList component.

---
+++ screens/dynamic-list/dynamic-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { PosScreen } from '../pos-screen/pos-screen.component';
+
+
+@Component({
+  selector: 'app-dynamic-list',
+  templateUrl: './dynamic-list.component.html'
+})
+export class DynamicListComponent extends PosScreen<any> {
+
+  buildScreen() {}
+
+}
