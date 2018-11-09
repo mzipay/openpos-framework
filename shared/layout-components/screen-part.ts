5ed290a9abc2548708f5080e02282f0a2f7fe688
Dan Kaste <dan.kaste@leadingedje.com>
2018-11-08 21:29:23 -0500

    add a helper

--- shared/layout-components/screen-part.ts
+++ shared/layout-components/screen-part.ts
@@ -1,6 +1,6 @@
 import { SessionService, AppInjector, IMenuItem } from '../../core';
 import { OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Subscription, Observable } from 'rxjs';
 import { filter } from 'rxjs/operators';
 export abstract class ScreenPart<T> implements OnDestroy {
 
@@ -24,6 +24,10 @@ export abstract class ScreenPart<T> implements OnDestroy {
 
     onMenuItemClick( menuItem: IMenuItem) {
         this.sessionService.onAction( menuItem );
+    }
+
+    isActionDisabled( action: string): Observable<boolean>{
+        return this.sessionService.actionIsDisabled(action);
     }
 
     abstract screenDataUpdated();
