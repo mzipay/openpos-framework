965af02215dac9ee037995b856d78fa52fc90018
Steve Carley <steven.carley@leadingedje.com>
2018-12-28 11:51:28 -0500

    Fixing issue where page state not updating when content changes. Paging controls no longer show when totalPages is less than 2.

--- shared/components/pager/pager.component.ts
+++ shared/components/pager/pager.component.ts
@@ -1,4 +1,5 @@
-import { Component, ViewChildren, TemplateRef, QueryList, ContentChildren, Input, AfterContentInit } from "@angular/core";
+import { Component, TemplateRef, QueryList, ContentChildren, Input, AfterContentInit, OnDestroy } from "@angular/core";
+import { Subscription } from "rxjs";
 
 @Component({
     selector: 'app-pager',
@@ -6,15 +7,33 @@ import { Component, ViewChildren, TemplateRef, QueryList, ContentChildren, Input
     styleUrls: ['./pager.component.scss']
 })
 
-export class PagerComponent implements AfterContentInit {
+export class PagerComponent implements AfterContentInit, OnDestroy {
     @ContentChildren("pagerItem") sections: QueryList<TemplateRef<any>>;
 
     @Input() pageSize: number = 5;
     currentPage: number = 1;
     totalPages: number;
     currentIndex: number = 0;
+    private subscription: Subscription;
 
     ngAfterContentInit(): void {
+        this._resetPageState();
+        if (this.sections) {
+            this.subscription = this.sections.changes.subscribe( () => {
+                this._resetPageState()
+            });
+        }
+    }
+
+    ngOnDestroy(): void {
+        if (this.subscription) {
+            this.subscription.unsubscribe();
+        }
+    }
+
+    private _resetPageState(): void {
+        this.currentPage = 1;
+        this.currentIndex = 0;
         this.totalPages = Math.ceil(this.sections.length / this.pageSize);
     }
 
