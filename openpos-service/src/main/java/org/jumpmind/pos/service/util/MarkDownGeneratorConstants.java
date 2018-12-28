8c0c2f97a28e91505d7b2eb1768a6d62e790609d
Chris Henson <chenson42@gmail.com>
2018-12-28 08:51:52 -0500

    updated markdown generator

---
+++ openpos-service/src/main/java/org/jumpmind/pos/service/util/MarkDownGeneratorConstants.java
@@ -0,0 +1,22 @@
+package org.jumpmind.pos.service.util;
+
+public class MarkDownGeneratorConstants {
+
+	public static final String SERVICES_HEADING = "# Services";
+	public static final String OPERATION_HEADING = "### ";
+	public static final String REQUEST_EXAMPLE_HEADING = "###### Request Example";
+	public static final String RESPONSE_EXAMPLE_HEADING = "###### Response Example";
+	public static final String MODELS_HEADING = "# Models";
+	
+	public static final String MODEL_HEADING_START = "## ";
+	public static final String SERVICE_NAME_HEADING_START = "## ";
+	
+	public static final String OPERATION_TABLE_HEADING = "| Path | HTTP | Request Body | ResponseBody | Description |";
+	public static final String OPERATION_TABLE_DIVIDER = "| --- | --- | --- | --- | --- |";
+	
+	public static final String MODEL_TABLE_HEADING = "| Property | Type | Description |";
+	public static final String MODEL_TABLE_DIVIDER = "| --- | --- | --- |";
+	
+	public static final String NO_VALUE = "NONE";
+	
+}
