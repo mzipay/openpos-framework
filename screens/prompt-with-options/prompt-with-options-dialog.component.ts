90231a3720abe992e046cd74942102c4c240ce0c
stojankz <kstojanovska@jumpmind.com>
2019-01-16 14:01:47 -0500

    add dialog component for prompt with options screen
    add dialog component for prompt with options screen

---
+++ screens/prompt-with-options/prompt-with-options-dialog.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { PromptWithOptionsComponent } from '..';
+
+
+@Component({
+    templateUrl: './prompt-with-options-dialog.component.html'
+})
+export class PromptWithOptionsDialogComponent extends PromptWithOptionsComponent {
+}
