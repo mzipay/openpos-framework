7d3e264729627a7ec3ec8a76400ac97dfa42e7b1
Mark Michalek <mmichalek@jumpmind.com>
2019-01-18 14:36:31 -0500

    Try to work around compiler complaint

--- openpos-util/src/main/groovy/org/jumpmind/pos/util/MoneyExtensions.groovy
+++ openpos-util/src/main/groovy/org/jumpmind/pos/util/MoneyExtensions.groovy
@@ -7,21 +7,20 @@ import com.fasterxml.jackson.databind.JsonSerializer
 import com.fasterxml.jackson.databind.SerializerProvider
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer
 import com.fasterxml.jackson.databind.ser.std.StdSerializer
-import javax.annotation.PostConstruct
 import org.joda.money.CurrencyUnit
 import org.joda.money.Money
+import org.springframework.beans.factory.InitializingBean
 import org.springframework.stereotype.Component
 
 @Component
-class MoneyExtentions {
+class MoneyExtentions implements InitializingBean {
   
     static def $(amount) {
         CurrencyUnit usd = CurrencyUnit.of("USD");
         org.joda.money.Money.of(usd, amount);
     }
     
-    @PostConstruct
-    initExtensions() {
+    void afterPropertiesSet() {
         Money.metaClass.multiply { int value ->
             multipliedBy(value);
         }
