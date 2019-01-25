bee8c761c7b6d001874bd0c1f98a46476e83328e
Dan Kaste <dan.kaste@leadingedje.com>
2019-01-25 08:44:20 -0500

    make sure we unlisten

--- shared/directives/inactivity-monitor.directive.ts
+++ shared/directives/inactivity-monitor.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, HostListener, Input, Renderer2, ElementRef } from '@angular/core';
+import { Directive, HostListener, Input, Renderer2, ElementRef, OnDestroy } from '@angular/core';
 import { Configuration } from '../../configuration/configuration';
 import { SessionService } from '../../core';
 
@@ -7,22 +7,29 @@ import { SessionService } from '../../core';
     // tslint:disable-next-line:directive-selector
     selector: '[inactivityMonitor]'
 })
-export class InactivityMonitorDirective {
+export class InactivityMonitorDirective implements OnDestroy {
 
     static lastKeepAliveFlushTime: number = new Date().getTime();
-
-    constructor(private elRef: ElementRef, public renderer: Renderer2, private session: SessionService) {
-        if (Configuration.useTouchListener) {
-            this.renderer.listen(elRef.nativeElement, 'touchstart', (event) => {
-                this.touchEvent(event);
-            });
-        }
-    }
 
     @Input() keepAliveMillis = Configuration.keepAliveMillis;
 
     private timerHandle: any;
     private _timeoutActive;
+
+    private unlisten = () => {};
+
+    constructor(private elRef: ElementRef, public renderer: Renderer2, private session: SessionService) {
+        if (Configuration.useTouchListener) {
+            this.unlisten = this.renderer.listen(elRef.nativeElement, 'touchstart', (event) => {
+                this.touchEvent(event);
+            });
+        }
+    }
+
+    ngOnDestroy(): void {
+        this.unlisten();
+    }
+
 
     @HostListener('window:keydown', ['$event'])
     keyEvent(event: KeyboardEvent) {
