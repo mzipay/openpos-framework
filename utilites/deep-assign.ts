39d89ffc5a2a45edbbf719408af574d7d74dfb51
Steve Carley <steven.carley@leadingedje.com>
2019-01-25 09:31:19 -0500

    Changing deepAssign to create new objects and arrays when missing prop on target

--- utilites/deep-assign.ts
+++ utilites/deep-assign.ts
@@ -2,17 +2,28 @@
 // This method assumes you do not have any cyclic references. For the case we plan on using it
 // there should be no issues with that since it currently is only being used to copy the JSON
 // screen objects into their local versions with out triggering a reference change
-export function deepAssign( target, src ) {
+export function deepAssign( target : any, src : any) : any {
 
     // if target is undefined or null we'll break out and return src
-    // if src is null or undefined or primative return the value
-    if ( target === undefined || target === null || src === undefined || src === null ||
-        typeof src === 'string' || typeof src === 'number' || typeof src === 'boolean') {
+    if ( target === null || typeof target === 'undefined' ) {
+        return _doCopy(src);
+    }
+
+    // if src is not an object or array return it.
+    if ( _isPrimitive(src) ) {
         return src;
     }
 
     // if we are an object we'll iterater our keys, copy our values and return target
-    if ( typeof src === 'object') {
+    if ( typeof src === 'object' ) {
+        if ( Array.isArray(src) ) {
+            if ( Array.isArray(target) ) {
+                (target as Array<any>).length = 0;
+            }
+            else {
+                target = [];
+            }
+        }
         Object.getOwnPropertyNames(src).forEach( prop => {
             target[prop] = deepAssign( target[prop], src[prop]);
         });
@@ -24,4 +35,33 @@ export function deepAssign( target, src ) {
         });
         return target;
     }
+}
+
+//Returns the value if primitive, or will create a array or object and copy all of the values. 
+function _doCopy( src:any ) : any {
+    if ( _isPrimitive(src) ) {
+        return src;
+    }
+    if ( Array.isArray(src) ) {
+        const arr = [];
+        (src as Array<any>).forEach( item => {
+            arr.push(_doCopy(item));
+        });
+        return arr;
+    }
+    if ( typeof src == 'object' ) {
+        const obj = {};
+        Object.getOwnPropertyNames(src).forEach( prop => {
+            obj[prop] = _doCopy(src[prop]);
+        });
+        return obj;
+    }
+    else {
+        return undefined;
+    }
+}
+
+//Returns true if src is null, undefiend, string, number or boolean.
+function _isPrimitive( src:any ) : boolean {
+    return src === null || typeof src === 'undefined' || typeof src === 'string' || typeof src === 'number' || typeof src === 'boolean';
 }
