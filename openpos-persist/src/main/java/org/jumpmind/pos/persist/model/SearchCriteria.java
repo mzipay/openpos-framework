fae74aae531a4dedb8b6ed4ac70275238bf1d24e
Chris Henson <chenson42@gmail.com>
2019-01-29 14:26:59 -0500

    remove phone logic from the search criteria

--- openpos-persist/src/main/java/org/jumpmind/pos/persist/model/SearchCriteria.java
+++ openpos-persist/src/main/java/org/jumpmind/pos/persist/model/SearchCriteria.java
@@ -10,7 +10,7 @@ public class SearchCriteria {
     Map<String, Object> criteria;
 
     Class<? extends AbstractModel> entityClass;
-    
+
     boolean useAnd = true;
 
     public SearchCriteria() {
@@ -39,36 +39,7 @@ public class SearchCriteria {
             this.criteria = new HashMap<String, Object>();
         }
 
-        if (field.equals("phone") && value instanceof String) {
-            String[] parsed = parsePhone((String) value);
-            if (parsed != null) {
-                this.criteria.put("areaCode", parsed[0]);
-                this.criteria.put("phoneNumber", parsed[1]);
-            }
-        } else {
-            this.criteria.put(field, value);
-        }
-    }
-
-    public static String[] parsePhone(String phoneNumber) {
-        String[] parsed = new String[2];
-        if (phoneNumber != null) {
-            phoneNumber.replaceAll("\\(", "");
-            phoneNumber.replaceAll("\\)", "");
-
-            String[] phoneParts = phoneNumber.split("-");
-            if (phoneNumber.length() == 10) {
-                parsed[0] = phoneNumber.substring(0, 3);
-                parsed[1] = phoneNumber.substring(3, 6) + "-" + phoneNumber.substring(6);
-            } else if (phoneParts.length > 2) {
-                parsed[0] = phoneParts[0];
-                parsed[1] = phoneParts[1] + " - " + phoneParts[2];
-            } else if (phoneParts.length > 1) {
-                parsed[1] = phoneParts[0] + " - " + phoneParts[1];
-            }
-            return parsed;
-        }
-        return null;
+        this.criteria.put(field, value);
     }
 
     public Class<? extends AbstractModel> getEntityClass() {
@@ -78,11 +49,11 @@ public class SearchCriteria {
     public void setEntityClass(Class<? extends AbstractModel> entityClass) {
         this.entityClass = entityClass;
     }
-    
+
     public void setUseAnd(boolean and) {
         this.useAnd = and;
     }
-    
+
     public boolean isUseAnd() {
         return useAnd;
     }
