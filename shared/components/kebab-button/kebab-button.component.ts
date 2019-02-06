e9525927968cb030206da768d73b3ed3849a9e69
maxwellpettit <maxwell.pettit@gmail.com>
2019-02-06 11:05:07 -0500

    Add color to kebab button

--- shared/components/kebab-button/kebab-button.component.ts
+++ shared/components/kebab-button/kebab-button.component.ts
@@ -4,35 +4,38 @@ import { MatDialog } from '@angular/material';
 import { KebabMenuComponent } from '../kebab-menu/kebab-menu.component';
 
 @Component({
-  selector: 'app-kebab-button',
-  templateUrl: './kebab-button.component.html',
-  styleUrls: ['./kebab-button.component.scss']
+    selector: 'app-kebab-button',
+    templateUrl: './kebab-button.component.html',
+    styleUrls: ['./kebab-button.component.scss']
 })
 export class KebabButtonComponent {
 
     @Input()
     menuItems: IMenuItem[];
 
+    @Input()
+    color?: string;
+
     @Output()
     menuItemClick = new EventEmitter<IMenuItem>();
 
-    constructor( private dialog: MatDialog, private log: Logger) {
+    constructor(private dialog: MatDialog, private log: Logger) {
     }
 
     public openKebabMenu() {
         const dialogRef = this.dialog.open(KebabMenuComponent, {
-          data: {
-            menuItems: this.menuItems,
-            payload: null,
-            disableClose: false,
-            autoFocus: false
-         }
+            data: {
+                menuItems: this.menuItems,
+                payload: null,
+                disableClose: false,
+                autoFocus: false
+            }
         });
 
         dialogRef.afterClosed().subscribe(result => {
-            if ( result ) {
+            if (result) {
                 this.menuItemClick.emit(result);
             }
         });
-      }
+    }
 }
