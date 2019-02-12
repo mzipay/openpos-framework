210dd14da34ed654fe49caa441942ac4fc7b503e
Dan Kaste <dan.kaste@leadingedje.com>
2019-02-12 15:26:49 -0500

    Update bacon strip Add decluttering for mobile Switch to CSS Grid

--- shared/screen-parts/bacon-strip/bacon-strip.component.ts
+++ shared/screen-parts/bacon-strip/bacon-strip.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { BaconStripInterface } from './bacon-strip.interface';
 import { ScreenPart, ScreenPartData } from '../screen-part';
+import { OpenposMediaService } from '../../../core';
+import { Observable } from 'rxjs';
 
 @ScreenPartData({name: 'baconStrip'})
 @Component({
@@ -9,6 +11,20 @@ import { ScreenPart, ScreenPartData } from '../screen-part';
     styleUrls: ['./bacon-strip.component.scss']
 })
 export class BaconStripComponent extends ScreenPart<BaconStripInterface> {
+
+    isMobile$: Observable<boolean>;
+
+    constructor( mediaService: OpenposMediaService) {
+        super();
+        const mobileMap = new Map([
+            ['xs', true],
+            ['sm', false],
+            ['md', false],
+            ['lg', false],
+            ['xl', false]
+        ]);
+        this.isMobile$ = mediaService.mediaObservableFromMap(mobileMap);
+    }
 
     screenDataUpdated() {
     }
