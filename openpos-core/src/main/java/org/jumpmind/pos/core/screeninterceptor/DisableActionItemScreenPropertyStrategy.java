c56063439527b0934e7a9b0d02dc00e0c5698191
Dan Kaste <dan.kaste@leadingedje.com>
2019-02-14 15:02:23 -0500

    added a screen interceptor that crawls the screen and runs strategies on them

---
+++ openpos-core/src/main/java/org/jumpmind/pos/core/screeninterceptor/DisableActionItemScreenPropertyStrategy.java
@@ -0,0 +1,24 @@
+package org.jumpmind.pos.core.screeninterceptor;
+
+import org.jumpmind.pos.core.screen.MenuItem;
+import org.jumpmind.pos.core.service.IDisableActionItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DisableActionItemScreenPropertyStrategy implements IScreenPropertyStrategy {
+
+	@Autowired
+	IDisableActionItemService disableActionItemService;
+	
+	@Override
+	public Object doStrategy(String appId, String deviceId, Object property, Class<?> clazz) {
+		if(property != null && MenuItem.class.equals(clazz)) {
+			MenuItem item = (MenuItem)property;
+			item.setEnabled(!disableActionItemService.isActionDisabled(appId, deviceId, item.getAction()));
+			return item;
+		}
+		
+		return property;
+	}
+}
---
+++ openpos-core/src/main/java/org/jumpmind/pos/core/screeninterceptor/ResourceLookupStringBuilder.java
@@ -0,0 +1,80 @@
+package org.jumpmind.pos.core.screeninterceptor;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.jumpmind.pos.util.DefaultObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+public class ResourceLookupStringBuilder implements Serializable{
+
+	Logger logger = LoggerFactory.getLogger(getClass());
+	
+	private static final long serialVersionUID = 1L;
+	private String key;
+	private String group;
+	private Map<String, String> parameters;
+	 
+	public ResourceLookupStringBuilder() {
+	}
+	
+	public ResourceLookupStringBuilder( String key ){
+		setKey(key);
+	}
+	
+	public ResourceLookupStringBuilder( String key, String group){
+		setKey(key);
+		setGroup(group);
+	}
+	 
+	public String getKey() {
+		return key;
+	}
+	public void setKey(String key) {
+		this.key = key;
+	}
+	public String getGroup() {
+		return group;
+	}
+	public void setGroup(String group) {
+		this.group = group;
+	}
+	public Map<String, String> getParameters() {
+		return parameters;
+	}
+	public void setParameters(Map<String, String> parameters) {
+		this.parameters = parameters;
+	}
+	public ResourceLookupStringBuilder addParameter(String name, String value) {
+		if( parameters == null) {
+			parameters = new HashMap<String, String>();
+		}
+		parameters.put(name, value);
+		
+		return this;
+	}
+	
+	public String toJson() {
+		try {
+			return DefaultObjectMapper.build().writeValueAsString(this);
+		} catch (JsonProcessingException e) {
+			logger.error("Failed to convert resource string to json", e);
+		}
+		
+		return null;
+	}
+	
+	public static ResourceLookupStringBuilder fromJson( String value ) {
+		try {
+			return DefaultObjectMapper.build().readValue(value, ResourceLookupStringBuilder.class);
+		} catch (IOException e) {
+			LoggerFactory.getLogger(ResourceLookupStringBuilder.class).error("Failed to convert json to resource object");
+		}
+		return null;
+	}
+}
