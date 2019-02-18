bfe1aa15315829aca97475d76866ffc23d27b1e6
Chris Henson <chenson42@gmail.com>
2019-02-18 07:49:13 -0500

    allow personalization via query params

--- core/components/startup/personalization-startup-task.ts
+++ core/components/startup/personalization-startup-task.ts
@@ -5,6 +5,8 @@ import { MatDialog } from '@angular/material';
 import { PersonalizationComponent } from '../personalization/personalization.component';
 import { StartupTaskNames } from './startup-task-names';
 import { Injectable } from '@angular/core';
+import { StartupTaskData } from './startup-task-data';
+import { Params } from '@angular/router';
 
 @Injectable({
     providedIn: 'root',
@@ -19,8 +21,9 @@ export class PersonalizationStartupTask implements IStartupTask {
 
     }
 
-    execute(): Observable<string> {
-        return Observable.create( (message: Subject<string>) => {
+    execute(data: StartupTaskData): Observable<string> {
+        return Observable.create((message: Subject<string>) => {
+            this.personalizeFromQueueParams(data.route.queryParams, message);
             message.next('Checking if device is personalized.');
             if (!this.personalization.isPersonalized()) {
                 message.next('Launching personalization screen.');
@@ -30,14 +33,30 @@ export class PersonalizationStartupTask implements IStartupTask {
                         hasBackdrop: false,
                         panelClass: 'openpos-default-theme'
                     }
-                    ).afterClosed().subscribe( () => {
-                        message.complete();
-                     } );
-            }  else  {
+                ).afterClosed().subscribe(() => {
+                    message.complete();
+                });
+            } else {
                 message.next('Device is already personalized.');
                 message.complete();
             }
         });
+    }
+
+    personalizeFromQueueParams(queryParams: Params, message: Subject<string>) {
+        const deviceId = queryParams.deviceId;
+        const serverName = queryParams.serverName;
+        let serverPort = queryParams.serverPort;
+        let sslEnabled = queryParams.sslEnabled;
+        const deviceType = queryParams.deviceType;
+        const brand = queryParams.brand;
+
+        if (deviceId && serverName) {
+            message.next('Personalizing from query params');
+            serverPort = !serverPort ? 6140 : serverPort;
+            sslEnabled = !sslEnabled ? false : sslEnabled;
+            this.personalization.personalize(serverName, serverPort, deviceId, deviceType, brand);
+        }
     }
 
 
