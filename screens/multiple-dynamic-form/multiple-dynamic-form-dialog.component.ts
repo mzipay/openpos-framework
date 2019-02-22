931819a00794f9b87e8caad0d138f5c8bd713307
EDenlinger <elliott.denlinger@gmail.com>
2019-02-22 11:59:36 -0500

    Adding better mobile view and functionality for the MultipleDynamicForms. Swaps from tabs to a vertical option list with navigation to/from their respective forms (#16)

--- screens/multiple-dynamic-form/multiple-dynamic-form-dialog.component.ts
+++ screens/multiple-dynamic-form/multiple-dynamic-form-dialog.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { MultipleDynamicFormComponent } from '../../screens/multiple-dynamic-form/multiple-dynamic-form.component';
+import { OpenposMediaService } from '../../core';
+import { Observable } from 'rxjs';
+import { IMultipleFormOption } from './multiple-dynamic-form-screen.interface';
 
 @Component({
     selector: 'app-multiple-dynamic-form-dialog',
@@ -7,4 +10,30 @@ import { MultipleDynamicFormComponent } from '../../screens/multiple-dynamic-for
 })
 export class MultipleDynamicFormDialogComponent extends MultipleDynamicFormComponent {
     current = 0;
+
+
+    public isMobile: Observable<boolean>;
+    public showOptions = true;
+    public selectedOption: IMultipleFormOption;
+
+    constructor( private mediaService: OpenposMediaService ) {
+        super();
+        const sizeMap = new Map([
+            ['xs', true],
+            ['sm', false],
+            ['md', false],
+            ['lg', false],
+            ['xl', false]
+        ]);
+        this.isMobile = this.mediaService.mediaObservableFromMap(sizeMap);
+    }
+
+      onMakeOptionSelection( formOption: IMultipleFormOption): void {
+          this.selectedOption = formOption;
+          this.showOptions = false;
+      }
+    
+      onBackButtonPressed(): void {
+       this.showOptions = true;
+      }
 }
