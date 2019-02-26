9a9c41e887d82d7e3350de0371372069b9c3ad7d
Mark Michalek <mmichalek@jumpmind.com>
2019-02-26 15:36:21 -0500

    Key binding support for options dialog

--- openpos-core/src/main/java/org/jumpmind/pos/core/screeninterceptor/KeyMappingScreenPropertyStrategy.java
+++ openpos-core/src/main/java/org/jumpmind/pos/core/screeninterceptor/KeyMappingScreenPropertyStrategy.java
@@ -4,7 +4,9 @@ import org.jumpmind.pos.core.screen.ActionItem;
 import org.jumpmind.pos.core.screen.Screen;
 import org.jumpmind.pos.core.service.IKeyMappingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class KeyMappingScreenPropertyStrategy implements IScreenPropertyStrategy {
 
 	@Autowired
@@ -12,7 +14,8 @@ public class KeyMappingScreenPropertyStrategy implements IScreenPropertyStrategy
 	
 	@Override
 	public Object doStrategy(String appId, String deviceId, Object property, Class<?> clazz, Screen screen) {
-		if(property != null && ActionItem.class.equals(clazz)) {
+		if(property != null 
+		        && ActionItem.class.isAssignableFrom(clazz)) {
 			ActionItem item = (ActionItem)property;
 			item.setKeybind(keyMappingService.getKeyMapping(screen.getId(), item.getAction()));
 			return item;
