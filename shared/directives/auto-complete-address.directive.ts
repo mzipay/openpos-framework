b8460e0db0fba9760c9064579499c1d9db2bd575
maxwellpettit <maxwell.pettit@gmail.com>
2019-02-26 15:30:15 -0500

    Get formatted address in auto complete directive

--- shared/directives/auto-complete-address.directive.ts
+++ shared/directives/auto-complete-address.directive.ts
@@ -1,5 +1,5 @@
 import { Directive, ElementRef, OnInit, Output, EventEmitter } from '@angular/core';
-// import { MapsAPILoader } from '@agm/core';
+import { MapsAPILoader } from '@agm/core';
 
 declare var google: any;
 
@@ -8,8 +8,8 @@ declare var google: any;
     // tslint:disable-next-line:directive-selector
     selector: '[autoCompleteAddress]'
 })
-export class AutoCompleteAddressDirective { // implements OnInit {
-/*
+export class AutoCompleteAddressDirective implements OnInit {
+
     @Output() onSelect: EventEmitter<any> = new EventEmitter();
 
     private element: HTMLInputElement;
@@ -19,29 +19,35 @@ export class AutoCompleteAddressDirective { // implements OnInit {
         this.element = elRef.nativeElement;
     }
 
-    getFormattedAddress(place) {
-
-        const location_obj = {};
-        for (const i of place.address_components) {
-            const item = place.address_components[i];
-
-            location_obj['formatted_address'] = place.formatted_address;
-            if (item['types'].indexOf('locality') > -1) {
-                location_obj['locality'] = item['long_name'];
-            } else if (item['types'].indexOf('administrative_area_level_1') > -1) {
-                location_obj['admin_area_l1'] = item['short_name'];
-            } else if (item['types'].indexOf('street_number') > -1) {
-                location_obj['street_number'] = item['short_name'];
-            } else if (item['types'].indexOf('route') > -1) {
-                location_obj['route'] = item['long_name'];
-            } else if (item['types'].indexOf('country') > -1) {
-                location_obj['country'] = item['long_name'];
-            } else if (item['types'].indexOf('postal_code') > -1) {
-                location_obj['postal_code'] = item['short_name'];
+    getFormattedAddress(place: any) {
+        const location = {};
+        for (const item of place.address_components) {
+            // Formatted Address
+            location['formatted_address'] = place.formatted_address;
+            if (item.types) {
+                if (item.types.indexOf('locality') > -1) {
+                    // City / Locality
+                    location['locality'] = item['long_name'];
+                } else if (item.types.indexOf('administrative_area_level_1') > -1) {
+                    // State
+                    location['state'] = item['long_name'];
+                } else if (item.types.indexOf('street_number') > -1) {
+                    // Street Number
+                    location['street_number'] = item['short_name'];
+                } else if (item.types.indexOf('route') > -1) {
+                    // Street Name / Route
+                    location['street_name'] = item['short_name'];
+                } else if (item.types.indexOf('country') > -1) {
+                    // Country
+                    location['country'] = item['long_name'];
+                } else if (item.types.indexOf('postal_code') > -1) {
+                    // Postal Code
+                    location['postal_code'] = item['short_name'];
+                }
             }
 
         }
-        return location_obj;
+        return location;
     }
 
     ngOnInit() {
@@ -49,18 +55,20 @@ export class AutoCompleteAddressDirective { // implements OnInit {
             .load()
             .then(() => {
                 // Restrict search predictions to geographical locations
-                const autocomplete = new google.maps.places.Autocomplete(this.element, { types: ['geocode'] });
+                const autocomplete = new google.maps.places.Autocomplete(this.element, { types: ['address'] });
 
                 // Restrict set of place fields returned
-                autocomplete.setFields('address_components');
+                autocomplete.setFields(['formatted_address', 'address_components']);
 
                 // Event listener to monitor place changes in the input
                 google.maps.event.addListener(autocomplete, 'place_changed', () => {
                     // Emit the new address object for the updated place
-                    this.onSelect.emit(this.getFormattedAddress(autocomplete.getPlace()));
+                    const place = autocomplete.getPlace();
+                    const location = this.getFormattedAddress(place);
+                    this.onSelect.emit(location);
                 });
             })
             .catch((err) => console.log(err));
     }
-*/
+
 }
