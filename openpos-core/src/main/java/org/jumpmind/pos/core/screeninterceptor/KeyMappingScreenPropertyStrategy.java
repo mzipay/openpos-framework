d75cb9539dce63b45ffc3aa4cd60a147a509300d
Dan Kaste <dan.kaste@leadingedje.com>
2019-02-27 07:47:02 -0500

    We can't make this a component because then it forces everyone to have it. The intention is to allow people to opt in by adding it to AppConfig.java

--- openpos-core/src/main/java/org/jumpmind/pos/core/screeninterceptor/KeyMappingScreenPropertyStrategy.java
+++ openpos-core/src/main/java/org/jumpmind/pos/core/screeninterceptor/KeyMappingScreenPropertyStrategy.java
@@ -6,7 +6,6 @@ import org.jumpmind.pos.core.service.IKeyMappingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component
 public class KeyMappingScreenPropertyStrategy implements IScreenPropertyStrategy {
 
 	@Autowired
