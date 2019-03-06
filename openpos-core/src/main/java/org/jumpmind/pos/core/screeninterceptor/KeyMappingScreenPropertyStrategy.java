00572bdd2528243f8cddaa5e8cbc32eefb2e4d92
Mark Michalek <mmichalek@jumpmind.com>
2019-03-06 14:46:09 -0500

    Add screenContext so individual strategies can keep track of their overall work on a screen

--- openpos-core/src/main/java/org/jumpmind/pos/core/screeninterceptor/KeyMappingScreenPropertyStrategy.java
+++ openpos-core/src/main/java/org/jumpmind/pos/core/screeninterceptor/KeyMappingScreenPropertyStrategy.java
@@ -1,11 +1,12 @@
 package org.jumpmind.pos.core.screeninterceptor;
 
+import java.util.Map;
+
 import org.apache.commons.lang3.StringUtils;
 import org.jumpmind.pos.core.screen.ActionItem;
 import org.jumpmind.pos.core.screen.Screen;
 import org.jumpmind.pos.core.service.IKeyMappingService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
 public class KeyMappingScreenPropertyStrategy implements IScreenPropertyStrategy {
 
@@ -13,11 +14,11 @@ public class KeyMappingScreenPropertyStrategy implements IScreenPropertyStrategy
 	IKeyMappingService keyMappingService;
 	
 	@Override
-	public Object doStrategy(String appId, String deviceId, Object property, Class<?> clazz, Screen screen) {
-		if(property != null 
+	public Object doStrategy(String appId, String deviceId, Object property, Class<?> clazz, Screen screen, Map<String, Object> screenContext) {
+		if (property != null 
 		        && ActionItem.class.isAssignableFrom(clazz)) {
 			ActionItem item = (ActionItem)property;
-			String keyMapping = keyMappingService.getKeyMapping(screen.getId(), item.getAction());
+			String keyMapping = keyMappingService.getKeyMapping(screen, item.getAction(), screenContext);
 			if (!StringUtils.isEmpty(keyMapping)) {			    
 			    item.setKeybind(keyMapping);
 			}
