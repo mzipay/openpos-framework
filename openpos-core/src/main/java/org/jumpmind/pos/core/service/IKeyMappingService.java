86fab5ea46fb12a719a3883981cd53494c6c3538
Mark Michalek <mmichalek@jumpmind.com>
2019-03-06 14:46:01 -0500

    Add screenContext so individual strategies can keep track of their overall work on a screen

--- openpos-core/src/main/java/org/jumpmind/pos/core/service/IKeyMappingService.java
+++ openpos-core/src/main/java/org/jumpmind/pos/core/service/IKeyMappingService.java
@@ -1,6 +1,11 @@
 package org.jumpmind.pos.core.service;
 
+import java.util.Map;
+
+import org.jumpmind.pos.core.screen.Screen;
+
 public interface IKeyMappingService {
-	String getKeyMapping( String screenId, String actionName);
+	
+    String getKeyMapping(Screen screen, String actionName, Map<String, Object> screenContext);
 
 }
