9a764bae72872c65206b71b80acd68de5945df0c
Dan Kaste <dan.kaste@leadingedje.com>
2019-03-08 17:11:06 -0500

    add handling to kabab for using arrow keys through menu selection

--- shared/components/kebab-menu/kebab-menu.component.ts
+++ shared/components/kebab-menu/kebab-menu.component.ts
@@ -1,14 +1,49 @@
-import { Component, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material';
+import { Component, Inject, ViewChildren, QueryList, ElementRef, Renderer2, OnDestroy } from '@angular/core';
+import { MAT_DIALOG_DATA, MatButton } from '@angular/material';
+import { KeyPressProvider } from '../../providers/keypress.provider';
+import { Subscribable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-kebab-menu',
   templateUrl: './kebab-menu.component.html',
   styleUrls: ['./kebab-menu.component.scss']
 })
-export class KebabMenuComponent {
+export class KebabMenuComponent implements OnDestroy {
 
-    constructor( @Inject(MAT_DIALOG_DATA) public data: any) {
+    @ViewChildren(MatButton, {read: ElementRef})
+    buttons: QueryList<ElementRef>;
+
+    private _subscription: Subscription;
+
+    constructor( @Inject(MAT_DIALOG_DATA) public data: any, keyPresses: KeyPressProvider) {
+        this._subscription = keyPresses.subscribe( 'ArrowUp', 1, (event: KeyboardEvent) => {
+
+            if ( event.repeat || event.type !== 'keydown' ) {
+                return;
+            }
+            const index = this.buttons.toArray().map(v => v.nativeElement).indexOf(document.activeElement);
+            if ( index > 0) {
+                console.log(event);
+                this.buttons.toArray()[index - 1].nativeElement.focus();
+            }
+        });
+
+        this._subscription.add( keyPresses.subscribe( 'ArrowDown', 1, (event: KeyboardEvent) => {
+            if ( event.repeat || event.type !== 'keydown' ) {
+                return;
+            }
+            const index = this.buttons.toArray().map(v => v.nativeElement).indexOf(document.activeElement);
+            if ( index + 1 < this.buttons.length) {
+                console.log(event);
+                this.buttons.toArray()[index + 1].nativeElement.focus();
+            }
+        }));
+    }
+
+    ngOnDestroy(): void {
+        if (this._subscription) {
+            this._subscription.unsubscribe();
+        }
     }
 
 }
