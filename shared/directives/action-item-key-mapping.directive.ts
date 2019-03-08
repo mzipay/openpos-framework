e4ab2ec205f557adeee93b1e925a824c4f180362
Dan Kaste <dan.kaste@leadingedje.com>
2019-03-08 10:17:58 -0500

    change they keypress provider so it can manage the keybindinds

--- shared/directives/action-item-key-mapping.directive.ts
+++ shared/directives/action-item-key-mapping.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, Input, Renderer2, ElementRef, OnInit, OnDestroy } from '@angular/core';
+import { Directive, Input, Renderer2, ElementRef, OnDestroy } from '@angular/core';
 import { IActionItem, SessionService } from '../../core';
 import { KeyPressProvider } from '../providers/keypress.provider';
 import { Subscription } from 'rxjs';
@@ -8,10 +8,27 @@ import { Configuration } from '../../configuration/configuration';
     // tslint:disable-next-line:directive-selector
     selector: '[actionItem]'
 })
-export class ActionItemKeyMappingDirective implements OnDestroy, OnInit {
+export class ActionItemKeyMappingDirective implements OnDestroy {
 
     @Input()
-    actionItem: IActionItem;
+    set actionItem(item: IActionItem) {
+        if ( this.subscription ) {
+            this.subscription.unsubscribe();
+        }
+
+        this.subscription = this.keyPresses.subscribe( item.keybind, 100, event => {
+            // ignore repeats
+            if ( event.repeat || !Configuration.enableKeybinds ) {
+                return;
+            }
+            if ( event.type === 'keydown') {
+                this.renderer.addClass(this.el.nativeElement, 'key-mapping-active');
+                this.session.onAction(item);
+            } else if ( event.type === 'keyup') {
+                this.renderer.removeClass(this.el.nativeElement, 'key-mapping-active');
+            }
+        });
+    }
 
     private subscription: Subscription;
 
@@ -23,37 +40,8 @@ export class ActionItemKeyMappingDirective implements OnDestroy, OnInit {
 
     }
 
-    ngOnInit(): void {
-        this.subscription = this.keyPresses.getKeyPresses().subscribe( event => {
-            // ignore repeats
-            if ( event.repeat || !Configuration.enableKeybinds ) {
-                return;
-            }
-            if ( event.type === 'keydown') {
-                this.onKeydown(event);
-            } else if ( event.type === 'keyup') {
-                this.onKeyup(event);
-            }
-        });
-    }
-
     ngOnDestroy(): void {
         this.subscription.unsubscribe();
     }
 
-
-    public onKeydown(event: KeyboardEvent) {
-        if (this.actionItem.keybind === event.key ) {
-            this.renderer.addClass(this.el.nativeElement, 'key-mapping-active');
-            this.session.onAction(this.actionItem);
-            event.preventDefault();
-        }
-    }
-
-    public onKeyup(event: KeyboardEvent) {
-        if (this.actionItem.keybind === event.key ) {
-            this.renderer.removeClass(this.el.nativeElement, 'key-mapping-active');
-            event.preventDefault();
-        }
-    }
 }
--- shared/providers/keypress.provider.ts
+++ shared/providers/keypress.provider.ts
@@ -1,27 +1,55 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy, OnInit } from '@angular/core';
 import { Observable, merge, Subject, Subscription } from 'rxjs';
 
 @Injectable()
-export class KeyPressProvider {
+export class KeyPressProvider implements OnDestroy {
 
-    private keyPresses$ = new Subject<KeyboardEvent>();
     private keyPressSources: Observable<KeyboardEvent>[] = [];
     private subscription: Subscription;
+
+    private subscribers = new Map<string, Map<number, (KeyboardEvent) => void>>();
+
+    ngOnDestroy(): void {
+        if ( this.subscription ) {
+            this.subscription.unsubscribe();
+        }
+    }
 
     registerKeyPressSource( source$: Observable<KeyboardEvent>) {
         this.keyPressSources.push(source$);
         this.rebuildKeyPressObserver();
     }
 
-    getKeyPresses(): Observable<KeyboardEvent> {
-        return this.keyPresses$;
+    subscribe( key: string, priority: number, next: (KeyboardEvent) => void): Subscription {
+        if ( !this.subscribers.has(key) ) {
+            this.subscribers.set( key, new Map<number, (KeyboardEvent) => void>());
+        }
+
+        if ( this.subscribers.get(key).has(priority)) {
+            throw new Error( `Another subscriber already exists with key ${key} and priority ${priority}`);
+        }
+        this.subscribers.get(key).set(priority, next);
+
+        return new Subscription( () => {
+            this.subscribers.get(key).delete(priority);
+        });
     }
 
     private rebuildKeyPressObserver() {
         if ( this.subscription ) {
             this.subscription.unsubscribe();
         }
-        this.subscription = merge(...this.keyPressSources).subscribe( event => this.keyPresses$.next(event));
+
+        this.subscription = merge(...this.keyPressSources).subscribe( event => {
+            if ( this.subscribers.has(event.key)) {
+                const priorityMap = this.subscribers.get(event.key);
+                const prioritiesList = Array.from(priorityMap.keys()).sort();
+                if ( prioritiesList.length > 0 ) {
+                    const priority = prioritiesList[0];
+                    this.subscribers.get(event.key).get(priority)(event);
+                }
+            }
+        });
     }
 
 }
