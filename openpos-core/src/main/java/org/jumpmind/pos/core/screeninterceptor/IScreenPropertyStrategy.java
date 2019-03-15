38542a5ca241657d1af2c66636655ee7f03ee37f
Chris Henson <chenson42@gmail.com>
2019-03-15 10:57:52 -0400

    cleanup

--- openpos-core/src/main/java/org/jumpmind/pos/core/screeninterceptor/IScreenPropertyStrategy.java
+++ openpos-core/src/main/java/org/jumpmind/pos/core/screeninterceptor/IScreenPropertyStrategy.java
@@ -2,7 +2,6 @@ package org.jumpmind.pos.core.screeninterceptor;
 
 import java.util.Map;
 
-import org.jumpmind.pos.core.screen.Screen;
 import org.jumpmind.pos.core.ui.UIMessage;
 
 public interface IScreenPropertyStrategy {
