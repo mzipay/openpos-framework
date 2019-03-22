c2c8f4983128cb6987e03270d05ceffbf8ff307b
stojankz <kstojanovska@jumpmind.com>
2019-03-22 11:51:16 -0400

    Add new MessageType & Message class in replace of DialogScreen

---
+++ openpos-core/src/main/java/org/jumpmind/pos/core/ui/message/DialogUIMessage.java
@@ -0,0 +1,70 @@
+package org.jumpmind.pos.core.ui.message;
+
+import org.jumpmind.pos.core.screen.ActionItem;
+import org.jumpmind.pos.core.screen.Line;
+import org.jumpmind.pos.core.ui.UIMessage;
+import org.jumpmind.pos.core.ui.messagepart.DialogHeaderPart;
+import org.jumpmind.pos.core.ui.messagepart.MessagePartConstants;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DialogUIMessage extends UIMessage {
+
+    private List<ActionItem> buttons = new ArrayList<>();
+
+    private List<String> message = new ArrayList<>();
+
+    private List<Line> messageLines = new ArrayList<>();
+
+    public DialogUIMessage() {
+        setScreenType(UIMessageType.DIALOG);
+        this.asDialog();
+    }
+
+    public DialogUIMessage(String title, ActionItem button) {
+        this();
+        DialogHeaderPart dialogHeader = new DialogHeaderPart();
+        dialogHeader.setHeaderText(title);
+        addMessagePart(MessagePartConstants.DialogHeader, dialogHeader);
+        this.addButton(button);
+    }
+
+    public List<ActionItem> getButtons() {
+        return buttons;
+    }
+
+    public void setButtons(List<ActionItem> buttons) {
+        this.buttons = buttons;
+    }
+
+    public void addButton(ActionItem button) {
+        this.buttons.add(button);
+    }
+
+    public List<String> getMessage() {
+        return message;
+    }
+
+    public void setMessage(List<String> message) {
+        this.message = message;
+    }
+
+    public DialogUIMessage addMessage(String message) {
+        this.message.add(message);
+        return this;
+    }
+
+    public List<Line> getMessageLines() {
+        return messageLines;
+    }
+
+    public void setMessageLines(List<Line> messageLines) {
+        this.messageLines = messageLines;
+    }
+
+    public DialogUIMessage addMessageLine(Line line) {
+        this.messageLines.add(line);
+        return this;
+    }
+}
--- openpos-core/src/main/java/org/jumpmind/pos/core/ui/message/UIMessageType.java
+++ openpos-core/src/main/java/org/jumpmind/pos/core/ui/message/UIMessageType.java
@@ -4,4 +4,5 @@ public interface UIMessageType {
     String SALE="Sale";
     String SELECTION_LIST="SelectionList";
     String RETURN="Return";
+    String DIALOG="Dialog";
 }
