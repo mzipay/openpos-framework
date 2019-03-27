cbd471b17a764145afbe635c1dedb3b6d1112e9f
stojankz <kstojanovska@jumpmind.com>
2019-03-27 15:15:30 -0400

    update prompt screen to use the new prompt-form-part screen part component

--- projects/openpos-client-core-lib/src/lib/screens-with-parts/prompt/prompt.interface.ts
+++ projects/openpos-client-core-lib/src/lib/screens-with-parts/prompt/prompt.interface.ts
@@ -1,26 +1,7 @@
 
-import { Validator } from '../../core/interfaces/validator.enum';
 import { IAbstractScreen } from '../../core/interfaces/abstract-screen.interface';
-import { FieldInputType } from '../../core/interfaces/field-input-type.enum';
 import { IActionItem } from '../../core/interfaces/menu-item.interface';
 
 export interface PromptInterface extends IAbstractScreen {
-    promptIcon: string;
-    placeholderText: string;
-    hintText: string;
-    instructions: string;
-    responseText: string;
-    editable: boolean;
-    responseType: FieldInputType;
-    actionButton: IActionItem;
-    otherActions: IActionItem[];
-    minLength: number;
-    maxLength: number;
-    comments: string;
-    showComments: boolean;
-    validationPattern: string;
-    scanEnabled: boolean;
-    keyboardPreference: string;
-    validators: Validator[];
     sausageLinks: IActionItem[];
 }
