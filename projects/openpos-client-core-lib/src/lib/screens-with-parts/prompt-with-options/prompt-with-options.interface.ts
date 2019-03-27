d1808eaf01b368521a18be64f7daecb2973a5479
stojankz <kstojanovska@jumpmind.com>
2019-03-27 14:39:59 -0400

    add interface for PromptWithOptions

---
+++ projects/openpos-client-core-lib/src/lib/screens-with-parts/prompt-with-options/prompt-with-options.interface.ts
@@ -0,0 +1,8 @@
+import { IOptionItem } from '../../screens-deprecated/choose-options/option-item.interface';
+import { IAbstractScreen } from '../../core/interfaces/abstract-screen.interface';
+import { IActionItem } from '../../core/interfaces/menu-item.interface';
+
+export interface PromptWithOptionsInterface extends IAbstractScreen {
+    options: IOptionItem[];
+    sausageLinks: IActionItem[];
+}
