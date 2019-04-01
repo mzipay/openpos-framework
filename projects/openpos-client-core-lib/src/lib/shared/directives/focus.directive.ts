5f5ac686f3005e6471f253c108c57b857d0223e7
Chris Henson <chenson42@gmail.com>
2019-04-01 15:14:08 -0400

    use the focus service

--- projects/openpos-client-core-lib/src/lib/shared/directives/focus.directive.ts
+++ projects/openpos-client-core-lib/src/lib/shared/directives/focus.directive.ts
@@ -1,3 +1,4 @@
+import { FocusService } from './../../core/services/focus.service';
 import { Directive, Input, ElementRef, AfterContentInit } from '@angular/core';
 
 @Directive({
@@ -8,12 +9,12 @@ export class FocusDirective implements AfterContentInit {
     @Input()
     appFocus: boolean;
 
-    constructor(private element: ElementRef) {
+    constructor(private element: ElementRef, private focusService: FocusService) {
     }
 
     ngAfterContentInit(): void {
         if (this.appFocus) {
-            setTimeout(() => this.element.nativeElement.focus());
+            this.focusService.requestFocus('appFocus', this.element);
         }
     }
 }
