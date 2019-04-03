46f31de7688350959b9708076a482905de903879
Jason Mihalick <jason@jcraftsmen.com>
2019-04-03 12:20:12 -0400

    Move Point class into openpos-util

--- openpos-core/src/main/java/org/jumpmind/pos/core/model/Signature.java
+++ openpos-core/src/main/java/org/jumpmind/pos/core/model/Signature.java
@@ -3,6 +3,8 @@ package org.jumpmind.pos.core.model;
 import java.io.Serializable;
 import java.util.List;
 
+import org.jumpmind.pos.util.model.Point;
+
 public class Signature implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -42,40 +44,4 @@ public class Signature implements Serializable {
         this.base64EncodedImage = base64EncodedImage;
     }
 
-
-    public static class Point implements Serializable {
-        private static final long serialVersionUID = 1L;
-            
-        private int x;
-        private int y;
-        private long time;
-        private String color;
-        
-        public Point() {}
-        
-        public int getX() {
-            return x;
-        }
-        public void setX(int x) {
-            this.x = x;
-        }
-        public int getY() {
-            return y;
-        }
-        public void setY(int y) {
-            this.y = y;
-        }
-        public long getTime() {
-            return time;
-        }
-        public void setTime(long time) {
-            this.time = time;
-        }
-        public String getColor() {
-            return color;
-        }
-        public void setColor(String color) {
-            this.color = color;
-        }
-    }
 }
