222f7c96f325fb9f9ce50dd7ef186b3ce9c31af2
Chris Henson <chenson42@gmail.com>
2019-04-08 14:40:01 -0400

    make the fact that the server has started more prominient

--- openpos-util/src/main/java/org/jumpmind/pos/util/startup/LastStartupTask.java
+++ openpos-util/src/main/java/org/jumpmind/pos/util/startup/LastStartupTask.java
@@ -3,20 +3,26 @@ package org.jumpmind.pos.util.startup;
 import static java.lang.Integer.MAX_VALUE;
 
 import org.apache.commons.lang3.mutable.MutableBoolean;
+import org.jumpmind.pos.util.BoxLogging;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Component
 @Order(MAX_VALUE)
 public class LastStartupTask extends AbstractStartupTask {    
-    
+        
     @Autowired
     MutableBoolean initialized;
+    
+    @Value("${server.port:?}")
+    String serverPort;
 
     @Override
     protected void doTask() throws Exception {
         initialized.setTrue();
+        logger.info(BoxLogging.box(String.format("Server started on port %s", serverPort)));
     }
     
 }
