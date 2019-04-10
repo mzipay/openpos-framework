a8222c640e0d726fed969618e316db171164442b
Chris Henson <chenson42@gmail.com>
2019-04-10 14:37:02 -0400

    Create ReturnTransDetailsUIMessage.java

---
+++ openpos-core/src/main/java/org/jumpmind/pos/core/ui/message/ReturnTransDetailsUIMessage.java
@@ -0,0 +1,46 @@
+package org.jumpmind.pos.core.ui.message;
+
+import java.util.List;
+
+import org.jumpmind.pos.core.screen.ActionItem;
+import org.jumpmind.pos.core.screen.SellItem;
+import org.jumpmind.pos.core.ui.UIMessage;
+
+public class ReturnTransDetailsUIMessage extends UIMessage {
+
+    private static final long serialVersionUID = 1L;
+    
+    public ReturnTransDetailsUIMessage() {
+        setId("returnTransactionDetails");
+        setScreenType("ReturnTransDetailDialog");
+    }
+
+    String instructions;
+    List<SellItem> items;
+    ActionItem selectionButton;
+    
+    public void setInstructions(String instructions) {
+        this.instructions = instructions;
+    }
+    
+    public String getInstructions() {
+        return instructions;
+    }
+    
+    public void setItems(List<SellItem> items) {
+        this.items = items;
+    }
+    
+    public List<SellItem> getItems() {
+        return items;
+    }
+    
+    public void setSelectionButton(ActionItem selectionButton) {
+        this.selectionButton = selectionButton;
+    }
+    
+    public ActionItem getSelectionButton() {
+        return selectionButton;
+    }
+    
+}
