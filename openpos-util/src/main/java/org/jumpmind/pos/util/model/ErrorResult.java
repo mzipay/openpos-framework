f4c7b8bef5a6a5cc535d46cbb0df8abf45b1f378
Chris Henson <chenson42@gmail.com>
2019-04-25 15:11:12 -0400

    serialize exceptions better.  return 500 error code when error occurs

--- openpos-util/src/main/java/org/jumpmind/pos/util/model/ErrorResult.java
+++ openpos-util/src/main/java/org/jumpmind/pos/util/model/ErrorResult.java
@@ -2,36 +2,40 @@ package org.jumpmind.pos.util.model;
 
 import java.io.Serializable;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonTypeInfo.As;
+
 public class ErrorResult implements Serializable {
 
     private static final long serialVersionUID = 1L;
-    
+
     String message;
 
+    @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = As.PROPERTY, property = "classname")
     Throwable throwable;
-    
+
     public ErrorResult() {
     }
-    
+
     public ErrorResult(String message, Throwable throwable) {
         this.message = message;
         this.throwable = throwable;
     }
-    
+
     public void setMessage(String message) {
         this.message = message;
     }
-    
+
     public String getMessage() {
         return message;
     }
-    
+
     public Throwable getThrowable() {
         return throwable;
     }
-    
+
     public void setThrowable(Throwable throwable) {
         this.throwable = throwable;
     }
-    
+
 }
