ddb2598955edd35b18fee0594468a0b2a097e9d6
stojankz <kstojanovska@jumpmind.com>
2019-04-26 08:36:38 -0400

    add option to show scan or search bar in the selection list screen & show placeholder text and/or icon, if present, if there are no items in the selectionList

--- projects/openpos-client-core-lib/src/lib/screens-with-parts/selection-list/selection-list.interface.ts
+++ projects/openpos-client-core-lib/src/lib/screens-with-parts/selection-list/selection-list.interface.ts
@@ -6,11 +6,14 @@ export interface SelectionListInterface extends IAbstractScreen {
     selectionList: SelectionListItem[];
     selectionButtons: IActionItem[];
     nonSelectionButtons: IActionItem[];
+    showScan: boolean;
     multiSelect: boolean;
     defaultSelect: boolean;
     defaultSelectItemIndex: number;
     selectionChangedAction: string;
     instructions: string;
     numberItemsPerPage: number;
+    noListItemsPlaceholderText: string;
+    noListItemsPlaceholderIcon: string;
     sausageLinks: IActionItem[];
 }
