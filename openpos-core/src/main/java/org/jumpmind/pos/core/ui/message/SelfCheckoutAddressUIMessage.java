7a029f4652fa2701df3b14abd513d0451a804042
maxwellpettit <maxwell.pettit@gmail.com>
2019-04-29 12:31:57 -0400

    Support state combo box on auto complete address

--- openpos-core/src/main/java/org/jumpmind/pos/core/ui/message/SelfCheckoutAddressUIMessage.java
+++ openpos-core/src/main/java/org/jumpmind/pos/core/ui/message/SelfCheckoutAddressUIMessage.java
@@ -1,5 +1,7 @@
 package org.jumpmind.pos.core.ui.message;
 
+import java.util.List;
+
 public class SelfCheckoutAddressUIMessage extends SelfCheckoutFormUIMessage {
 
     private static final long serialVersionUID = 1L;
@@ -13,6 +15,15 @@ public class SelfCheckoutAddressUIMessage extends SelfCheckoutFormUIMessage {
         this.getForm().addTextField("addressLine2", "Address Line 2", "", false);
         this.getForm().addTextField("locality", "City", "", true);
         this.getForm().addTextField("state", "State", "", true);
+        this.getForm().addTextField("postalCode", "Postal Code", "", true);
+        this.getForm().addTextField("country", "Country", "", true);
+    }
+
+    public void addAddressFieldsWithComboState(List<String> states) {
+        this.getForm().addTextField("streetAddress", "Street Address", "", true);
+        this.getForm().addTextField("addressLine2", "Address Line 2", "", false);
+        this.getForm().addTextField("locality", "City", "", true);
+        this.getForm().addComboBox("state", "State", states, true);
         this.getForm().addTextField("postalCode", "Postal Code", "", true);
         this.getForm().addTextField("country", "Country", "", true);
     }
