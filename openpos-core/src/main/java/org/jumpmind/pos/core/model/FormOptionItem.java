1ce3f21bad839e39a6ad325a14018a6804f842d7
stojankz <kstojanovska@jumpmind.com>
2019-05-07 16:07:20 -0400

    Mark MultipleFormOption as deprecated and create a FormOptionItem

---
+++ openpos-core/src/main/java/org/jumpmind/pos/core/model/FormOptionItem.java
@@ -0,0 +1,54 @@
+package org.jumpmind.pos.core.model;
+
+import org.jumpmind.pos.core.screen.ActionItem;
+
+import java.io.Serializable;
+
+public class FormOptionItem implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private Form form;
+    private String icon;
+    private String name;
+    private ActionItem optionAction;
+    private String instructions;
+    private boolean enabled=true;
+
+    public Form getForm() {
+        return form;
+    }
+    public void setForm(Form form) {
+        this.form = form;
+    }
+    public String getIcon() {
+        return icon;
+    }
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+    public String getInstructions() {
+        return instructions;
+    }
+    public void setInstructions(String instructions) {
+        this.instructions = instructions;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public ActionItem getOptionAction() {
+        return optionAction;
+    }
+    public void setOptionAction(ActionItem optionAction) {
+        this.optionAction = optionAction;
+    }
+    public boolean isEnabled() {
+        return enabled;
+    }
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+}
+
