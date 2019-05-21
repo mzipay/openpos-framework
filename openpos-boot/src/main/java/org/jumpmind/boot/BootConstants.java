d3826b4742a9b3ab700a13a922270f8f72cc0c2a
Chris Henson <chenson42@gmail.com>
2019-05-21 08:05:09 -0400

    remove openpos-boot from the project

--- openpos-boot/src/main/java/org/jumpmind/boot/BootConstants.java
+++
@@ -1,9 +0,0 @@
-package org.jumpmind.boot;
-
-final public class BootConstants {
-
-    private BootConstants() {}
-    
-    public static final int RESPONSE_CODE_UPDATE_AVAILABLE = 555;
-    public static final int RESPONSE_CODE_UP_TO_DATE = 777;
-}
