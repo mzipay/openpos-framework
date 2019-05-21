21b60865b1b064ab5a43f5f5cb2c517709564ea7
Jason Mihalick <jason@jcraftsmen.com>
2019-05-21 09:23:51 -0400

    Move ConfirmDialog from screens-deprecated to screens-with-parts

--- projects/openpos-client-core-lib/src/lib/screens-deprecated/screen.constants.ts
+++ projects/openpos-client-core-lib/src/lib/screens-deprecated/screen.constants.ts
@@ -1,4 +1,3 @@
-import { ConfirmDialogComponent } from './confirm-dialog/confirm-dialog.component';
 import { MultiselectItemListComponent } from './multiselect-item-list/multiselect-item-list.component';
 import { BasicItemSearchComponent } from './basic-item-search/basic-item-search.component';
 import { ChooseOptionsComponent } from './choose-options/choose-options.component';
@@ -138,7 +137,6 @@ export const ScreenConstants = {
         { name: 'Change', component: ChangeComponent },
         { name: 'PrintPreview', component: PrintPreviewComponent },
         { name: 'Wait', component: WaitComponent },
-        { name: 'ConfirmDialog', component: ConfirmDialogComponent },
         { name: 'CustomerSearch', component: CustomerSearchResultsComponent },
         { name: 'FullPageImage', component: FullPageImageComponent },
         { name: 'CallForAuthorization', component: CallForAuthorizationComponent },
