fdc885c712f51f976b0d53ae14401a726e2cc433
Dan Kaste <dan.kaste@leadingedje.com>
2019-05-23 14:13:45 -0400

    fix tests

--- openpos-core/src/test/java/org/jumpmind/pos/core/flow/StateManagerDoubleExistSubTest.java
+++ openpos-core/src/test/java/org/jumpmind/pos/core/flow/StateManagerDoubleExistSubTest.java
@@ -1,23 +1,32 @@
 package org.jumpmind.pos.core.flow;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.*;
 
 import org.jumpmind.pos.server.model.Action;
+import org.jumpmind.pos.server.service.IMessageService;
+import org.jumpmind.pos.util.model.Message;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class StateManagerDoubleExistSubTest {
     
     @InjectMocks
-    Injector injector;    
+    Injector injector;
+
+    @Mock
+    IMessageService messageService;
 
     @Test
     public void testDoubleSubTransitionExit() throws Exception {
 
-        
+        doNothing().when(messageService).sendMessage(any(String.class), any(String.class), any(Message.class));
+
         StateManager stateManager = StateManagerTestUtils.buildStateManager(injector, "testflows/test-double-sub-exit-flow.yml");
         
         assertEquals(OrderDetailState.class, stateManager.getCurrentState().getClass());
