d7c75febc3b92d3bae12d7507f3ffde4814d6ab6
Dan Kaste <dan.kaste@leadingedje.com>
2019-05-24 12:14:42 -0400

    update wedge to work with special chars and complex start and end seqences

--- projects/openpos-client-core-lib/src/lib/core/plugins/wedge-scanner.plugin.ts
+++ projects/openpos-client-core-lib/src/lib/core/plugins/wedge-scanner.plugin.ts
@@ -1,53 +1,62 @@
 import { IScanner } from './scanner.interface';
 import { Injectable } from '@angular/core';
-import { Observable, fromEvent, merge, of } from 'rxjs';
-import { map, filter, bufferToggle, timeout, catchError } from 'rxjs/operators';
+import { Observable, fromEvent } from 'rxjs';
+import { map, filter, bufferToggle } from 'rxjs/operators';
 import { IScanData } from './scan.interface';
 import { SessionService } from '../services/session.service';
+
+interface ControlSequence { modifiers: string[]; key: string; }
 
 @Injectable({
     providedIn: 'root'
   })
   export class WedgeScannerPlugin implements IScanner {
 
-    private startChar = '*';
-    private endChar = 'Enter';
-    private bufferTimeout = 100;
+    private startSequence = '*';
+    private endSequence = 'Enter';
+    private codeTypeLength = 0;
     private scannerActive: boolean;
 
     constructor( sessionService: SessionService ) {
         sessionService.getMessages('ConfigChanged').pipe(
-            filter( m => m.configName === 'WedgeScanner')
+            filter( m => m.configType === 'WedgeScanner')
         ).subscribe( m => {
-            this.startChar = m.startChar;
-            this.endChar = m.endChar;
+            if ( m.startSequence ) {
+                this.startSequence = m.startSequence;
+            }
+            if ( m.endSequence ) {
+                this.endSequence = m.endSequence;
+            }
+            if ( m.codeTypeLength ) {
+                this.codeTypeLength = m.codeTypeLength;
+            }
         });
     }
 
     startScanning(): Observable<IScanData> {
         this.scannerActive = true;
 
-        const startScanBuffer = fromEvent(document, 'keydown').pipe(
-            filter( (e: KeyboardEvent) => this.scannerActive && e.key === this.startChar ));
+        const startSequenceObj = this.getControlStrings(this.startSequence);
+        const endSequenceObj = this.getControlStrings(this.endSequence);
 
-        const stopScanBuffer = fromEvent(document, 'keydown').pipe(
-            filter((e: KeyboardEvent) => !this.scannerActive || e.key === this.endChar));
+        const startScanBuffer = fromEvent(document, 'keypress').pipe(
+            filter( (e: KeyboardEvent) => this.filterForControlSequence(startSequenceObj, e)));
 
-        const timoutScanBuffer = startScanBuffer.pipe(
-            timeout(this.bufferTimeout),
-            catchError( e => of(true))
-        );
+        const stopScanBuffer = fromEvent(document, 'keypress').pipe(
+            filter((e: KeyboardEvent) => this.filterForControlSequence(endSequenceObj, e)));
 
-        return fromEvent(document, 'keydown').pipe(
-            map( (e: KeyboardEvent) => e.key ),
+        return fromEvent(document, 'keypress').pipe(
             bufferToggle(
                     startScanBuffer,
-                    () => merge( stopScanBuffer, timoutScanBuffer)
+                    () => stopScanBuffer
             ),
-            filter( s => s[0] === this.startChar && s[s.length - 1] === this.endChar),
+            // Make sure we get a full buffer and didn't timout
+            filter( (s: KeyboardEvent[]) => this.filterForControlSequence(startSequenceObj, s[0]) &&
+                                            this.filterForControlSequence(endSequenceObj, s[s.length - 1])),
+            map( events => this.convertKeyEventsToChars(events) ),
             // Join the buffer into a string and remove the start and stop characters
             map( (s) => s.join('').slice(1, s.length - 1)),
-            map( s => this.getScanData(s))
+            map( (s: string) => this.getScanData(s)),
         );
     }
 
@@ -55,8 +64,70 @@ import { SessionService } from '../services/session.service';
         this.scannerActive = false;
     }
 
+    private getControlStrings( sequence: string): ControlSequence {
+        let modifiers: string[];
+        let key: string;
+
+        if ( sequence.includes('+')) {
+            modifiers = sequence.split('+');
+            modifiers = modifiers.slice(0, modifiers.lastIndexOf('+'));
+            key = modifiers.slice(modifiers.lastIndexOf('+'))[0];
+        } else {
+            key = sequence;
+        }
+
+        return { modifiers, key};
+    }
+
+    private filterForControlSequence( sequence: ControlSequence, e: KeyboardEvent ): boolean {
+        if ( !this.scannerActive ) {
+            return false;
+        }
+        const keyPressed = e.key === sequence.key;
+        if ( !!sequence.modifiers ) {
+            const modifiersPressed = sequence.modifiers.map( m => this.checkModifier(e, m)).reduce((accum, m) => accum && m );
+            return modifiersPressed && keyPressed;
+        }
+        return keyPressed;
+    }
+
+    private checkModifier( e: KeyboardEvent, modifier: string): boolean {
+        switch (modifier) {
+            case 'ctrl':
+                return e.ctrlKey;
+            case 'alt':
+                return e.altKey;
+        }
+    }
+
+    private convertKeyEventsToChars( events: KeyboardEvent[] ): string[] {
+        // We need to look for 2 character sequences with the alt key pressed and convert them into the
+        // special characters they represent
+        // Mac gives us the individual key for special characters ie (3 and then 0 with alt held down) for return
+        // Where as windows gives us 30;
+        const charList = [];
+        for ( let i = 0; i < events.length; i++ ) {
+            const e = events[i];
+
+            if ( e.altKey && i < events.length - 1 ) {
+                // get the next number
+                const e2 = events[i + 1];
+
+                // convert the char code into a string
+                charList.push(String.fromCharCode( parseInt(e.key + e2.key, 10) ));
+
+                // skip the next value since we already accounted for it.
+                i++;
+            } else {
+                charList.push(e.key);
+            }
+        }
+
+        return charList;
+    }
+
     private getScanData( s: string ): IScanData {
-        return { type: s, data: s};
+        return { type: s.slice(0, this.codeTypeLength), data: s.slice(this.codeTypeLength)};
     }
 
 }
