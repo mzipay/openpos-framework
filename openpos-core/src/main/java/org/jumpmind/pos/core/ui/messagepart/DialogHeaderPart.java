1037ca28736fed23dc7c611bc859adc0ac25be82
Jason Mihalick <jason@jcraftsmen.com>
2019-06-03 15:58:04 -0400

    Add constructors for developer friendliness

--- openpos-core/src/main/java/org/jumpmind/pos/core/ui/messagepart/DialogHeaderPart.java
+++ openpos-core/src/main/java/org/jumpmind/pos/core/ui/messagepart/DialogHeaderPart.java
@@ -12,6 +12,23 @@ public class DialogHeaderPart implements IHasBackButton, Serializable {
     private String headerIcon;
     private ActionItem backButton;
 
+    public DialogHeaderPart() {
+    }
+
+    public DialogHeaderPart(String headerText) {
+        this(headerText, null, null);
+    }
+
+    public DialogHeaderPart(String headerText, String headerIcon) {
+        this(headerText, headerIcon, null);
+    }
+    
+    public DialogHeaderPart(String headerText, String headerIcon, ActionItem backButton) {
+        this.headerText = headerText;
+        this.headerIcon = headerIcon;
+        this.backButton = backButton;
+    }
+    
     public String getHeaderText() {
         return headerText;
     }
