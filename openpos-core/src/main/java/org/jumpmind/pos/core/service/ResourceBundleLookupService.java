ac8e2fb3a217e4cbc8d017e198342e41750fa6af
Jason Mihalick <jason@jcraftsmen.com>
2019-06-07 10:58:22 -0400

    Allow for property lookups whose keys exactly match in case

--- openpos-core/src/main/java/org/jumpmind/pos/core/service/ResourceBundleLookupService.java
+++ openpos-core/src/main/java/org/jumpmind/pos/core/service/ResourceBundleLookupService.java
@@ -1,8 +1,8 @@
 package org.jumpmind.pos.core.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
+import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 public class ResourceBundleLookupService implements IResourceLookupService{
@@ -12,7 +12,18 @@ public class ResourceBundleLookupService implements IResourceLookupService{
 	
 	@Override
 	public String getString(String appId, String deviceId, String group, String key) {
-		return screenProperties.getString(key.toLowerCase());
+        String value = null;
+        try {
+            value = screenProperties.getString(key);
+        } catch (MissingResourceException ex) {
+            // allow failure for key as given, then try lower case version of key
+            // below. prior behavior was to only try lower case version of key
+        }
+        if (value == null) {
+            value = screenProperties.getString(key.toLowerCase());
+        }
+
+		return value;
 	}
 
 }
