5ca4abac34f5257a653d7332ea3035328743ee55
Dan Kaste <dan.kaste@leadingedje.com>
2019-06-17 09:21:55 -0400

    .

--- projects/openpos-client-core-lib/src/lib/core/plugins/openpos-barcode-type.enum.ts
+++ projects/openpos-client-core-lib/src/lib/core/plugins/openpos-barcode-type.enum.ts
@@ -1,6 +1,12 @@
 export enum OpenposBarcodeType {
     CODE128 = 'CODE128',
     EAN13 = 'EAN13',
+    EAN13_2 = 'EAN13_2',
+    EAN13_5 = 'EAN13_5',
     UPCA = 'UPCA',
+    UPCA_2 = 'UPCA_2',
+    UPCA_5 = 'UPCA_5',
     UPCE = 'UPCE',
+    UPCE_2 = 'UPCE_2',
+    UPCE_5 = 'UPCE_5',
 }
