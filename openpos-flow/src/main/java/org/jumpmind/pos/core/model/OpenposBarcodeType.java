4a417e18778bdd128d65432858b0638e4c430514
Dan Kaste <dan.kaste@leadingedje.com>
2019-06-17 13:09:07 -0400

    add more barcode types

--- openpos-flow/src/main/java/org/jumpmind/pos/core/model/OpenposBarcodeType.java
+++ openpos-flow/src/main/java/org/jumpmind/pos/core/model/OpenposBarcodeType.java
@@ -3,6 +3,12 @@ package org.jumpmind.pos.core.model;
 public enum OpenposBarcodeType {
     CODE128,
     EAN13,
+    EAN13_2,
+    EAN13_5,
     UPCA,
-    UPCE
+    UPCA_2,
+    UPCA_5,
+    UPCE,
+    UPCE_2,
+    UPCE_5
 }
