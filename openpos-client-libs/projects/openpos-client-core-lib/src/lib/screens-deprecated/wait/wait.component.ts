1c4858e7a977c61d85d9b4b93ff3156e7c01ed46
Eric Amiralian <eamiralian@users.noreply.github.com>
2019-07-26 15:03:00 -0400

    Moving Wait Component to temporarily-shared-screens.module (#163)

--- openpos-client-libs/projects/openpos-client-core-lib/src/lib/screens-deprecated/wait/wait.component.ts
+++ openpos-client-libs/projects/openpos-client-core-lib/src/lib/screens-deprecated/wait/wait.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { PosScreen } from '../pos-screen/pos-screen.component';
+import { ScreenComponent } from '../../shared/decorators/screen-component.decorator';
 
 /**
  * @ignore
  */
+@ScreenComponent({
+  name: 'Wait'
+})
 @Component({
   selector: 'app-wait-component',
   templateUrl: './wait.component.html'
