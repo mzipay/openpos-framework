61fb2e320442185852d0056f6075715f28a5549e
Jared <jaredfrees@gmail.com>
2019-07-31 10:05:56 -0400

    Add support for getting a map of objects out of ctx_config (#174)

--- openpos-service/src/main/java/org/jumpmind/pos/service/IConfigApplicator.java
+++ openpos-service/src/main/java/org/jumpmind/pos/service/IConfigApplicator.java
@@ -1,7 +1,13 @@
 package org.jumpmind.pos.service;
 
+import java.util.Map;
+
 public interface IConfigApplicator {
 
     public void applyAdditionalConfiguration(String deviceId, String startsWith, Object applyTo);
+    
+    public <T> Map<String, T> getAdditionalConfigurationMap(String deviceId, String startsWith, T configType);
+    
+    public void applyAdditionalConfigurationYml(String deviceId, String configName, Object configType);
     
 }
