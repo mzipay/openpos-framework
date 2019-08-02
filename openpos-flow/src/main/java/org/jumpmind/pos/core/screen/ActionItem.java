d3be03d36ef2556b03d9f672573349ea933537d6
Eric Amiralian <eamiralian@users.noreply.github.com>
2019-08-02 08:42:31 -0400

    ActionItem confirmation message constructor now sets title (#183)

--- openpos-flow/src/main/java/org/jumpmind/pos/core/screen/ActionItem.java
+++ openpos-flow/src/main/java/org/jumpmind/pos/core/screen/ActionItem.java
@@ -81,6 +81,7 @@ public class ActionItem implements Serializable {
         this(action, title, icon);
         if (confirmationMessage != null) {
             this.confirmationDialog = new ConfirmationDialog();
+            this.confirmationDialog.setTitle(title);
             this.confirmationDialog.setMessage(confirmationMessage);
         }
     }
