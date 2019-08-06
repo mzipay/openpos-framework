71deee64db783a9c10f757ff63c1ef89edfdfddb
chenson42 <chenson42@gmail.com>
2019-08-06 08:35:27 -0400

    publish openpos-client-libs

--- openpos-client-libs/projects/openpos-client-core-lib/src/lib/version.ts
+++ openpos-client-libs/projects/openpos-client-core-lib/src/lib/version.ts
@@ -1,4 +1,4 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
-export const VERSION = {"componentName":"openpos-client-core-lib","version":"0.4.2-SNAPSHOT","gitHash":"b23384061f22d7d1f8c556f0295c0e2f61b3f48e","buildTime":"2019-08-02 12:43:46","gitBranch":"feature/personalize-app"};
+export const VERSION = {"componentName":"openpos-client-core-lib","version":"0.5.0-SNAPSHOT","gitHash":"4ef1a902b6461f8dc68e45ed00f60b6d600e3efb","buildTime":"2019-08-06 12:18:43","gitBranch":"master"};
 /* tslint:enable */
