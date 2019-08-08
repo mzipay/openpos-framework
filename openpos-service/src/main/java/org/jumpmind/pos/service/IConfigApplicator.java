44735d49acd577faf7f53049cd8a9f0b5bc2c4c0
Jared <jaredfrees@gmail.com>
2019-08-08 09:14:03 -0400

    Remove IConfigApplicator method (#194)

--- openpos-service/src/main/java/org/jumpmind/pos/service/IConfigApplicator.java
+++ openpos-service/src/main/java/org/jumpmind/pos/service/IConfigApplicator.java
@@ -1,11 +1,7 @@
 package org.jumpmind.pos.service;
 
-import java.util.Map;
-
 public interface IConfigApplicator {
 
     public void applyAdditionalConfiguration(String deviceId, String startsWith, Object applyTo);
     
-    public <T> Map<String, T> getAdditionalConfigurationMap(String deviceId, String startsWith, T configType);
-        
 }
