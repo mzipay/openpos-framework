025fab47adde28827cd86386a59c692892c4a936
chenson42 <chenson42@gmail.com>
2019-08-13 07:41:49 -0400

    add version.ts to gitignore

--- openpos-client-libs/projects/openpos-client-core-lib/src/lib/version.ts
+++
@@ -1,4 +0,0 @@
-// IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
-/* tslint:disable */
-export const VERSION = {"componentName":"openpos-client-core-lib","version":"0.5.0-SNAPSHOT","gitHash":"a54f52e4753fc837d65e320daf52fd35d7f826b0","buildTime":"2019-08-02 16:33:12","gitBranch":"feature/action-service"};
-/* tslint:enable */
