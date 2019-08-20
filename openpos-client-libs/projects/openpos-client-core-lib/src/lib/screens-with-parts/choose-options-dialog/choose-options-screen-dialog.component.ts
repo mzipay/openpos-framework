2be7c37185ac5ad3861370b290120b0afc1f338e
Klementina S. Chirico <kstojanovska@jumpmind.com>
2019-08-20 14:10:03 -0400

    Bugfix/scanner disabled on choose options destry (#217)
    * bumping the version number
    
    * Add support for disabling the language icons on language selector (#214)
    
    * Build openpos-client-personalize via gradle (#215)
    
    * choose-options-screen-dialog was stopping the scanner service onDestroy lifecycle incorrectly. Causes issues with screen that allows scanning that pops up a choose options dialog, when dialog closed scanning no longer enabled.

--- openpos-client-libs/projects/openpos-client-core-lib/src/lib/screens-with-parts/choose-options-dialog/choose-options-screen-dialog.component.ts
+++ openpos-client-libs/projects/openpos-client-core-lib/src/lib/screens-with-parts/choose-options-dialog/choose-options-screen-dialog.component.ts
@@ -1,9 +1,7 @@
-import { Component, OnDestroy, Injector } from '@angular/core';
+import { Component, Injector } from '@angular/core';
 import { DialogComponent } from '../../shared/decorators/dialog-component.decorator';
 import { PosScreen } from '../pos-screen/pos-screen.component';
 import { ChooseOptionsScreenDialogInterface } from './choose-options-screen-dialog.interface';
-import { ScannerService } from '../../core/platform-plugins/scanners/scanner.service';
-import { Subscription } from 'rxjs';
 
 @DialogComponent({
     name: 'ChooseOptions'
@@ -13,22 +11,13 @@ import { Subscription } from 'rxjs';
   templateUrl: './choose-options-screen-dialog.component.html',
   styleUrls: ['./choose-options-screen-dialog.component.scss']
 })
-export class ChooseOptionsScreenDialogComponent extends PosScreen<ChooseOptionsScreenDialogInterface> implements OnDestroy {
+export class ChooseOptionsScreenDialogComponent extends PosScreen<ChooseOptionsScreenDialogInterface> {
 
-  private scanServiceSubscription: Subscription;
-
-  constructor(private scannerService: ScannerService, injector: Injector) {
+  constructor(injector: Injector) {
       super(injector);
   }
 
   buildScreen() {
-  }
-
-  ngOnDestroy(): void {
-      if (this.scanServiceSubscription != null) {
-          this.scanServiceSubscription.unsubscribe();
-      }
-      this.scannerService.stopScanning();
   }
 
 }
