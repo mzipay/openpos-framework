fd8a619c8f7abe657b5a91b05493f7068031e0ba
Chris Henson <chenson42@users.noreply.github.com>
2019-09-11 12:40:16 -0400

    Make the session timer more robust by using spring's infrastructure (#258)

--- openpos-flow/src/main/java/org/jumpmind/pos/core/flow/SessionTimer.java
+++ openpos-flow/src/main/java/org/jumpmind/pos/core/flow/SessionTimer.java
@@ -2,51 +2,33 @@ package org.jumpmind.pos.core.flow;
 
 import java.util.Timer;
 
-import java.util.TimerTask;
-
-import javax.annotation.PostConstruct;
-
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SessionTimer {
-    
+
     private Timer timer;
-    
+
     private static final Logger log = Logger.getLogger(SessionTimer.class);
-    
+
     @Autowired
-    private StateManagerContainer stateManagerFactory;
-    
-    private static long TIMEOUT_CHECK_INTERVAL = 1000;
-        
+    private StateManagerContainer stateManagerContainer;
+
+    private static final long TIMEOUT_CHECK_INTERVAL = 1000;
+
     public static final String ACTION_KEEP_ALIVE = "KeepAlive";
-    
-    @PostConstruct
-    public void start() {
-        timer = new Timer("SessionTimer", true);
-        
-        timer.scheduleAtFixedRate(new TimerTask() {
-            @Override
-            public void run() {
-                checkSessionAges();
-            }
-        }, TIMEOUT_CHECK_INTERVAL, TIMEOUT_CHECK_INTERVAL);
-    }
-    
-    public void stop() {
-        timer.cancel();
-    }
-    
+
+    @Scheduled(fixedRate = TIMEOUT_CHECK_INTERVAL)
     protected void checkSessionAges() {
-        if (stateManagerFactory == null) {
+        if (stateManagerContainer == null) {
             throw new FlowException("stateManagerFactory cannot be null.");
         }
-        log.debug("checking if StateManagers need to time out due to inactivity.");
-        for (StateManager stateManager : stateManagerFactory.getAllStateManagers()) {
+        log.debug("checking if StateManagers need to time out due to inactivity");
+        for (StateManager stateManager : stateManagerContainer.getAllStateManagers()) {
             stateManager.checkSessionTimeout();
         }
-    }    
+    }
 }
