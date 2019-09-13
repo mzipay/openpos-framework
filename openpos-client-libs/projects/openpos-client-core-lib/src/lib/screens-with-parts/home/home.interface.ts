ec12f1559fd0188c572496441edeae4bb2823b59
stevencarley <45565101+stevencarley@users.noreply.github.com>
2019-09-13 10:05:26 -0400

    Adding support for status strip on Home Page Component (#264)

--- openpos-client-libs/projects/openpos-client-core-lib/src/lib/screens-with-parts/home/home.interface.ts
+++ openpos-client-libs/projects/openpos-client-core-lib/src/lib/screens-with-parts/home/home.interface.ts
@@ -1,6 +1,7 @@
 import { IAbstractScreen } from '../../core/interfaces/abstract-screen.interface';
 import { IActionItem } from '../../core/interfaces/action-item.interface';
 import { INotificationItem } from '../../core/interfaces/notification-item.interface';
+import { StatusStripInterface } from '../../shared/screen-parts/status-strip/status-strip.interface';
 
 
 export interface HomeInterface extends IAbstractScreen {
@@ -8,4 +9,5 @@ export interface HomeInterface extends IAbstractScreen {
     backgroundImage: string;
     logo: string;
     notificationItems: INotificationItem[];
+    statusStrip: StatusStripInterface;
 }
