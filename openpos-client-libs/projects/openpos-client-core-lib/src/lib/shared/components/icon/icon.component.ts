3cd39ee2e2e0e9c928e85ba977cdbbafc864b3ed
Dan Kaste <dan.kaste@leadingedje.com>
2019-09-30 14:49:20 -0400

    make icons resize with different media sizes (#307)

--- openpos-client-libs/projects/openpos-client-core-lib/src/lib/shared/components/icon/icon.component.ts
+++ openpos-client-libs/projects/openpos-client-core-lib/src/lib/shared/components/icon/icon.component.ts
@@ -1,7 +1,9 @@
 import { Component, Input, OnInit, ElementRef, Renderer2, OnChanges } from '@angular/core';
+import {MediaService} from '@angular/flex-layout';
 import { IconService } from '../../../core/services/icon.service';
 import { Observable } from 'rxjs';
 import { SafeHtml } from '@angular/platform-browser';
+import {OpenposMediaService} from '../../../core/services/openpos-media.service';
 
 @Component({
     selector: 'app-icon',
@@ -20,7 +22,9 @@ export class IconComponent implements OnInit, OnChanges {
     parser = new DOMParser();
     icon: Observable<SafeHtml>;
 
-    constructor(private iconService: IconService, private elementRef: ElementRef, private renderer: Renderer2) { }
+    constructor(private iconService: IconService, private elementRef: ElementRef, private renderer: Renderer2, private media: MediaService) {
+        media.subscribe( () => this.renderIcon() );
+    }
 
     ngOnInit(): void {
         this.renderIcon();
@@ -42,6 +46,10 @@ export class IconComponent implements OnInit, OnChanges {
         this.icon = this.iconService.getIconHtml(this.iconName);
         if (this.iconClass) {
             this.iconClass.split(' ').forEach(e => this.renderer.addClass(this.elementRef.nativeElement, e));
+            if( this.media.isActive('lt-md')) {
+                this.renderer.addClass(this.elementRef.nativeElement, 'mobile')
+            }
+
         }
     }
 }
