a87b09a2e3248ddf95e2d3ae2faac896f5527cb7
Chris Henson <chenson42@users.noreply.github.com>
2019-10-07 09:28:36 -0400

    Change the default timeout to 2 seconds.  Application become unusable with a default of 30. (#334)
    * Change the default timeout to 2 seconds.  Application become unusable with a default of 30.
    
    * remove @primary
    
    * fix the property names

--- openpos-flow/src/main/java/org/jumpmind/pos/core/service/DefaultHttpConnectionService.java
+++ openpos-flow/src/main/java/org/jumpmind/pos/core/service/DefaultHttpConnectionService.java
@@ -1,8 +1,11 @@
 package org.jumpmind.pos.core.service;
 
+import java.io.IOException;
+
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpResponseException;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpRequestBase;
@@ -11,36 +14,45 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
 
-import java.io.IOException;
-import java.nio.charset.Charset;
-
-@Component
+@Service
 public class DefaultHttpConnectionService implements IHttpConnectionService {
 
     private Logger logger = LoggerFactory.getLogger(getClass());
     CloseableHttpClient httpclient = HttpClients.createDefault();
 
+    @Value("${openpos.http.client.connect.timeout:2000}")
+    private int connectTimeout;
+
+    @Value("${openpos.http.client.socket.timeout:2000}")
+    private int socketTimeout;
+
+    @Value("${openpos.http.client.connection.request.timeout:1}")
+    private int connectionRequestTimeout;
+
     @Override
     public byte[] get(String url) throws IOException {
         byte[] data;
+        RequestConfig requestConfig = RequestConfig.custom()
+                .setConnectionRequestTimeout(connectionRequestTimeout).setConnectTimeout(connectTimeout).setSocketTimeout(socketTimeout).build();
         HttpGet httpGet = new HttpGet(url);
+        httpGet.setConfig(requestConfig);
         logRequest(httpGet);
-        try(CloseableHttpResponse response = httpclient.execute(httpGet)){
+        try (CloseableHttpResponse response = httpclient.execute(httpGet)) {
             logResponse(response);
-            if( response.getStatusLine().getStatusCode() >= 200 && response.getStatusLine().getStatusCode() < 300) {
+            if (response.getStatusLine().getStatusCode() >= 200 && response.getStatusLine().getStatusCode() < 300) {
                 HttpEntity entity = response.getEntity();
                 data = EntityUtils.toByteArray(entity);
                 EntityUtils.consume(entity);
             } else {
                 throw new HttpResponseException(response.getStatusLine().getStatusCode(), response.getStatusLine().getReasonPhrase());
             }
-
-        } catch ( Exception e ){
+        } catch (Exception e) {
             throw new IOException("Http get failed to get url: " + url, e);
         }
-
         return data;
     }
 
