fde7192b6038dc6b70205c3ea4edab43c656ac22
Jason Mihalick <jason@jcraftsmen.com>
2019-10-15 14:01:13 -0400

    Add method for getting FQDN of host

--- openpos-util/src/main/java/org/jumpmind/pos/util/NetworkUtils.java
+++ openpos-util/src/main/java/org/jumpmind/pos/util/NetworkUtils.java
@@ -3,8 +3,16 @@ package org.jumpmind.pos.util;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
+import java.util.concurrent.atomic.AtomicReference;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class NetworkUtils {
+    static final Logger logger = LoggerFactory.getLogger(NetworkUtils.class);
+
+    static AtomicReference<String> FULLY_QUALIFIED_HOSTNAME = new AtomicReference<String>(null);
 
     /**
      * Reports whether or not the given TCP port is already in use.
@@ -19,5 +27,30 @@ public class NetworkUtils {
         } catch (Exception ex) {
             return false;
         }
+    }
+
+    /**
+     * Attempts to return the fully qualified domain name of localhost.
+     * @return
+     */
+    public static String getNetworkHostname() {
+        if (FULLY_QUALIFIED_HOSTNAME.get() == null) {
+            try {
+                String hostName = InetAddress.getLocalHost().getCanonicalHostName();
+                
+                if (StringUtils.isBlank(hostName)) {
+                    hostName = InetAddress.getByName(
+                            InetAddress.getLocalHost().getHostAddress()).getHostName();
+                }
+                
+                if (StringUtils.isNotBlank(hostName)) {
+                    hostName = hostName.trim();
+                }
+                FULLY_QUALIFIED_HOSTNAME.compareAndSet(null, hostName);
+            } catch (Exception ex) {
+                logger.info("Unable to lookup hostname: " + ex);
+            }
+        }
+        return FULLY_QUALIFIED_HOSTNAME.get();
     }
 }
