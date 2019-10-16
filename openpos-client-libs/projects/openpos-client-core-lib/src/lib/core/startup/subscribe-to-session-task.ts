65d9015062c246d8bdefb58865e2140fefc0c64d
Jason Mihalick <jason@jcraftsmen.com>
2019-10-16 18:16:12 -0400

    Ensure connection exists or fail startup
    - When running in managed mode, need to make sure that the connection is
    really there because if the management server was unable to resolve a
    URL for the device, it will return null and then the session service
    won't establish a connection.

--- openpos-client-libs/projects/openpos-client-core-lib/src/lib/core/startup/subscribe-to-session-task.ts
+++ openpos-client-libs/projects/openpos-client-core-lib/src/lib/core/startup/subscribe-to-session-task.ts
@@ -1,17 +1,20 @@
 import { Injectable } from '@angular/core';
 import { IStartupTask } from './startup-task.interface';
-import { Observable, Subject } from 'rxjs';
+import { Observable, Subject, timer, Subscription } from 'rxjs';
 import { SessionService } from '../services/session.service';
 import { Router } from '@angular/router';
 import { Logger } from '../services/logger.service';
 import { StartupTaskData } from './startup-task-data';
 import { StartupTaskNames } from './startup-task-names';
+import { takeUntil } from 'rxjs/operators';
 
 @Injectable()
 export class SubscribeToSessionTask implements IStartupTask {
     name = StartupTaskNames.SUBSCRIBE_TO_SESSION;
     order = 600;
 
+    private connectionTimeoutSubscr: Subscription;
+    
     constructor(
         protected session: SessionService,
         protected router: Router,
@@ -41,11 +44,41 @@ export class SubscribeToSessionTask implements IStartupTask {
                 message.next(`[StartupService] Subscribing to server using appId '${this.session.getAppId()}'...`);
                 this.session.unsubscribe();
                 await this.session.subscribe();
-                message.complete();
+
+                const connected = await this.confirmConnection();                
+                if (connected) {
+                    message.complete();
+                } else {
+                    message.error(`A connection to the server could not be established.`);
+                }
             } else {
                 /* we shouldn't be coming there here if we are already subscribed.  lets do a refresh to get a clean start */
                 window.location.reload();
             }
+        });
+    }
+
+    protected confirmConnection(maxWaitMillis = 5000): Promise<boolean> {
+        return new Promise<boolean>((resolve, reject) => { 
+            this.connectionTimeoutSubscr = timer(0, 500).pipe(takeUntil(timer(maxWaitMillis))).subscribe(
+                async x => {
+                    if (this.session.connected()) {
+                        this.connectionTimeoutSubscr.unsubscribe();
+                        resolve(true);
+                    } else {
+                        this.log.debug(`Session not connected yet`)
+                    }
+                },
+                (err) => {
+                    this.connectionTimeoutSubscr.unsubscribe();
+                    resolve(false)
+                },
+                () => {
+                    this.connectionTimeoutSubscr.unsubscribe();
+                    this.log.error(`Timed out waiting for connection to be established`);
+                    resolve(false);
+                }
+            );
         });
     }
 }
