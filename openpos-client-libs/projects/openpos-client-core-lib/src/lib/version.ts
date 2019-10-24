1985150836b2df547727b3045db2ebf666762d4e
maxwellpettit <maxwell.pettit@gmail.com>
2019-10-24 11:29:00 -0500

    Block disabled actions in session service (#366)

--- openpos-client-libs/projects/openpos-client-core-lib/src/lib/version.ts
+++ openpos-client-libs/projects/openpos-client-core-lib/src/lib/version.ts
@@ -1,4 +1,4 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
-export const VERSION = {"componentName":"openpos-client-core-lib","version":"0.4.2-SNAPSHOT","gitHash":"b23384061f22d7d1f8c556f0295c0e2f61b3f48e","buildTime":"2019-08-02 12:43:46","gitBranch":"feature/personalize-app"};
+export const VERSION = {"componentName":"openpos-client-core-lib","version":"0.4.10-SNAPSHOT","gitHash":"749e5b9c95cc944ca86676208987c17d63be17d9","buildTime":"2019-10-24 16:04:05","gitBranch":"releases/0.4"};
 /* tslint:enable */
