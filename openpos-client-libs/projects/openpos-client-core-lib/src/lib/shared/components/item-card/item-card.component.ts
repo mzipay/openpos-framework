881d8bf68b54d2bf709a926362d07ed1dc591287
Klementina S. Chirico <kstojanovska@jumpmind.com>
2019-11-01 09:20:28 -0400

    update the item-card to make hovering over the card more subtle than changing the background-color (#390)

--- openpos-client-libs/projects/openpos-client-core-lib/src/lib/shared/components/item-card/item-card.component.ts
+++ openpos-client-libs/projects/openpos-client-core-lib/src/lib/shared/components/item-card/item-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, HostListener } from '@angular/core';
 import { ISellItem } from '../../../core/interfaces/sell-item.interface';
 import { SessionService } from '../../../core/services/session.service';
 import { IActionItem } from '../../../core/actions/action-item.interface';
@@ -15,6 +15,9 @@ export class ItemCardComponent {
   @Input() item: ISellItem;
   @Input() isReadOnly = false;
   @Input() expanded = true;
+  @Input() enableHover = true;
+
+  public hover = false;
 
   constructor(public actionService: ActionService, public session: SessionService) { }
 
@@ -29,5 +32,17 @@ export class ItemCardComponent {
     }
     return enabled;
   }
+
+  @HostListener('mouseenter')
+  onMouseEnter() {
+    if (this.enableHover) {
+      this.hover = true;
+    }
+  }
+
+  @HostListener('mouseleave')
+  onMouseLeave() {
+    this.hover = false;
+  }
 
 }
