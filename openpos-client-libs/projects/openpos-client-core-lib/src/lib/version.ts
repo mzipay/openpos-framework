6bf1abb758c8a8d07c28cb2c155058708ffebc4b
maxwellpettit <maxwell.pettit@gmail.com>
2019-11-01 11:44:22 -0500

    Support unformatting eu currency

--- openpos-client-libs/projects/openpos-client-core-lib/src/lib/version.ts
+++ openpos-client-libs/projects/openpos-client-core-lib/src/lib/version.ts
@@ -1,4 +1,4 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
-export const VERSION = {"componentName":"openpos-client-core-lib","version":"0.4.10-SNAPSHOT","gitHash":"749e5b9c95cc944ca86676208987c17d63be17d9","buildTime":"2019-10-24 16:04:05","gitBranch":"releases/0.4"};
+export const VERSION = {"componentName":"openpos-client-core-lib","version":"0.4.7-urbn-SNAPSHOT","gitHash":"53541aa09490b05633426238dce4aec5338a5940","buildTime":"2019-11-01 16:32:26","gitBranch":"urbn/0.4"};
 /* tslint:enable */
