d4dda7bc7724788d88cb65d553ab2b54e1629076
Chris Henson <chenson42@users.noreply.github.com>
2019-12-15 16:31:03 -0500

    App locks up after an unexpected error (#474)

--- openpos-flow/src/main/java/org/jumpmind/pos/core/error/DefaultErrorHandler.java
+++ openpos-flow/src/main/java/org/jumpmind/pos/core/error/DefaultErrorHandler.java
@@ -1,6 +1,8 @@
 package org.jumpmind.pos.core.error;
 
 import org.jumpmind.pos.core.flow.IStateManager;
+import org.jumpmind.pos.core.ui.Toast;
+import org.jumpmind.pos.core.ui.UIMessage;
 import org.jumpmind.pos.server.service.IMessageService;
 import org.jumpmind.pos.util.model.Message;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,8 +20,11 @@ public class DefaultErrorHandler implements IErrorHandler {
     @Override
     public void handleError(IStateManager stateManager, Throwable throwable) {
         Message message = incidentService.createIncident(throwable, new IncidentContext(stateManager.getDeviceId()));
-
-        if( message != null ) {
+        if(message instanceof UIMessage) {
+            stateManager.showScreen((UIMessage) message);
+        } else if (message instanceof Toast) {
+            stateManager.showToast((Toast)message);
+        } else {
             messageService.sendMessage(stateManager.getAppId(), stateManager.getDeviceId(), message);
         }
     }
