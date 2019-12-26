55466d908299d20ecd3bab909bc507fd3fe59085
Chris Henson <chenson42@users.noreply.github.com>
2019-12-26 09:17:40 -0500

    Add selectedAction to ActionItemGroup (#504)

--- openpos-client-libs/projects/openpos-client-core-lib/src/lib/core/actions/action-item-group.interface.ts
+++ openpos-client-libs/projects/openpos-client-core-lib/src/lib/core/actions/action-item-group.interface.ts
@@ -3,5 +3,6 @@ import { IActionItem } from './action-item.interface';
 export interface IActionItemGroup {
     title: string;
     keybind: string;
+    selectedAction: string;
     actionItems: IActionItem[];
 }
