d32b23330115ed4c5b01869a017249b29daefc66
Jason Mihalick <jrmihalick@users.noreply.github.com>
2020-01-10 11:54:42 -0500

    Rearrange numeric keyboard to standard layout like on phones (#530)

--- openpos-client-libs/projects/openpos-client-core-lib/src/lib/keyboard/configs/keyboard-layouts.config.ts
+++ openpos-client-libs/projects/openpos-client-core-lib/src/lib/keyboard/configs/keyboard-layouts.config.ts
@@ -137,16 +137,16 @@ const keyboardLayouts: IKeyboardLayouts = {
         [KeyboardClassKey.Bksp, KeyboardClassKey.Bksp, KeyboardClassKey.Bksp]
       ],
       [
-        ['7', '7', '7'], ['8', '8', '8'], ['9', '9', '9']
+        ['1', '1', '1'], ['2', '2', '2'], ['3', '3', '3']
       ],
       [
         ['4', '4', '4'], ['5', '5', '5'], ['6', '6', '6']
       ],
       [
-        ['1', '1', '1'], ['2', '2', '2'], ['3', '3', '3']
+        ['7', '7', '7'], ['8', '8', '8'], ['9', '9', '9']
       ],
       [
-        ['0', '0', '0'], ['.', '.', '.'], [KeyboardClassKey.Enter, KeyboardClassKey.Enter, KeyboardClassKey.Enter]
+        ['.', '.', '.'], ['0', '0', '0'], [KeyboardClassKey.Enter, KeyboardClassKey.Enter, KeyboardClassKey.Enter]
       ]
     ],
     'lang': ['numeric']
