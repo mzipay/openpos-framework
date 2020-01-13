4f5b07499adf9616307773a962a5ffdae40770e4
Chris Henson <chenson42@users.noreply.github.com>
2020-01-13 08:39:32 -0500

    Customer display shows events from other devices (#535)

--- openpos-flow/src/main/java/org/jumpmind/pos/core/flow/EventBroadcaster.java
+++ openpos-flow/src/main/java/org/jumpmind/pos/core/flow/EventBroadcaster.java
@@ -29,32 +29,36 @@ public class EventBroadcaster {
 
     public boolean postEventToObject(Class clazz, Object object, Event event) {
         boolean foundMethod = false;
-        List<Method> methods = MethodUtils.getMethodsListWithAnnotation(clazz, OnEvent.class, true, true);
-        if (methods != null && !methods.isEmpty()) {
-            if (object == null) {
-                try {
-                    object = clazz.newInstance();
-                    stateManager.performInjections(object);
-                } catch (InstantiationException | IllegalAccessException ex) {
-                    throw new FlowException("Failed to create event handler of type " + clazz.getName(), ex);
-                } catch (Exception ex) {
-                    throw new FlowException("Failed to inject values on the event handler of type " + clazz.getName(), ex);
-                }
-            }
-            for (Method method : methods) {
-                try {
-                    OnEvent onEvent = method.getAnnotation(OnEvent.class);
-                    if (onEvent.receiveEventsFromSelf() || !event.getSource().equals(AppEvent.createSourceString(stateManager.getAppId(), stateManager.getDeviceId()))) {
-                        method.setAccessible(true);
-                        foundMethod = true;
-                        if (method.getParameters() != null && method.getParameters().length == 1 && method.getParameterTypes()[0].isAssignableFrom(event.getClass())) {
-                            method.invoke(object, event);
-                        } else if (method.getParameters() == null || method.getParameters().length == 0) {
-                            method.invoke(object);
-                        }
+        // if an appevent then only receive events from the same device id
+        if (!(event instanceof AppEvent) || ((AppEvent) event).getDeviceId().equals(stateManager.getDeviceId())) {
+            List<Method> methods = MethodUtils.getMethodsListWithAnnotation(clazz, OnEvent.class, true, true);
+            if (methods != null && !methods.isEmpty()) {
+                if (object == null) {
+                    try {
+                        object = clazz.newInstance();
+                        stateManager.performInjections(object);
+                    } catch (InstantiationException | IllegalAccessException ex) {
+                        throw new FlowException("Failed to create event handler of type " + clazz.getName(), ex);
+                    } catch (Exception ex) {
+                        throw new FlowException("Failed to inject values on the event handler of type " + clazz.getName(), ex);
                     }
-                } catch (Exception ex) {
-                    throw new FlowException("Failed to execute method on state. Method: " + method + " object: " + object, ex);
+                }
+                for (Method method : methods) {
+                    try {
+                        OnEvent onEvent = method.getAnnotation(OnEvent.class);
+                        if (onEvent.receiveEventsFromSelf() ||
+                                !event.getSource().equals(AppEvent.createSourceString(stateManager.getAppId(), stateManager.getDeviceId()))) {
+                            method.setAccessible(true);
+                            foundMethod = true;
+                            if (method.getParameters() != null && method.getParameters().length == 1 && method.getParameterTypes()[0].isAssignableFrom(event.getClass())) {
+                                method.invoke(object, event);
+                            } else if (method.getParameters() == null || method.getParameters().length == 0) {
+                                method.invoke(object);
+                            }
+                        }
+                    } catch (Exception ex) {
+                        throw new FlowException("Failed to execute method on state. Method: " + method + " object: " + object, ex);
+                    }
                 }
             }
         }
