d951cf552c4fb4b3b155d0e07df1383dfbe604a8
Chris Henson <chenson42@users.noreply.github.com>
2020-02-05 17:23:15 -0500

    Support Enter and Esc with the confirmation dialog (#575)

--- openpos-client-libs/projects/openpos-client-core-lib/src/lib/core/components/confirmation-dialog/confirmation-dialog.component.ts
+++ openpos-client-libs/projects/openpos-client-core-lib/src/lib/core/components/confirmation-dialog/confirmation-dialog.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
-import { IConfirmationDialog } from '../../actions/confirmation-dialog.interface';
-import { MatDialogRef } from '@angular/material';
+import {Component, HostListener} from '@angular/core';
+import {IConfirmationDialog} from '../../actions/confirmation-dialog.interface';
+import {MatDialogRef} from '@angular/material';
 
 @Component({
-  selector: 'app-confirmation-dialog',
-  templateUrl: './confirmation-dialog.component.html',
-  styleUrls: ['./confirmation-dialog.component.scss']
+    selector: 'app-confirmation-dialog',
+    templateUrl: './confirmation-dialog.component.html',
+    styleUrls: ['./confirmation-dialog.component.scss']
 })
 export class ConfirmationDialogComponent {
 
@@ -14,7 +14,19 @@ export class ConfirmationDialogComponent {
     constructor(public dialogRef: MatDialogRef<ConfirmationDialogComponent>) {
     }
 
-    closeDialog(confirmed: boolean) {
-      this.dialogRef.close(confirmed);
+    @HostListener('document:keydown.escape', ['$event']) onEscape(event: KeyboardEvent) {
+        this.deny();
+    }
+
+    @HostListener('document:keydown.enter', ['$event']) onEnter(event: KeyboardEvent) {
+        this.confirm();
+    }
+
+    confirm() {
+        this.dialogRef.close(true);
+    }
+
+    deny() {
+        this.dialogRef.close(false);
     }
 }
