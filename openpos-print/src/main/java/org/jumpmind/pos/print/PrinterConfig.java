bc89f02f60a23236bdc846eb7ba0ab410e24b15a
Mark Michalek <mmichalek@jumpmind.com>
2020-05-05 17:20:07 -0400

    remove styleSheet from printer level configuration

--- openpos-print/src/main/java/org/jumpmind/pos/print/PrinterConfig.java
+++ openpos-print/src/main/java/org/jumpmind/pos/print/PrinterConfig.java
@@ -1,43 +1,14 @@
 package org.jumpmind.pos.print;
 
+import lombok.Data;
+
 import java.util.Map;
 
+@Data
 public class PrinterConfig {
 
     boolean enabled = true;
     PrinterTypeCode type;
     Map<String, Object> settings;
-    String styleSheet;
 
-    public PrinterTypeCode getType() {
-        return type;
-    }
-
-    public void setType(PrinterTypeCode type) {
-        this.type = type;
-    }
-
-    public Map<String, Object> getSettings() {
-        return settings;
-    }
-
-    public void setSettings(Map<String, Object> settings) {
-        this.settings = settings;
-    }
-
-    public String getStyleSheet() {
-        return styleSheet;
-    }
-
-    public void setStyleSheet(String styleSheet) {
-        this.styleSheet = styleSheet;
-    }
-
-    public boolean isEnabled() {
-        return enabled;
-    }
-
-    public void setEnabled(boolean enabled) {
-        this.enabled = enabled;
-    }
 }
