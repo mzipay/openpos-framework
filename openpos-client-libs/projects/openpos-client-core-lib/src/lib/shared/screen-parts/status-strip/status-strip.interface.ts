a0783dde9ca91f5d9acc58eac243d6a7a5d4bed3
Dan Kaste <dan.kaste@leadingedje.com>
2020-05-08 10:17:13 -0400

    add transaction number to status strip (#689)

--- openpos-client-libs/projects/openpos-client-core-lib/src/lib/shared/screen-parts/status-strip/status-strip.interface.ts
+++ openpos-client-libs/projects/openpos-client-core-lib/src/lib/shared/screen-parts/status-strip/status-strip.interface.ts
@@ -5,4 +5,5 @@ export interface StatusStripInterface {
     registerNumber: string;
     systemStatus: ISystemStatus;
     timestampBegin: number;
+    transactionNumber: string;
 }
--- openpos-flow/src/main/java/org/jumpmind/pos/core/ui/messagepart/StatusStripPart.java
+++ openpos-flow/src/main/java/org/jumpmind/pos/core/ui/messagepart/StatusStripPart.java
@@ -1,10 +1,12 @@
 package org.jumpmind.pos.core.ui.messagepart;
 
+import lombok.Data;
 import org.jumpmind.pos.core.model.SystemStatus;
 
 import java.io.Serializable;
 import java.util.Date;
 
+@Data
 public class StatusStripPart implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
@@ -12,29 +14,6 @@ public class StatusStripPart implements Serializable {
     private String registerNumber;
     private SystemStatus systemStatus;
     private Date timestampBegin;
-    
-	public String getStoreNumber() {
-		return storeNumber;
-	}
-	public void setStoreNumber(String storeNumber) {
-		this.storeNumber = storeNumber;
-	}
-	public String getRegisterNumber() {
-		return registerNumber;
-	}
-	public void setRegisterNumber(String registerNumber) {
-		this.registerNumber = registerNumber;
-	}
-	public SystemStatus getSystemStatus() {
-		return systemStatus;
-	}
-	public void setSystemStatus(SystemStatus systemStatus) {
-		this.systemStatus = systemStatus;
-	}
-	public Date getTimestampBegin() {
-		return timestampBegin;
-	}
-	public void setTimestampBegin(Date timestampBegin) {
-		this.timestampBegin = timestampBegin;
-	}
+    private String transactionNumber;
+
 }
