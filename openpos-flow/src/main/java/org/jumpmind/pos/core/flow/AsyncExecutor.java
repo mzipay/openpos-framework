9e5725ecbe847d559cfedfcbd3454fbe6948ab86
maxwellpettit <maxwell.pettit@gmail.com>
2020-05-12 11:07:36 -0500

    Set the current state manager when executing AsyncExecutor (#698)

--- openpos-flow/src/main/java/org/jumpmind/pos/core/flow/AsyncExecutor.java
+++ openpos-flow/src/main/java/org/jumpmind/pos/core/flow/AsyncExecutor.java
@@ -1,5 +1,7 @@
 package org.jumpmind.pos.core.flow;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.stereotype.Component;
 
@@ -9,14 +11,18 @@ import java.util.List;
 import java.util.concurrent.ScheduledFuture;
 import java.util.function.Consumer;
 import java.util.function.Function;
-import java.util.function.Supplier;
 
 @Component
+@Scope("device")
 public class AsyncExecutor {
-
     private ThreadPoolTaskScheduler scheduler;
-
     private List<ScheduledFuture<?>> scheduledJobs;
+
+    @In(scope = ScopeType.Device)
+    private IStateManager stateManager;
+
+    @Autowired
+    private StateManagerContainer stateManagerContainer;
 
     @PostConstruct
     public void init() {
@@ -27,9 +33,10 @@ public class AsyncExecutor {
         scheduledJobs = new ArrayList<ScheduledFuture<?>>();
     }
 
-    public <T,R> void execute(T request, Function<T, R> doWork, Consumer<R> handleResult, Consumer<Throwable> handleError) {
+    public <T, R> void execute(T request, Function<T, R> doWork, Consumer<R> handleResult, Consumer<Throwable> handleError) {
         scheduler.execute(() -> {
             try {
+                stateManagerContainer.setCurrentStateManager(stateManager);
                 R result = doWork.apply(request);
                 handleResult.accept(result);
             } catch (Throwable throwable) {
@@ -37,5 +44,4 @@ public class AsyncExecutor {
             }
         });
     }
-
 }
