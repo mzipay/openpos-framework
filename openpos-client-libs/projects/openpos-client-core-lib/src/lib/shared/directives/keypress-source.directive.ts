d91de9bc4ac8ed92514785e906a7ebdebb5e3d6d
Andy Blubaugh <blubaugha@users.noreply.github.com>
2020-05-14 14:47:37 -0400

    Updated KeypressSourceDirective and KeypressProvider to support key modifiers

--- openpos-client-libs/projects/openpos-client-core-lib/src/lib/shared/directives/keypress-source.directive.ts
+++ openpos-client-libs/projects/openpos-client-core-lib/src/lib/shared/directives/keypress-source.directive.ts
@@ -1,17 +1,18 @@
-import { Directive, ElementRef, OnInit, HostListener, Renderer2 } from '@angular/core';
-import { KeyPressProvider } from '../providers/keypress.provider';
-import { Subject } from 'rxjs';
+import {Directive, ElementRef, OnDestroy, OnInit, Renderer2} from '@angular/core';
+import {KeyPressProvider} from '../providers/keypress.provider';
+import {fromEvent, merge, Subject} from 'rxjs';
+import {filter, takeUntil, tap} from 'rxjs/operators';
 
 @Directive({
     selector: '[appKeypressSource]',
     providers: [KeyPressProvider]
 })
-export class KeyPressSourceDirective implements OnInit {
-
+export class KeyPressSourceDirective implements OnInit, OnDestroy {
     keydown$ = new Subject<KeyboardEvent>();
     keyup$ = new Subject<KeyboardEvent>();
+    destroyed$ = new Subject();
 
-    constructor( private renderer: Renderer2, private el: ElementRef, private keyPressProvider: KeyPressProvider) {
+    constructor(private renderer: Renderer2, private el: ElementRef, private keyPressProvider: KeyPressProvider) {
     }
 
     ngOnInit(): void {
@@ -19,22 +20,39 @@ export class KeyPressSourceDirective implements OnInit {
         this.keyPressProvider.registerKeyPressSource(this.keydown$);
         // Need to do this so that this element can grab key events
         this.renderer.setAttribute(this.el.nativeElement, 'tabindex', '0');
+
+        const keydownEvent$ = fromEvent<KeyboardEvent>(window, 'keydown');
+        const keyupEvent$ = fromEvent<KeyboardEvent>(window, 'keyup');
+
+        merge(keydownEvent$, keyupEvent$).pipe(
+            filter(event => this.el.nativeElement.contains(event.target)),
+            tap(event => event.type === 'keydown' ? this.keydown$.next(event) : this.keyup$.next(event)),
+            filter(event => this.keyPressProvider.keyHasSubscribers(event)),
+            tap(event => event.stopPropagation()),
+            tap(event => event.preventDefault()),
+            takeUntil(this.destroyed$)
+        ).subscribe();
     }
 
-
-    @HostListener('keydown', ['$event'])
-    onkeydown( event: KeyboardEvent) {
-        this.keydown$.next(event);
-        if (this.keyPressProvider.keyHasSubscribers(event.key)) {
-            event.stopPropagation();
-        }
+    ngOnDestroy(): void {
+        this.keyPressProvider.unregisterKeyPressSource(this.keyup$);
+        this.keyPressProvider.unregisterKeyPressSource(this.keydown$);
+        this.destroyed$.next();
     }
 
-    @HostListener('keyup', ['$event'])
-    onkeyup( event: KeyboardEvent) {
-        this.keyup$.next(event);
-        if (this.keyPressProvider.keyHasSubscribers(event.key)) {
-            event.stopPropagation();
-        }
-    }
+    // @HostListener('keydown', ['$event'])
+    // onkeydown(event: KeyboardEvent) {
+    //     this.keydown$.next(event);
+    //     if (this.keyPressProvider.keyHasSubscribers(event)) {
+    //         event.stopPropagation();
+    //     }
+    // }
+    //
+    // @HostListener('keyup', ['$event'])
+    // onkeyup(event: KeyboardEvent) {
+    //     this.keyup$.next(event);
+    //     if (this.keyPressProvider.keyHasSubscribers(event)) {
+    //         event.stopPropagation();
+    //     }
+    // }
 }
--- openpos-client-libs/projects/openpos-client-core-lib/src/lib/shared/providers/keypress.provider.ts
+++ openpos-client-libs/projects/openpos-client-core-lib/src/lib/shared/providers/keypress.provider.ts
@@ -1,35 +1,54 @@
-import { Injectable, OnDestroy } from '@angular/core';
-import { Observable, merge, Subscription } from 'rxjs';
+import {Injectable, OnDestroy} from '@angular/core';
+import {merge, Observable, Subject, Subscription} from 'rxjs';
+import {takeUntil} from 'rxjs/operators';
 
 @Injectable()
 export class KeyPressProvider implements OnDestroy {
-
     private keyPressSources: Observable<KeyboardEvent>[] = [];
-    private subscription: Subscription;
-
     private subscribers = new Map<string, Map<number, KeybindSubscription>>();
+    destroyed$ = new Subject();
+    keypressSourceRegistered$ = new Subject<Observable<KeyboardEvent>>();
+    keypressSourceUnregistered$ = new Subject<Observable<KeyboardEvent>>();
+    stopObserver$ = merge(this.destroyed$, this.keypressSourceRegistered$, this.keypressSourceUnregistered$);
+
+    constructor() {
+        merge(
+            this.keypressSourceRegistered$,
+            this.keypressSourceUnregistered$
+        ).subscribe(() => this.rebuildKeyPressObserver());
+    }
 
     ngOnDestroy(): void {
-        if (this.subscription) {
-            this.subscription.unsubscribe();
-        }
+        this.destroyed$.next();
     }
 
     registerKeyPressSource(source$: Observable<KeyboardEvent>) {
         this.keyPressSources.push(source$);
-        this.rebuildKeyPressObserver();
+        this.keypressSourceRegistered$.next(source$);
+    }
+
+    unregisterKeyPressSource(source$: Observable<KeyboardEvent>) {
+        const index = this.keyPressSources.indexOf(source$);
+
+        if (index >= 0) {
+            this.keyPressSources.splice(index, 1);
+        }
+
+        this.keypressSourceUnregistered$.next();
     }
 
     subscribe(key: string, priority: number, next: (KeyboardEvent) => void): Subscription {
-
-        if (key === null || key === undefined) {
-            console.warn('Cannot subscribe to null or undefined keybinding');
+        if (!key) {
+            console.warn('Cannot subscribe to null or undefined or empty string keybinding');
             return;
         }
+
+        key = this.getNormalizedKey(key);
 
         const subscriptionOutput = new Subscription(() => {
             const priorityMap = this.subscribers.get(key);
             const keybindSubscription = priorityMap.get(priority);
+
             if (keybindSubscription && keybindSubscription.subscription === subscriptionOutput) {
                 priorityMap.delete(priority);
             }
@@ -42,34 +61,108 @@ export class KeyPressProvider implements OnDestroy {
         if (this.subscribers.get(key).has(priority)) {
             console.warn(`Another subscriber already exists with key ${key} and priority ${priority}`);
         }
-        this.subscribers.get(key).set(priority, new KeybindSubscription(subscriptionOutput, priority, next));
+        this.subscribers.get(key).set(priority, new KeybindSubscription(key, subscriptionOutput, priority, next));
 
         return subscriptionOutput;
     }
 
-    keyHasSubscribers(key: string): boolean {
-        return this.subscribers.has(key);
+    keyHasSubscribers(obj: KeyboardEvent | Keybinding | string): boolean {
+        const key = this.getNormalizedKey(obj);
+        return this.subscribers.has(key) && this.subscribers.get(key).size > 0;
     }
 
-    private rebuildKeyPressObserver() {
-        if (this.subscription) {
-            this.subscription.unsubscribe();
-        }
+    rebuildKeyPressObserver() {
+        merge(...this.keyPressSources).pipe(
+            takeUntil(this.stopObserver$)
+        ).subscribe(event => {
+            const key = this.getNormalizedKey(event);
 
-        this.subscription = merge(...this.keyPressSources).subscribe(event => {
-            if (this.subscribers.has(event.key)) {
-                const priorityMap = this.subscribers.get(event.key);
-                const prioritiesList = Array.from(priorityMap.keys()).sort();
-                if (prioritiesList.length > 0) {
-                    const priority = prioritiesList[0];
-                    this.subscribers.get(event.key).get(priority).event(event);
-                }
+            if (!this.subscribers.has(key)) {
+                return;
+            }
+
+            const priorityMap = this.subscribers.get(key);
+            const prioritiesList = Array.from(priorityMap.keys()).sort();
+
+            if (prioritiesList.length > 0) {
+                const priority = prioritiesList[0];
+                this.subscribers.get(key).get(priority).event(event);
             }
         });
     }
 
+    getNormalizedKey(obj: KeyboardEvent | Keybinding | string): string {
+        let keyBinding = typeof obj === 'string' ? this.parse(obj as string) : obj as Keybinding;
+        let normalizedKey = '';
+
+        normalizedKey += (keyBinding.ctrlKey ? 'ctrl+' : '');
+        normalizedKey += (keyBinding.altKey ? 'alt+' : '');
+        normalizedKey += (keyBinding.shiftKey ? 'shift+' : '');
+        normalizedKey += (keyBinding.metaKey ? 'meta+' : '');
+        normalizedKey += keyBinding.key.toLowerCase();
+
+        return normalizedKey;
+    }
+
+    parse(key: string): Keybinding {
+        if (!key) {
+            return null;
+        }
+
+        const parts = key.split('+');
+        const keyBinding: Keybinding = {
+            key: parts[parts.length - 1].toLowerCase()
+        };
+
+        for (let i = 0; i < parts.length - 1; i++) {
+            const part = parts[i].toLowerCase();
+
+            // Being flexible with how developers want to specify key modifiers
+            switch (part) {
+                case 'command':
+                case 'cmd':
+                case 'mta':
+                case 'met':
+                case 'meta':
+                    keyBinding.metaKey = true;
+                    break;
+                case 'alt':
+                case 'opt':
+                case 'option':
+                case 'optn':
+                    keyBinding.altKey = true;
+                    break;
+                case 'ctl':
+                case 'ctr':
+                case 'ctrl':
+                case 'control':
+                    keyBinding.ctrlKey = true;
+                    break;
+                case 'shft':
+                case 'sft':
+                case 'shift':
+                    keyBinding.shiftKey = true;
+                    break;
+            }
+        }
+
+        return keyBinding;
+    }
 }
 
 class KeybindSubscription {
-    constructor(public subscription: Subscription, public priority: number, public event: (KeyboardEvent) => void) { }
+    constructor(
+        public key: string,
+        public subscription: Subscription,
+        public priority: number,
+        public event: (KeyboardEvent) => void) {
+    }
+}
+
+interface Keybinding {
+    key: string;
+    ctrlKey?: boolean;
+    altKey?: boolean;
+    shiftKey?: boolean;
+    metaKey?: boolean;
 }
