61b8d9029c765d4cf4363f5fe6770606898a7d62
Chris Henson <chenson42@users.noreply.github.com>
2020-06-10 18:02:43 -0400

    Allow scanning in generic dialogs (#733)

--- openpos-flow/src/main/java/org/jumpmind/pos/core/ui/messagepart/ScanPart.java
+++ openpos-flow/src/main/java/org/jumpmind/pos/core/ui/messagepart/ScanPart.java
@@ -1,21 +1,20 @@
 package org.jumpmind.pos.core.ui.messagepart;
 
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 
+@Data
+@Builder
+@NoArgsConstructor
 public class ScanPart implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private String scanActionName = "Scan";
 
     public ScanPart(String scanActionName) {
-        this.scanActionName = scanActionName;
-    }
-
-    public String getScanActionName() {
-        return scanActionName;
-    }
-
-    public void setScanActionName(String scanActionName) {
         this.scanActionName = scanActionName;
     }
 
