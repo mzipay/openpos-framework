b386b71f4914f9d9eef0d9d7b95727c9c38f3bdc
chenson42 <chenson42@gmail.com>
2020-06-11 07:43:17 -0400

    see if this fixed the openpos-framework build

--- openpos-service/src/main/java/org/jumpmind/pos/service/AbstractRDBMSModule.java
+++ openpos-service/src/main/java/org/jumpmind/pos/service/AbstractRDBMSModule.java
@@ -182,7 +182,7 @@ abstract public class AbstractRDBMSModule extends AbstractServiceFactory impleme
 
     @Override
     public String getURL() {
-        return getDbProperties(DB_POOL_URL, "jdbc:h2:mem:" + getName());
+        return getDbProperties(DB_POOL_URL, "jdbc:h2:mem:" + getName() + ";DB_CLOSE_ON_EXIT=FALSE");
     }
 
     @Override
