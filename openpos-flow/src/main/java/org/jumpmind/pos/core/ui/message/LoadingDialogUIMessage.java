44bfa3a1700c00fc5cebeaba100ce71eae13ef58
maxwellpettit <maxwell.pettit@gmail.com>
2020-06-19 12:09:29 -0700

    Add optional cancel button on loading screen (#752)

--- openpos-flow/src/main/java/org/jumpmind/pos/core/ui/message/LoadingDialogUIMessage.java
+++ openpos-flow/src/main/java/org/jumpmind/pos/core/ui/message/LoadingDialogUIMessage.java
@@ -1,10 +1,13 @@
 package org.jumpmind.pos.core.ui.message;
 
+import org.jumpmind.pos.core.ui.ActionItem;
 import org.jumpmind.pos.core.ui.UIMessage;
 
 public class LoadingDialogUIMessage extends UIMessage {
 
     private String message;
+
+    private ActionItem cancelButton;
 
     public LoadingDialogUIMessage() {
         this.setScreenType(UIMessageType.LOADING_DIALOG);
@@ -17,5 +20,13 @@ public class LoadingDialogUIMessage extends UIMessage {
 
     public void setMessage(String message) {
         this.message = message;
+    }
+
+    public ActionItem getCancelButton() {
+        return cancelButton;
+    }
+
+    public void setCancelButton(ActionItem cancelButton) {
+        this.cancelButton = cancelButton;
     }
 }
