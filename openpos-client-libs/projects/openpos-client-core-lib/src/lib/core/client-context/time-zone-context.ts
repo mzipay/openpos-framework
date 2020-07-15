0d87e8934bbd6f058e56c76678b8df00a8986349
Andy Blubaugh <blubaugha@users.noreply.github.com>
2020-07-15 15:55:57 -0400

    Bugfix/client timezone offset not in correct format (#798)
    * Fixed the client timezone offset not being in the correct ISO-8601 format

--- openpos-client-libs/projects/openpos-client-core-lib/src/lib/core/client-context/time-zone-context.ts
+++ openpos-client-libs/projects/openpos-client-core-lib/src/lib/core/client-context/time-zone-context.ts
@@ -1,12 +1,46 @@
-import { IClientContext } from './client-context-provider.interface';
-import { Injectable } from '@angular/core';
+import {IClientContext} from './client-context-provider.interface';
+import {Injectable} from '@angular/core';
 
 @Injectable()
 export class TimeZoneContext implements IClientContext {
     getContextProperties(): Map<string, string> {
         return new Map([
-            ['timezoneOffset', (new Date().getTimezoneOffset() / 60).toString()]
+            ['timezoneOffset', this.getOffset()]
         ]);
     }
 
+    /**
+     * Gets the timezone offset in the ISO-8601 format.
+     *
+     * Examples:
+     *
+     *   +5:30   when timezone is ahead of UTC
+     *   -01:00  when timezone is behind of UTC
+     *   Z       when timezone is UTC
+     */
+    getOffset(): string {
+        const timezoneOffsetMinutes = new Date().getTimezoneOffset();
+
+        // If offset is 0, it means timezone is UTC
+        if (timezoneOffsetMinutes === 0) {
+            return 'Z';
+        }
+
+        const absTimezoneOffsetHours = Math.abs(timezoneOffsetMinutes / 60);
+        const absTimezoneOffsetMinutes = Math.abs(timezoneOffsetMinutes % 60);
+        let offsetHours = absTimezoneOffsetHours.toString(10);
+        let offsetMinutes = absTimezoneOffsetMinutes.toString(10);
+
+        if (absTimezoneOffsetHours < 10) {
+            offsetHours = `0${offsetHours}`;
+        }
+
+        if (absTimezoneOffsetMinutes < 10) {
+            offsetMinutes = `0${offsetMinutes}`;
+        }
+
+        // Add an opposite sign to the offset
+        const direction = timezoneOffsetMinutes < 0 ? '+' : '-';
+        return `${direction}${offsetHours}:${offsetMinutes}`;
+    }
 }
