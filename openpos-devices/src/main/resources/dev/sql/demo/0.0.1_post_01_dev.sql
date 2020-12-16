34e76ab94269edfbb1cd5c155ab5401b3e74f545
chenson42 <chenson42@gmail.com>
2020-12-16 08:22:36 -0500

    Remove demo script

--- openpos-devices/src/main/resources/dev/sql/demo/0.0.1_post_01_dev.sql
+++
@@ -1,2 +0,0 @@
-insert into dev_device (device_id, app_id, device_type, locale, timezone_offset, business_unit_id, description, create_time, create_by, last_update_time, last_update_by, TAG_REGION, TAG_COUNTRY, TAG_STATE, TAG_STORE_NUMBER)
-values ('05243-013', 'pos', null,'en_US','-05:00','05243',null,{ts '2018-12-24 15:40:03.000'},'openpos-devices',{ts '2018-12-27 14:21:11.000'},'startup','*','*','*','*');
