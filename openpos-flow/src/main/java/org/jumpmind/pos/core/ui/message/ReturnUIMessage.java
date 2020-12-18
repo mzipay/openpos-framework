894b89a1f05f387cb649efb3246f45b3eb28a312
Andy Blubaugh <blubaugha@users.noreply.github.com>
2020-12-18 14:48:53 -0500

    Fixed the linked loyalty button not showing on the returns screen (#1028)

--- openpos-flow/src/main/java/org/jumpmind/pos/core/ui/message/ReturnUIMessage.java
+++ openpos-flow/src/main/java/org/jumpmind/pos/core/ui/message/ReturnUIMessage.java
@@ -1,5 +1,6 @@
 package org.jumpmind.pos.core.ui.message;
 
+import lombok.Data;
 import org.jumpmind.pos.core.model.Total;
 import org.jumpmind.pos.core.ui.ActionItem;
 import org.jumpmind.pos.core.ui.AssignKeyBindings;
@@ -10,6 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @AssignKeyBindings
+@Data
 public class ReturnUIMessage extends UIMessage {
     private static final long serialVersionUID = 1L;
 
@@ -23,6 +25,7 @@ public class ReturnUIMessage extends UIMessage {
 
     private ActionItem checkoutButton;
     private ActionItem loyaltyButton;
+    private ActionItem linkedCustomerButton;
     private ActionItem removeReceiptAction;
 
     private boolean transactionActive = false;
@@ -39,47 +42,11 @@ public class ReturnUIMessage extends UIMessage {
         this.setId("returns");
     }
 
-    public String getProviderKey() {
-        return providerKey;
-    }
-
-    public void setProviderKey(String providerKey) {
-        this.providerKey = providerKey;
-    }
-
-    public List<Total> getTotals() {
-        return totals;
-    }
-
-    public void setTotals(List<Total> totals) {
-        this.totals = totals;
-    }
-
     public void addTotal(String name, String amount) {
         if (totals == null) {
             totals = new ArrayList<>();
         }
         totals.add(new Total(name, amount));
-    }
-
-    public Total getGrandTotal() {
-        return grandTotal;
-    }
-
-    public void setGrandTotal(Total grandTotal) {
-        this.grandTotal = grandTotal;
-    }
-
-    public void setGrandTotal(String name, String amount) {
-        this.grandTotal = new Total(name, amount);
-    }
-
-    public List<TransactionReceipt> getReceipts() {
-        return receipts;
-    }
-
-    public void setReceipts(List<TransactionReceipt> receipts) {
-        this.receipts = receipts;
     }
 
     public void addReceipt(TransactionReceipt receipt) {
@@ -88,77 +55,4 @@ public class ReturnUIMessage extends UIMessage {
         }
         this.receipts.add(receipt);
     }
-
-    public ActionItem getCheckoutButton() {
-        return checkoutButton;
-    }
-
-    public void setCheckoutButton(ActionItem checkoutButton) {
-        this.checkoutButton = checkoutButton;
-    }
-
-    public ActionItem getLoyaltyButton() {
-        return loyaltyButton;
-    }
-
-    public void setLoyaltyButton(ActionItem loyaltyButton) {
-        this.loyaltyButton = loyaltyButton;
-    }
-
-    public ActionItem getRemoveReceiptAction() {
-        return  removeReceiptAction;
-    }
-
-    public void setRemoveReceiptAction(ActionItem removeReceiptAction) {
-        this.removeReceiptAction = removeReceiptAction;
-    }
-
-    public Total getItemCount() {
-        return itemCount;
-    }
-
-    public void setItemCount(Total itemCount) {
-        this.itemCount = itemCount;
-    }
-
-    public void setTransactionActive(boolean isTransactionActive) {
-        this.transactionActive = isTransactionActive;
-    }
-
-    public boolean isTransactionActive() {
-        return transactionActive;
-    }
-
-    public UICustomer getCustomer() {
-        return customer;
-    }
-
-    public void setCustomer(UICustomer customer) {
-        this.customer = customer;
-    }
-
-    public boolean isLocationEnabled() {
-        return locationEnabled;
-    }
-
-    public void setLocationEnabled(boolean locationEnabled) {
-        this.locationEnabled = locationEnabled;
-    }
-
-    public String getLocationOverridePrompt() {
-        return locationOverridePrompt;
-    }
-
-    public void setLocationOverridePrompt(String locationOverridePrompt) {
-        this.locationOverridePrompt = locationOverridePrompt;
-    }
-
-    public boolean isEnableCollapsibleItems() {
-        return enableCollapsibleItems;
-    }
-
-    public void setEnableCollapsibleItems(boolean enableCollapsibleItems) {
-        this.enableCollapsibleItems = enableCollapsibleItems;
-    }
-
 }
