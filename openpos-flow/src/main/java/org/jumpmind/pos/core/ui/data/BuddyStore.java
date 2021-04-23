e1aa2a13bdaae0bbdc648b6b116d885be94c2396
Anton Pomanytskyi <77682108+apomanytskyi@users.noreply.github.com>
2021-04-23 14:48:03 +0300

    Mark BuddyStore as serializable (#1291)
    Co-authored-by: Anton Pomanytskyi <pomanitska@ae.com>

--- openpos-flow/src/main/java/org/jumpmind/pos/core/ui/data/BuddyStore.java
+++ openpos-flow/src/main/java/org/jumpmind/pos/core/ui/data/BuddyStore.java
@@ -5,11 +5,13 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class BuddyStore {
+public class BuddyStore implements Serializable {
     private String storeName;
     private String storeCount;
 }
