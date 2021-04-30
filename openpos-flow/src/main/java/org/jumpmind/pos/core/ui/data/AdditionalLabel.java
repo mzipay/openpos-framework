93b202410615a256dc872abe5ec60811804d7781
Kevin DeLorey <kevin.delorey@leadingedje.com>
2021-04-30 14:52:00 -0600

    Sale Promo Details (#1310)

--- openpos-flow/src/main/java/org/jumpmind/pos/core/ui/data/AdditionalLabel.java
+++ openpos-flow/src/main/java/org/jumpmind/pos/core/ui/data/AdditionalLabel.java
@@ -1,32 +1,17 @@
 package org.jumpmind.pos.core.ui.data;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.io.Serializable;
 
+@Getter
+@Setter
+@AllArgsConstructor
 public class AdditionalLabel implements Serializable {
-
     private static final long serialVersionUID = 1L;
 
     private String label;
     private String value;
-
-    public AdditionalLabel(String label, String value) {
-        this.label = label;
-        this.value = value;
-    }
-
-    public String getLabel() {
-        return label;
-    }
-
-    public void setLabel(String label) {
-        this.label = label;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
-    }
 }
--- openpos-flow/src/main/java/org/jumpmind/pos/core/ui/messagepart/PromptButtonRowPart.java
+++ openpos-flow/src/main/java/org/jumpmind/pos/core/ui/messagepart/PromptButtonRowPart.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.jumpmind.pos.core.ui.ActionItem;
+import org.jumpmind.pos.server.model.Action;
 
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -18,6 +19,10 @@ public class PromptButtonRowPart implements Serializable {
     ActionItem primaryButton;
     List<ActionItem> secondaryButtons = new ArrayList<>();
     List<ActionItem> warnButtons = new ArrayList<>();
+
+    public PromptButtonRowPart(ActionItem primaryButton) {
+        this.primaryButton = primaryButton;
+    }
 
     public void addSecondaryButton(ActionItem button) {
         this.secondaryButtons.add(button);
