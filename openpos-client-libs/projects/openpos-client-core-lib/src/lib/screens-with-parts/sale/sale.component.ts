db036ff7d4e4f71c05c7679d89f5f6137f091520
Andy Blubaugh <blubaugha@users.noreply.github.com>
2021-05-03 14:31:24 -0400

    Include Fixes From Demo April 2021 Symposium (#1311)
    * Added new "Ready" image
    
    * Fixed layout issues when there's cart items and when the cart is empty
    
    * Made item details mOrE better
    
    * Made loyalty mOrE better
    
    * Fixed style issue with iPad landscape where big boy image would cause scrollbar
    
    * Fixed style issue with iPad portrait and REWARDS image not being centered

--- openpos-client-libs/projects/openpos-client-core-lib/src/lib/screens-with-parts/sale/sale.component.ts
+++ openpos-client-libs/projects/openpos-client-core-lib/src/lib/screens-with-parts/sale/sale.component.ts
@@ -1,17 +1,19 @@
-import { Component, Injector } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, Injector, ViewChild } from '@angular/core';
 import { SaleInterface } from './sale.interface';
-import { MatDialog, MatBottomSheet } from '@angular/material';
+import { MatBottomSheet, MatDialog } from '@angular/material';
 import { PosScreen } from '../pos-screen/pos-screen.component';
 import { ScreenComponent } from '../../shared/decorators/screen-component.decorator';
 import { ITotal } from '../../core/interfaces/total.interface';
 
-import {Subscription, Observable, merge, Subject} from 'rxjs';
+import { merge, Observable, Subject, Subscription } from 'rxjs';
 import { IActionItem } from '../../core/actions/action-item.interface';
-import { OpenposMediaService, MediaBreakpoints } from '../../core/media/openpos-media.service';
+import { MediaBreakpoints, OpenposMediaService } from '../../core/media/openpos-media.service';
 import { Configuration } from './../../configuration/configuration';
 import { MobileSaleOrdersSheetComponent } from './mobile-sale-orders-sheet/mobile-sale-orders-sheet.component';
-import {KeyPressProvider} from '../../shared/providers/keypress.provider';
-import {takeUntil} from 'rxjs/operators';
+import { KeyPressProvider } from '../../shared/providers/keypress.provider';
+import { delay, filter, takeUntil, tap } from 'rxjs/operators';
+import { SaleItemCardListComponent } from '../../shared/screen-parts/sale-item-card-list/sale-item-card-list.component';
+import { ISellItem } from '../../core/interfaces/sell-item.interface';
 
 
 @ScreenComponent({
@@ -22,8 +24,10 @@ import {takeUntil} from 'rxjs/operators';
     templateUrl: './sale.component.html',
     styleUrls: ['./sale.component.scss']
 })
-export class SaleComponent extends PosScreen<SaleInterface> {
-    isMobile: Observable<boolean>;
+export class SaleComponent extends PosScreen<SaleInterface> implements AfterViewInit {
+    @ViewChild('scrollList') private saleItemCardList: SaleItemCardListComponent;
+
+    isMobile$: Observable<boolean>;
     totals: ITotal[];
     Configuration = Configuration;
     initialized = false;
@@ -31,14 +35,16 @@ export class SaleComponent extends PosScreen<SaleInterface> {
     buildScreen$ = new Subject();
     stop$: Observable<any>;
     sidenavOpened = false;
+    isEmpty = true;
 
     constructor(protected dialog: MatDialog,
                 injector: Injector,
                 media: OpenposMediaService,
                 private bottomSheet: MatBottomSheet,
-                private keyPressProvider: KeyPressProvider) {
+                private keyPressProvider: KeyPressProvider,
+                private changeDetectorRef: ChangeDetectorRef) {
         super(injector);
-        this.isMobile = media.observe(new Map([
+        this.isMobile$ = media.observe(new Map([
             [MediaBreakpoints.MOBILE_PORTRAIT, true],
             [MediaBreakpoints.MOBILE_LANDSCAPE, true],
             [MediaBreakpoints.TABLET_PORTRAIT, true],
@@ -58,10 +64,16 @@ export class SaleComponent extends PosScreen<SaleInterface> {
         this.removeOrderAction = this.screen.removeOrderAction;
         this.dialog.closeAll();
 
-        if(this.screen.logoutButton) {
+        if (this.screen.logoutButton) {
             this.keyPressProvider.globalSubscribe(this.screen.logoutButton).pipe(
                 takeUntil(this.stop$)
             ).subscribe(action => this.doAction(action));
+        }
+    }
+
+    ngAfterViewInit() {
+        if (this.saleItemCardList && this.saleItemCardList.items$) {
+            this.saleItemCardList.items$.subscribe(items => this.isEmpty === !items || items.length === 0);
         }
     }
 
@@ -84,10 +96,10 @@ export class SaleComponent extends PosScreen<SaleInterface> {
 
     openSheet(): void {
         console.log('Entering openSheet()');
-        const ref = this.bottomSheet.open( MobileSaleOrdersSheetComponent,
+        const ref = this.bottomSheet.open(MobileSaleOrdersSheetComponent,
             {data: this.screen, panelClass: 'sheet'});
-        this.subscriptions.add(new Subscription( () => ref.dismiss()));
-        this.subscriptions.add(ref.afterDismissed().subscribe( item => {
+        this.subscriptions.add(new Subscription(() => ref.dismiss()));
+        this.subscriptions.add(ref.afterDismissed().subscribe(item => {
             if (item !== undefined && item !== null) {
                 if (typeof item === 'object') {
                     this.doAction(this.removeOrderAction, item.number);
@@ -96,5 +108,12 @@ export class SaleComponent extends PosScreen<SaleInterface> {
                 }
             }
         }));
+    }
+
+    onItemsChanged(items: ISellItem[]): void {
+        this.isEmpty = items.length === 0;
+        // Angular gets angry if you update the current view when a child property changes. This prevents the error:
+        // ExpressionChangedAfterItHasBeenCheckedError: Expression has changed after it was checked.
+        this.changeDetectorRef.detectChanges();
     }
 }
