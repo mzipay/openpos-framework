17479c4649a6433c1fda3a5c3fc2e011e517cff4
Kevin DeLorey <kevin.delorey@leadingedje.com>
2021-05-26 14:30:18 -0600

    One Document at a Time Printing (#1374)

--- openpos-client-libs/projects/openpos-client-core-lib/src/lib/core/platform-plugins/printers/browser-printer.plugin.ts
+++ openpos-client-libs/projects/openpos-client-core-lib/src/lib/core/platform-plugins/printers/browser-printer.plugin.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { IPrinter } from './printer.interface';
 
 @Injectable({
@@ -13,26 +14,37 @@ export class BrowserPrinterPlugin implements IPrinter {
         return true;
     }
 
-    print(html: string) {
-        const hiddenPrintFrame = document.createElement("iframe");
+    print(html: string): Observable<void> {
+        return new Observable(observer => {
+            const hiddenPrintFrame = document.createElement("iframe");
 
-        hiddenPrintFrame.onload = e => {
-            let frame = e.target as HTMLIFrameElement;
-            let contentWindow = frame.contentWindow;
-            contentWindow.onbeforeunload = () => document.body.removeChild(hiddenPrintFrame);
-            contentWindow.onafterprint = () => document.body.removeChild(hiddenPrintFrame);
-            contentWindow.focus();
-            contentWindow.print();
-        };
+            hiddenPrintFrame.onload = e => {
+                let frame = e.target as HTMLIFrameElement;
+                let contentWindow = frame.contentWindow;
+                contentWindow.onbeforeunload = () => {
+                    document.body.removeChild(hiddenPrintFrame);
+                };
 
-        hiddenPrintFrame.style.position = "fixed";
-        hiddenPrintFrame.style.right = "0";
-        hiddenPrintFrame.style.bottom = "0";
-        hiddenPrintFrame.style.width = "0";
-        hiddenPrintFrame.style.height = "0";
-        hiddenPrintFrame.style.border = "0";
-        hiddenPrintFrame.srcdoc = html;
+                contentWindow.onafterprint = () => {
+                    observer.next();
+                    observer.complete();
 
-        document.body.appendChild(hiddenPrintFrame);
+                    document.body.removeChild(hiddenPrintFrame);
+                };
+    
+                contentWindow.focus();
+                contentWindow.print();
+            };
+    
+            hiddenPrintFrame.style.position = "fixed";
+            hiddenPrintFrame.style.right = "0";
+            hiddenPrintFrame.style.bottom = "0";
+            hiddenPrintFrame.style.width = "0";
+            hiddenPrintFrame.style.height = "0";
+            hiddenPrintFrame.style.border = "0";
+            hiddenPrintFrame.srcdoc = html;
+    
+            document.body.appendChild(hiddenPrintFrame);
+        });
     }
 }
