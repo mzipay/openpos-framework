21f208b8f5ddc82e3a80b2502658e10f998309e2
Chris Henson <chenson42@users.noreply.github.com>
2021-06-16 09:49:25 -0400

    Receipt simulator not working (#1404)

--- openpos-flow/src/main/java/org/jumpmind/pos/core/flow/EventBroadcaster.java
+++ openpos-flow/src/main/java/org/jumpmind/pos/core/flow/EventBroadcaster.java
@@ -52,7 +52,9 @@ public class EventBroadcaster {
                                 processEvent = true;
                             }
                         }
-                        if (event instanceof AppEvent && onEvent.receiveEventsFromPairedDevice() && stateManager.getDeviceId().equals(((AppEvent) event).getPairedDeviceId())) {
+                        if (event instanceof AppEvent && onEvent.receiveEventsFromPairedDevice() &&
+                                (stateManager.getDeviceId().equals(((AppEvent) event).getPairedDeviceId()) ||
+                                        ((AppEvent) event).getDeviceId().equals(stateManager.getPairedDeviceId()))) {
                             processEvent = true;
                         }
                     }
